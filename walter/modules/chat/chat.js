85c9c1c216da55b61fc9cb8dfa1df861ed41b362
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-12-26 19:51:33 -0300

    modifique la pantalla para que tenga un área donde se mueve el mouse y muesre el chat. tambien para que cuando llegan mensajes de otras páginas muestre el chat automáticamente. y cada vez que se envía un mensaje se oculta.

--- walter/modules/chat/chat.js
+++ walter/modules/chat/chat.js
@@ -2,12 +2,19 @@ var app = angular.module('mainApp');
 
 app.controller('ChatCtrl', function($rootScope, $scope, WebSocket, Utils) {
 
+  var delay = 1000;
   $scope.new_message = {'text':'escriba algo'};
   $scope.messages = [];
+  $scope.cclass = '';
 
   $scope.$on('ChatMessage', function(event,data) {
     var message = {'data':data}
     $scope.messages.push(message);
+    if ($scope.cclass == 'chat-my-message') {
+      $scope.cclass = 'chat-hide';
+    } else {
+      $scope.cclass = 'chat-show';
+    }
   });
 
 
@@ -19,5 +26,10 @@ app.controller('ChatCtrl', function($rootScope, $scope, WebSocket, Utils) {
       'data': $scope.new_message.text
     };
     WebSocket.send(JSON.stringify(msg));
+    $scope.cclass = 'chat-my-message';
+  }
+
+  $scope.showChat = function() {
+    $scope.cclass = 'chat-show';
   }
 });
