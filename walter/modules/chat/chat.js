069b859f5241af184c7b08e233ce221ed8de619f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-12-27 12:27:44 -0300

    haciendo mas pruebas con el chat y css. para ver como se puede llegar a maximizar, etc.

--- walter/modules/chat/chat.js
+++ walter/modules/chat/chat.js
@@ -6,14 +6,16 @@ app.controller('ChatCtrl', function($rootScope, $scope, WebSocket, Utils) {
   $scope.new_message = {'text':'escriba algo'};
   $scope.messages = [];
   $scope.cclass = '';
+  $scope.cmessages = '';
+  $scope.m = false;
 
   $scope.$on('ChatMessage', function(event,data) {
     var message = {'data':data}
     $scope.messages.push(message);
     if ($scope.cclass == 'chat-my-message') {
-      $scope.cclass = 'chat-hide';
+      $scope.hideChat();
     } else {
-      $scope.cclass = 'chat-show';
+      $scope.showChat();
     }
   });
 
@@ -27,9 +29,45 @@ app.controller('ChatCtrl', function($rootScope, $scope, WebSocket, Utils) {
     };
     WebSocket.send(JSON.stringify(msg));
     $scope.cclass = 'chat-my-message';
+    $scope.cmessages = '';
   }
 
   $scope.showChat = function() {
     $scope.cclass = 'chat-show';
   }
+
+  $scope.hideChat = function() {
+    $scope.cclass = 'chat-hide';
+  }
+
+
+  $scope.closeChat = function() {
+    $scope.cmessages = '';
+    $scope.hideChat();
+  }
+
+  $scope.maximizeChat = function() {
+    $scope.cmessages = 'chat-maximize';
+  }
+
+  $scope.minimizeChat = function() {
+    $scope.cmessages = 'chat-minimize';
+  }
+
+/*
+  $scope.mouseOver = function() {
+    $scope.
+  }
+
+
+  $scope.notmoving = function() {
+    $scope.m = false;
+  }
+
+  $scope.moving = function() {
+    $scope.m = true;
+  }
+*/
+
+
 });
