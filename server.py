c388b912aa1c4b2d723adea1d92b0a9090389ea8
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-12-28 16:41:50 -0300

    cambie el servidor para que sirva el contenido e indique que no se debe cachear nada. estaba habiendo problemas de cahce.

--- server.py
+++ server.py
@@ -4,9 +4,18 @@ import SocketServer
 
 PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+  def end_headers(self):
+    self.send_my_headers()
+    SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+  
+  def send_my_headers(self):
+    self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+    self.send_header("Pragma", "no-cache")
+    self.send_header("Expires", "0")
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+httpd = SocketServer.TCPServer(("", PORT), MyHTTPRequestHandler)
 
 print "serving at port", PORT
 httpd.serve_forever()
