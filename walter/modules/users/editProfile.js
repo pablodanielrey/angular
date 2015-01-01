5e6084feb02956c2afebe708560df4e713d9f03a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-12-31 22:25:20 -0300

    agregue los botones para actualizar los datos. y el para cancelar la actualizacion de datos.

--- walter/modules/users/editProfile.js
+++ walter/modules/users/editProfile.js
@@ -4,24 +4,61 @@ app.controller('EditProfileCtrl', function($scope, Session, Messages, Utils) {
 
   $scope.user = {};
 
-  $scope.$on('UserSelectedEvent', function(event,data) {
-
+  $scope.findUserData = function(id, callbackOk, callbackError) {
     var msg = {
       id: Utils.getId(),
       session: Session.getSessionId(),
       action: 'findUser',
-      user: { id: data }
+      user: { id: id}
     }
     Messages.send(msg, function(response) {
-      if (response.ok == undefined) {
-        alert('error');
-        return;
+      if (response.error != undefined) {
+        callbackError(response.error);
+      } else {
+        callbackOk(response.user);
       }
-      $scope.user = response.user;
     });
+  }
+
+  $scope.$on('UserSelectedEvent', function(event,data) {
+
+    $scope.user = {};
+    $scope.findUserData(data,
+      function(user) {
+        $scope.user = user;
+      },
+      function(error) {
+        alert(error);
+      });
 
   });
 
+
+  $scope.update = function() {
+    var msg = {
+      id: Utils.getId(),
+      session: Session.getSessionId(),
+      action: 'updateUser',
+      user: $scope.user
+    };
+    Messages.send(msg,function(response) {
+      if (response.error != undefined) {
+        alert(response.error);
+        return
+      }
+    });
+  }
+
+  $scope.cancel = function() {
+    $scope.user = {};
+    $scope.findUserData(data,
+      function(user) {
+        $scope.user = user;
+      },
+      function(error) {
+        alert(error);
+      });
+  }
 
 
 });
