48b0531ce13ae2b05ab20a5ac106b820b660b553
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-01-06 13:39:06 -0300

    modifique el archivo para obtener la config de un archvio json ubicado en la raiz del servidor web del cual se sirve el sitio.

--- walter/services/Config.js
+++ walter/services/Config.js
@@ -1,13 +1,27 @@
 
 var app = angular.module('mainApp');
 
-app.service('Config', function() {
+app.service('Config', function($http) {
 
   this.websocket = {
     proto: 'ws',
     host: '192.168.0.100',
     port: '8001'
   }
+
+  instance = this;
+
+  this.initialize = function() {
+    $http.get('config.json').
+    success(function(data, status, headers, config) {
+      instance.websocket = data;
+      alert(instance.websocket);
+    }).
+    error(function(data, status, headers, config) {
+      alert('el sistema no pudo obtener la configuración, algunas funciones no estarán disponibles');
+    })
+  }
+
 
   this.getWebsocketConnectionUrl = function() {
     return this.websocket.proto + '://' + this.websocket.host + ':' + this.websocket.port;
@@ -16,5 +30,7 @@ app.service('Config', function() {
   this.getServerUrl = function() {
     return document.URL;
   }
+
+  this.initialize();
 
 });
