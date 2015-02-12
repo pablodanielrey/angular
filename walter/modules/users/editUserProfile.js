ad26dca01b8d53d8216caf7b3b75dcd33e42de93
ivancas84 <ivancas84@gmail.com>
2015-02-12 13:33:35 -0300

    optimizar menu

--- walter/modules/users/editUserProfile.js
+++ walter/modules/users/editUserProfile.js
@@ -9,9 +9,16 @@ app.controller('EditUserProfileCtrl',function($scope,$rootScope,$timeout,Session
       return;
     }
 
-    var uid = s.user_id;
-    s.selectedUser = s.user_id;
-    Session.saveSession(s);
+
+	var uid = null;
+	if($rootScope.userId != null){
+		uid = $rootScope.userId;
+	} else {
+	     uid = s.user_id;
+	    s.selectedUser = s.user_id;
+	    Session.saveSession(s);
+	
+	}
 
     $rootScope.$broadcast('UserSelectedEvent',uid);
   });
