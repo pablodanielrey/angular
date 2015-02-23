57759853477708ac9efc8e144a14d9d7a7f75141
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-02-22 22:00:18 -0300

    cambie el header para que el inicio sea por funcion y no con un href. tambien el salir.

--- walter/modules/header/header.js
+++ walter/modules/header/header.js
@@ -1,7 +1,15 @@
 var app = angular.module('mainApp');
 
-app.controller('HeaderCtrl', function() {
+app.controller('HeaderCtrl', function($scope,$rootScope,$location) {
 
-  
+  $scope.goHome = function() {
+    $location.path('/main');
+    $rootScope.$broadcast("MenuOptionSelectedEvent",'Home');
+  }
+
+  $scope.logout = function() {
+    $location.path('/logout');
+    $rootScope.$broadcast("MenuOptionSelectedEvent",'Logout');
+  }
 
 });
