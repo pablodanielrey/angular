efeeb87dc1d019a6c317a8a93f752f26e70306be
Emanuel Pais <emanuel@d12.econo.unlp.edu.ar>
2015-02-24 12:25:26 -0300

    actualice los datos correspondientes a la base de datos

--- websocket/model/laboralInsertion.py
+++ websocket/model/laboralInsertion.py
@@ -6,18 +6,17 @@ class LaboralInsertions:
     def createLaboralInsertion(self,con,laboralInsertion):
         params = (laboralInsertion['id'],laboralInsertion['cv'],laboralInsertion['residir'],laboralInsertion['viajar'])
         cur = con.cursor()
-        cur.execute("insert into laboralInsertion (id,cv,residir,viajar) values (%s,%s,%s,%s)",params)
+        cur.execute("insert into laboral_insertion.users (id,cv,reside,travel) values (%s,%s,%s,%s)",params)
 
     def updateLaboralInsertion(self,con,li):
         laboralInsertion = ObjectView(li)
         params = (laboralInsertion.cv,laboralInsertion.residir,laboralInsertion.viajar,laboralInsertion.id)
         cur = con.cursor()
-        cur.execute('update laboralInsertion set cv = %s,residir = %s,viajar = %s where id = %s',params)
+        cur.execute('update laboral_insertion.users set cv = %s,reside = %s,travel = %s where id = %s',params)
 
     def findLaboralInsertion(self,con,id):
-        print "LaboralInsertions.findLaboralInserion id: %s" %id
         cur = con.cursor()
-        cur.execute('select id,cv,residir,viajar from laboralInsertion where id = %s',(id))
+        cur.execute('select id,cv,reside,travel from laboral_insertion.users where id = %s',(id))
         li = cur.fetchone()
         if li:
             return self.convertToDic(s)
@@ -26,7 +25,7 @@ class LaboralInsertions:
 
     def findAll(self,con):
         cur = con.cursor()
-        cur.execute('select id,cv,residir,viajar from laboralInsertion where id = %s',(id))
+        cur.execute('select id,cv,reside,travel from laboral_insertion.users where id = %s',(id))
         data = cur.fetchall()
         laboralInsertions = []
         for li in data:
@@ -38,7 +37,7 @@ class LaboralInsertions:
         laboralInsertion = {
             'id':li[0],
             'cv':li[1],
-            'residir':li[2],
-            'viajar':li[3]
+            'reside':li[2],
+            'travel':li[3]
         }
         return laboralInsertion
