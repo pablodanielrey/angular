aaba9ccb188300b53f296fcd954ea2b70fb54c22
Emanuel Pais <emanuel@d12.econo.unlp.edu.ar>
2015-02-24 12:30:32 -0300

    agregue un dato de prueba para el de insercion laboral

--- websocket/insert_basic_data.sql
+++ websocket/insert_basic_data.sql
@@ -2,3 +2,4 @@
 
 insert into users (id,dni,name,lastname) values ('1','1','admin','admin');
 insert into user_password (id,user_id,username,password) values ('1','1','admin','admin');
+insert into laboral_insertion.users (id) values ('1');
