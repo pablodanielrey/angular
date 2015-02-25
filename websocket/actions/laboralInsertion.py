571fd8224997381d1d784cc3e0a2176ec93a0ca0
Emanuel Pais <emanuel@d12.econo.unlp.edu.ar>
2015-02-25 12:28:49 -0300

    Complete la parte de idiomas correspondiente a insercion laboral del lado del servidor

--- websocket/actions/laboralInsertion.py
+++ websocket/actions/laboralInsertion.py
@@ -21,8 +21,8 @@ peticion:
     "laboralInsertion": {
         "id":"id del usuario a agregar la info de insercion laboral",
         "cv":"curriculum vitae del usario",
-        "residir":"si esta dispuesto a residir en otro lugar,
-        "viajar":"si esta dispuesto a viajar"
+        "reside":"si esta dispuesto a residir en otro lugar,
+        "travel":"si esta dispuesto a viajar"
     }
 }
 
@@ -56,8 +56,9 @@ class PersistLaboralInsertion:
         sid = message['session']
         self.profiles.checkAccess(sid,['ADMIN','USER'])
 
-        con = psycopg2.connect(host=self.config.configs['database_host'], dbname=self.config.configs['database_database'], user=self.config.configs['database_user'], password=self.config.configs['database_password'])
         try:
+            con = psycopg2.connect(host=self.config.configs['database_host'], dbname=self.config.configs['database_database'], user=self.config.configs['database_user'], password=self.config.configs['database_password'])
+
             laboralInsertion = message['laboralInsertion']
             self.laboralInsertion.persistLaboralInsertion(con,laboralInsertion)
             con.commit()
@@ -79,85 +80,6 @@ class PersistLaboralInsertion:
         finally:
             con.close()
 
-"""
-peticion:
-{
-    "id":"",
-    "action":"createLaboralInsertion",
-    "session":"session de usuario",
-    "laboralInsertion": {
-        "id":"id del usuario a agregar la info de insercion laboral",
-        "cv":"curriculum vitae del usario",
-        "residir":"si esta dispuesto a residir en otro lugar,
-        "viajar":"si esta dispuesto a viajar"
-    }
-}
-
-respuesta:
-{
-    "id":"id de la peticion",
-    "ok":"",
-    "error":""
-}
-
-"""
-
-class UpdateLaboralInsertion:
-
-    laboralInsertion = inject.attr(LaboralInsertion)
-    events = inject.attr(Events)
-    profiles = inject.attr(Profiles)
-    config = inject.attr(Config)
-
-    def handleAction(self, server, message):
-
-        if (message['action'] != 'updateLaboralInsertionData'):
-            return False
-
-        if 'laboralInsertion' not in message:
-            response = {'id':message['id'], 'error':'no existe la info correspondiente a insercion laboral '}
-            server.sendMessage(response)
-            return True
-
-        if 'id' not in message['laboralInsertion']:
-            response = {'id':message['id'], 'error':'no esta definido el id de insercion laboral '}
-            server.sendMessage(response)
-            return True
-
-        if message['laboralInsertion']['id'] == None:
-            response = {'id':message['id'], 'error':'insercionLaboral.id == null'}
-            server.sendMessage(response)
-            return True
-
-        """ chequeo que exista la sesion, etc """
-        sid = message['session']
-        self.profiles.checkAccess(sid,['ADMIN','USER'])
-
-        con = psycopg2.connect(host=self.config.configs['database_host'], dbname=self.config.configs['database_database'], user=self.config.configs['database_user'], password=self.config.configs['database_password'])
-        try:
-            laboralInsertion = message['laboralInsertion']
-            if laboralInsertion == None:
-                raise MailformedMessage()
-
-            self.LaboralInsertion.updateLaboralInsertion(con,laboralInsertion)
-            con.commit()
-
-            response = {'id':message['id'], 'ok':''}
-            server.sendMessage(response)
-
-            event = {
-                'type':'UserUpdatedEvent',
-                'data':laboralInsertion['id']
-            }
-            self.events.boradcast(server,event)
-            return True
-
-        except psycopg2.DatabaseError, e:
-            con.rollback()
-            raise e
-
-        finally:
-            con.close()
 
 
 """
@@ -201,8 +123,6 @@ class FindLaboralInsertion:
             return False
 
 
-        print "Holaaaaaaaaaaaaaaaa"
-
         """ chequeo que exista la sesion, etc """
         sid = message['session']
         self.profiles.checkAccess(sid,['ADMIN','USER'])
@@ -220,3 +140,323 @@ class FindLaboralInsertion:
 
         finally:
             con.close()
+
+
+        """-------------------------------"""
+"""
+    Modulo de acceso a los datos de los idiomas del usuario
+"""
+
+
+"""
+peticion:
+{
+    "id":"id de la peticion",
+    "action":"persistLanguage",
+    "session":"session de usuario",
+    "language": {
+        "id":"id del lenguaje a actualizar",
+        "user_id":"id del usuario",
+        "name":"nombre del lenguaje",
+        "level":"nivel"
+    }
+}
+
+respuesta:
+{
+    "id":"id de la peticion",
+    "ok":"",
+    "error":""
+}
+
+evento:
+{
+    'type':'LanguagePersistedEvent',
+    'data':language['id']
+}
+"""
+
+class PersistLanguage:
+
+    req = inject.attr(LaboralInsertion)
+    events = inject.attr(Events)
+    profiles = inject.attr(Profiles)
+    config = inject.attr(Config)
+
+    def handleAction(self, server, message):
+        #El procesamiento continuara solo si el mensaje solicitado por el cliente es el que se indica
+        if (message['action'] != 'persistLanguage'):
+            return False
+
+        #Verificar datos del mensaje, si no es correcto responder con un error
+        if 'language' not in message:
+            response = {'id':messages['id'], 'error':'no existe la info del lenguaje'}
+            server.sendMessage(response)
+            return True
+
+        #Verificar rol del usuario conectado a la session
+        sid = message['session']
+        self.profiles.checkAccess(sid,['ADMIN','USER'])
+
+        try:
+            #Abrir conexion con base de datos
+            con = psycopg2.connect(host=self.config.configs['database_host'], dbname=self.config.configs['database_database'], user=self.config.configs['database_user'], password=self.config.configs['database_password'])
+
+            #persistir el dato en la base
+            language = message['language']
+            self.req.persistLanguage(con,language)
+            con.commit()
+
+            #enviar respuesta al cliente
+            response = {'id':message['id'],'ok':''}
+            server.sendMessage(response)
+
+            #enviar evento al cliente
+            event = {
+                'type':'LanguagePersistedEvent',
+                'data':language['id']
+            }
+            self.events.broadcast(server,event)
+
+            return True
+
+        except psycopg2.DatabaseError, e:
+            con.rollback()
+            raise e
+
+        finally:
+            con.close()
+
+"""
+peticion:
+{
+    "id":"",
+    "action":"removeLanguage",
+    "session":"session de usuario",
+    "language": {
+        id:"id del lenguaje a eliminar"
+    }
+}
+
+respuesta:
+{
+    "id":"id de la peticion",
+    "ok":"",
+    "error":""
+}
+
+
+evento:
+{
+    'type':'LanguageDeletedEvent',
+    'data':language['id']
+}
+"""
+class DeleteLanguage:
+
+    req = inject.attr(LaboralInsertion)
+    events = inject.attr(Events)
+    profiles = inject.attr(Profiles)
+    config = inject.attr(Config)
+
+    def handleAction(self, server, message):
+        #El procesamiento continuara solo si el mensaje solicitado por el cliente es el que se indica
+        if message['action'] != 'deleteLanguage':
+            return False
+
+        #Verificar datos del mensaje, si no es correcto responder con un error
+        if 'language' not in message:
+            response = {'id':message['id'], 'error':'no existe la info del lenguaje'}
+            server.sendMessage(response)
+            return True
+
+        if 'id' not in message['language']:
+            response = {'id':message['id'], 'error': 'language.id == null'}
+            server.sendMessage(response)
+            return True
+
+        #Verificar rol del usuario conectado a la session
+        sid = message['session']
+        self.profiles.checkAccess(sid,['ADMIN','USER'])
+
+        try:
+            #Abrir conexion con base de datos
+            con = psycopg2.connect(host=self.config.configs['database_host'], dbname=self.config.configs['database_database'], user=self.config.configs['database_user'], password=self.config.configs['database_password'])
+
+            #buscar si existe el lenguaje
+            l = message['language']
+            language = self.req.findLanguage(con,l['id'])
+            if language == None:
+                response = {'id':message['id'],'error':'lenguaje inexistente'}
+                server.send(response)
+                return True
+
+            #eliminar el lenguaje
+            self.req.deleteLanguage(con,l['id'])
+
+            #enviar respuesta al cliente
+            response = {'id':message['id'],'ok':''}
+            server.sendMessage(response)
+
+            #enviar evento al cliente
+            event = {
+                'type':'LanguageDeletedEvent',
+                'data':language['id']
+            }
+            self.events.broadcast(server,event)
+
+            return True
+
+        except psycopg2.DatabaseError, e:
+            con.rollback()
+            raise e
+
+        finally:
+            con.close()
+
+
+"""
+
+peticion:
+{
+    "id":"",
+    "action":"findLanguage"
+    "session":"sesion de usuario"
+    "language":{
+        "id":"id del idioma"
+    }
+}
+
+respuesta:
+{
+    "id":"id de la peticion",
+    "language":[
+        {
+        "id":"id del idioma a actualizar",
+        "user_id":"id del usuario",
+        "name":"nombre del idioma",
+        "level":"nivel"
+        }
+    ],
+    "ok":""
+    "error":""
+}
+
+"""
+class FindLanguage:
+
+    req = inject.attr(LaboralInsertion)
+    profiles = inject.attr(Profiles)
+    config = inject.attr(Config)
+
+    def handleAction(self, server, message):
+
+        #El procesamiento continuara solo si el mensaje solicitado por el cliente es el que se indica
+        if message['action'] != 'findLanguageData':
+            return False
+
+        #Verificar datos del mensaje, si no es correcto responder con un error
+        if 'language' not in message:
+            response = {'id':message['id'], 'error':'no existe la info del idioma'}
+            server.sendMessage(response)
+            return True
+
+        if 'id' not in message['language']:
+            response = {'id':message['id'], 'error':'language.id == null'}
+            server.sendMessage(response)
+            return True
+
+        #verificar rol del usuario conectado a la sesion
+        sid = message['session']
+        self.profiles.checkAccess(sid,['ADMIN','USER'])
+
+        try:
+            #Abrir conexion a la base de datos
+            con = psycopg2.connect(host=self.config.configs['database_host'], dbname=self.config.configs['database_database'], user=self.config.configs['database_user'], password=self.config.configs['database_password'])
+
+            #realizo la consulta al modelo
+            l = message['language']
+            language = self.req.finLanguage(con,l['id'])
+            response = {'id':message['id'], 'language':language, 'ok':''}
+            server.sendMessage(response)
+
+            return True
+
+        finally:
+            con.close()
+
+"""
+
+peticion:
+{
+    "id":"",
+    "action":"listLanguageData"
+    "session":"sesion de usuario"
+    "user_id":"id del usuario del cual se necesitan sus idiomas"
+}
+
+respuesta:
+{
+    "id":"id de la peticion",
+    "languages":[
+        {
+        "id":"id del idioma a actualizar",
+        "user_id":"id del usuario",
+        "name":"nombre del idioma",
+        "level":"nivel"
+        }
+    ],
+    "ok":""
+    "error":""
+}
+
+"""
+class ListLanguages:
+
+    req = inject.attr(LaboralInsertion)
+    profiles = inject.attr(Profiles)
+    config = inject.attr(Config)
+
+    def handleAction(self, server, message):
+
+        #El procesamiento continuara solo si el mensaje solicitando por el cliente es el que se indica
+        if message['action'] != 'listLenguageData':
+            return False
+
+        #verificar datos del mensaje, si no es correcto responder con un error
+        if 'user_id' not in message:
+            response = {'id':message['id'], 'error':'no existe el id del usuario'}
+            server.sendMessage(response)
+            return True
+
+        #verificar el rol del usuario conectado a la sesion
+        sid = message['session']
+        self.profiles.checkAccess(sid,['ADMIN','USER'])
+
+        try:
+            #Abrir conexion a la base de datos
+            con = psycopg2.connect(host=self.config.configs['database_host'], dbname=self.config.configs['database_database'], user=self.config.configs['database_user'], password=self.config.configs['database_password'])
+
+            #realizo la consulta al modelo
+            languages = self.req.listLanguage(con, message['user_id'])
+            response = {'id':message['id'], 'ok':'', 'languages':languages}
+            server.sendMessage(response)
+            return True
+
+        finally:
+            con.close()
+
+
+        """-------------------------------"""
+
+#class PersistDegree:
+
+
+
+#class DeleteDegree:
+
+
+#class FindDegree:
+
+
+#class ListDegree:
--- websocket/model/laboralInsertion.py
+++ websocket/model/laboralInsertion.py
@@ -15,21 +15,6 @@ class LaboralInsertion:
             cur.execute('update laboral_insertion.users set cv = %s,reside = %s,travel = %s where id = %s',params)
 
 
-    """
-    def createLaboralInsertion(self,con,laboralInsertion):
-        params = (laboralInsertion['id'],laboralInsertion['cv'],laboralInsertion['residir'],laboralInsertion['viajar'])
-        cur = con.cursor()
-        cur.execute("insert into laboral_insertion.users (id,cv,reside,travel) values (%s,%s,%s,%s)",params)
-
-
-    def updateLaboralInsertion(self,con,li):
-        laboralInsertion = ObjectView(li)
-        params = (laboralInsertion.cv,laboralInsertion.residir,laboralInsertion.viajar,laboralInsertion.id)
-        cur = con.cursor()
-        cur.execute('update laboral_insertion.users set cv = %s,reside = %s,travel = %s where id = %s',params)
-    """
-
-
     def findLaboralInsertion(self,con,id):
         cur = con.cursor()
         cur.execute('select id,cv,reside,travel from laboral_insertion.users where id = %s',(id))
@@ -69,16 +54,15 @@ class LaboralInsertion:
         }
         return language
 
-    def createLanguage(self,con,language):
-        params = (language['id'],language['user_id'],language['name'],language['level'])
-        cur = con.cursor()
-        cur.execute('insert into laboral_insertion.languages (id,user_id,name,level) values(%s,%s,%s,%s)',params)
-
-    def updateLanguage(self,con,l):
-        language = ObjectView(l)
-        params = (language.user_id,language.name,language.level,language.id)
-        cur = con.cursor()
-        cur.execute('update laboral_insertion.languages set user_id = %s, name = %s, level = %s where id = %s',params)
+    def persistLanguage(self,con,data):
+        if findLanguage(con,data['id']) == None:
+            params = (data['id'],data['user_id'],data['name'],data['level'])
+            cur = con.cursor()
+            cur.execute('insert into laboral_insertion.languages (id,user_id,name,level) values(%s,%s,%s,%s)',params)
+        else:
+            params = (data['user_id'],data['name'],data['level'],data['id'])
+            cur = con.cursor()
+            cur.execute('update laboral_insertion.languages set user_id = %s, name = %s, level = %s where id = %s',params)
 
     def deleteLanguage(self,con,id):
         cur = con.cursor()
@@ -117,17 +101,15 @@ class LaboralInsertion:
         }
         return degree
 
-    def createDegree(self,con,d):
-        degree = ObjectView(d)
-        params = (degree.id,degree.user_id,degree.name,degree.curses,degree.aproved,degree.average1,degree.average2,degree.work_type)
-        cur = con.cursor()
-        cur.execute('insert into laboral_insertion.degree (id,user_id,name,curses,aproved,average1,average2,work_type) values(%s,%s,%s,%s,%s,%s,%s,%s)',params)
-
-    def updateDegree(self,con,d):
-        degree = ObjectView(d)
-        params = (degree.user_id,degree.name,degree.curses,degree.aproved,degree.average1,degree.average2,degree.work_type,degree.id)
-        cur = con.cursor()
-        cur.execute('update laboral_insertion.degree set user_id = %s, name = %s, curses = %s, aproved = %s, average1 = %s, average2 = %s, work_type = %s where id = %s',params)
+    def persistDegree(self,con,data):
+        if findDegree(con,data['id']) == None:
+            params = (degree['id'],degree['user_id'],degree['name'],degree['curses'],degree['aproved'],degree['average1'],degree['average2'],degree['work_type'])
+            cur = con.cursor()
+            cur.execute('insert into laboral_insertion.degree (id,user_id,name,curses,aproved,average1,average2,work_type) values(%s,%s,%s,%s,%s,%s,%s,%s)',params)
+        else:
+            params = (degree['user_id'],degree['name'],degree['curses'],degree['aproved'],degree['average1'],degree['average2'],degree['work_type'],degree['id'])
+            cur = con.cursor()
+            cur.execute('update laboral_insertion.degree set user_id = %s, name = %s, curses = %s, aproved = %s, average1 = %s, average2 = %s, work_type = %s where id = %s',params)
 
     def deleteDegree(self,con,id):
         cur = con.cursor()
