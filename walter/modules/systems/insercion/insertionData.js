81e57e981189297384c673dc1a9daef0b58a175f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-02-25 23:57:12 -0300

    ahora estoy actualizando el scope y esta funcioanndo sin el apply. o sea que deberíamos dejarlo asi.

--- walter/modules/systems/insercion/insertionData.js
+++ walter/modules/systems/insercion/insertionData.js
@@ -4,22 +4,27 @@ var app = angular.module('mainApp');
 app.controller('LaboralInsertionDataCtrl', function($scope) {
 
   $scope.addCv = function() {
-    var f = document.getElementById('fileCv').files[0];
-    var r = new FileReader();
-    r.onloadend = function(e) {
-      var data = e.target.result;
-      // aca se tiene data con los datos del cv.
 
-      var uInt8 = new Uint8Array(data);
-      var len = uInt8.byteLength;
-      var binary = '';
-      for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode(uInt8[i]);
+//    $scope.$apply(function() {
+
+      var f = document.getElementById('fileCv').files[0];
+      var r = new FileReader();
+      r.onloadend = function(e) {
+        var data = e.target.result;
+        // aca se tiene data con los datos del cv.
+
+        var uInt8 = new Uint8Array(data);
+        var len = uInt8.byteLength;
+        var binary = '';
+        for (var i = 0; i < len; i++) {
+          binary += String.fromCharCode(uInt8[i]);
+        }
+        $scope.insertionData.cv = window.btoa(binary);
       }
-      console.log(window.btoa(binary));
-    }
-    r.readAsArrayBuffer(f);
-    
+      r.readAsArrayBuffer(f);
+
+//    });
+
   }
 
 });
