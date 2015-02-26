1b0286b6ee5f221d8c35fed5b51cdc3630b61c0b
ivancas84 <ivancas84@gmail.com>
2015-02-26 10:34:26 -0300

    actualizar controlador de languages para que deje por defecto un lenguaje vacio (a pedido de walter para facilitar el disenio)

--- walter/modules/systems/insercion/languages.js
+++ walter/modules/systems/insercion/languages.js
@@ -1,7 +1,17 @@
 var app = angular.module('mainApp');
 
 
-app.controller('LanguagesLaboralInsertionCtrl', function($scope) {
+app.controller('LanguagesLaboralInsertionCtrl', ["$scope", "$timeout", function($scope, $timeout) {
+	
+	$scope.initializeLanguage = function(){
+		if(($scope.insertionData.languages == null) || ($scope.insertionData.languages == undefined)){
+			$scope.insertionData.languages = [];
+		}
+		
+		if($scope.insertionData.languages.length == 0){
+			$scope.addLanguage();
+		}
+	}
 
 	$scope.addLanguage = function(){
 		$scope.insertionData.languages.push({language:"", level:"basico"});
@@ -9,7 +19,11 @@ app.controller('LanguagesLaboralInsertionCtrl', function($scope) {
 	
 	$scope.deleteLanguage = function($index){
 		$scope.insertionData.languages.splice($index, 1);
+		$scope.initializeLanguage();
 	}
 	
+	$timeout(function() {
+		$scope.initializeLanguage()
+	},0);
 
-});
+}]);
