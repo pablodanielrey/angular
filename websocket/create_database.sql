187b23ce16913a67515592e294a3655e66220be7
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-03-01 02:53:46 -0300

    agregue la tabla para el sistema de au24. en este caso se mantiene si esta permitido login y de que tipo es el usuario

--- websocket/create_database.sql
+++ websocket/create_database.sql
@@ -122,7 +122,15 @@ create table students.users (
 );
 
 
+/*
+  tablas del módulo de au24
+*/
+create schema au24;
 
+create table au24.users (
+  id varchar not null primary key references profile.users (id),
+  type varchar
+);
 
 /*
   tablas del módulo de inserción laboral
