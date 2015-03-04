b057cda34e78e12baff26071f16691aa5acea7aa
ivancas84 <ivancas84@gmail.com>
2015-03-04 12:27:18 -0300

    actualizar controlador insertionData para leer archivos

--- walter/modules/systems/insercion/insertionData.js
+++ walter/modules/systems/insercion/insertionData.js
@@ -3,29 +3,13 @@ var app = angular.module('mainApp');
 
 app.controller('LaboralInsertionDataCtrl', function($scope, $timeout, LaboralInsertion) {
 
-  $scope.addCv = function() {
-
-//    $scope.$apply(function() {
-
-      var f = document.getElementById('fileCv').files[0];
-      var r = new FileReader();
-      r.onloadend = function(e) {
-        var data = e.target.result;
-        // aca se tiene data con los datos del cv.
-
-        var uInt8 = new Uint8Array(data);
-        var len = uInt8.byteLength;
-        var binary = '';
-        for (var i = 0; i < len; i++) {
-          binary += String.fromCharCode(uInt8[i]);
-        }
-        $scope.model.insertionData.cv = window.btoa(binary);
-      }
-      r.readAsArrayBuffer(f);
-
-//    });
-
-  }
+	/**
+	 * Agregar cv como base 64
+	 */
+	$scope.addCv = function($fileContent){
+		$scope.model.insertionData.cv = window.btoa($fileContent)
+		$scope.model.insertionData.cv = null;
+	};
 
 
   $scope.loadData = function() {
