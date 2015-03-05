befc3e73b0d851544400554b1724080a96292149
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-03-04 23:31:26 -0300

    agregue algunas vistas que son utiles a la hora de debuggear o encontrar por conola a un usuario

--- websocket/create_database.sql
+++ websocket/create_database.sql
@@ -206,3 +206,36 @@ create table system.sessions (
   data varchar,
   expire timestamp default now()
 );
+
+
+/*
+  Vistas útiles para buscar usuarios rápido
+*/
+
+create schema views;
+
+
+create or replace view views.users_full_data_with_pass as
+  select p.id, p.dni, p.name, p.lastname, c.username, c.password, s.student_number, m.email from profile.users as p
+  left outer join credentials.user_password as c on p.id = c.user_id
+  left outer join students.users as s on p.id = s.id
+  left outer join profile.mails as m on p.id = m.user_id;
+
+create or replace view views.users_full_data as
+  select p.id, p.dni, p.name, p.lastname, c.username, s.student_number, m.email from profile.users as p
+  left outer join credentials.user_password as c on p.id = c.user_id
+  left outer join students.users as s on p.id = s.id
+  left outer join profile.mails as m on p.id = m.user_id;
+
+create or replace view views.users_with_pass as
+  select p.dni, c.username, c.password, s.student_number, m.email from profile.users as p
+  left outer join credentials.user_password as c on p.id = c.user_id
+  left outer join students.users as s on p.id = s.id
+  left outer join profile.mails as m on p.id = m.user_id;
+
+create or replace view views.users as
+  select p.id, p.dni, c.username, s.student_number, m.email, a.type from profile.users as p
+  left outer join credentials.user_password as c on p.id = c.user_id
+  left outer join students.users as s on p.id = s.id
+  left outer join profile.mails as m on p.id = m.user_id
+  left outer join au24.users as a on p.id = a.id;
