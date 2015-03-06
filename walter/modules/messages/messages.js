bca6712291ea3c6340dcac268ae0b535e4bc8daa
Walter <walter@pc5comp>
2015-03-06 10:44:50 -0300

    estoy modificando el css de los mensajes

--- walter/modules/messages/messages.js
+++ walter/modules/messages/messages.js
@@ -3,7 +3,7 @@ var app = angular.module('mainApp');
 app.controller('MessagesCtrl',function($scope,$timeout) {
 
   $scope.visible = false;
-  $scope.messages = ['hola','maxi'];
+  $scope.messages = ['Error creando la contraseña','No se pudo enviar el mail'];
 
   $scope.$on('ShowMessageEvent',function(event,data) {
     $scope.messages.push(data);
