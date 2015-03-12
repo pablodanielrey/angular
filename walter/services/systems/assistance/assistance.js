3e7bc843a19da9626f874fc241fd5ffc84473148
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-03-12 14:44:53 -0300

    agregue mensajes y defini prototipos de los mensajes en el service de asistencia.

---
+++ walter/services/systems/assistance/assistance.js
@@ -0,0 +1,27 @@
+var app = angular.module('mainApp');
+
+app.service('Assistance',function($scope) {
+
+  this.getAssistanceStatus = function() {
+
+  }
+
+  this.getAssistanceData = function() {
+
+  }
+
+  this.getOfficesByUser = function() {
+
+  }
+
+  this.getJustifications = function() {
+
+  }
+
+  this.getJustificationStock = function() {
+
+  }
+
+
+
+})
