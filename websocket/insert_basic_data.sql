c4cb4217eae822b3e26b815d15da409b6eac5258
ivancas84 <ivancas84@gmail.com>
2015-03-16 08:50:52 -0300

    agregar roles adicionales al usuario administrador

--- websocket/insert_basic_data.sql
+++ websocket/insert_basic_data.sql
@@ -4,3 +4,4 @@ insert into profile.users (id,dni,name,lastname) values ('1','1','admin','admin'
 insert into credentials.user_password (id,user_id,username,password) values ('1','1','admin','admin');
 insert into credentials.auth_profile (user_id,profile) values ('1','ADMIN');
 insert into credentials.auth_profile (user_id,profile) values ('1','ADMIN-TUTOR');
+insert into credentials.auth_profile (user_id,profile) values ('1','ADMIN-ASSISTANCE');
