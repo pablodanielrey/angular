e381eb355c683542a50a09be1a880d216ea2f81a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-03-17 15:19:23 -0300

    me faltaba acomodar el merge. ahi lo hice y comente el codigo de ema hasta que este listo para pasar a producción

--- websocket/Main.py
+++ websocket/Main.py
@@ -14,11 +14,7 @@ from Ws.SimpleWebSocketServer import SimpleWebSocketServer
 from Ws.SimpleWebSocketServer import SimpleSSLWebSocketServer
 from websocketServer import WebsocketServer
 
-<<<<<<< HEAD
 #from model.assistance import AssistanceWebsocketServer
-=======
-from model.assistance import AssistanceWebsocketServer,AssistanceWebSocketClient
->>>>>>> a7016f8c043765183a715a751f274ccfa76f3f24
 
 
 from actions.chat import SendEventToClients
@@ -78,13 +74,11 @@ if __name__ == '__main__':
   ''' codigo de inicialización del servidor '''
 
   websocketServer = SimpleWebSocketServer(config.configs['server_ip'],int(config.configs['server_port']),WebsocketServer,actions)
-<<<<<<< HEAD
+
   #websocketServerAssistance = SimpleWebSocketServer(config.configs['server_ip'],8026,AssistanceWebsocketServer,None)
-=======
-  websocketServerAssistance = SimpleWebSocketServer(config.configs['server_ip'],8026,AssistanceWebsocketServer,None)
-  websocketClientAssistance = AssistanceWebSocketClient()
-  websocketClientAssistance.handle(config)
->>>>>>> a7016f8c043765183a715a751f274ccfa76f3f24
+  #websocketClientAssistance = AssistanceWebSocketClient()
+  #websocketClientAssistance.handle(config)
+
   #websocketSslServer = SimpleSSLWebSocketServer(config.configs['server_ssl_ip'],int(config.configs['server_ssl_port']),WebsocketServer,actions, config.configs['server_ssl_cert'],config.configs['server_ssl_key'])
   #httpServer = SocketServer.TCPServer(('192.168.0.100',8002), MyHttpServerRequestHandler)
 
@@ -97,7 +91,7 @@ if __name__ == '__main__':
   signal.signal(signal.SIGINT,close_sig_handler)
 
   thread.start_new_thread(serveWebsockets,(websocketServer,1))
-  thread.start_new_thread(serveWebsockets,(websocketServerAssistance,1))
+  #thread.start_new_thread(serveWebsockets,(websocketServerAssistance,1))
   #thread.start_new_thread(serveWebsockets,(websocketSslServer,1))
   #thread.start_new_thread(serveHttp,(httpServer,1))
 
