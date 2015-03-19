af7b2bd0e444521b8b215df68e97ea8ed6fde722
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-03-19 16:30:26 -0300

    error de lenguaje. era python y no javascript

--- websocket/websocketServer.py
+++ websocket/websocketServer.py
@@ -91,9 +91,9 @@ class WebsocketServer(WebSocket):
 
   def sendMessage(self,msg):
       jmsg = json.dumps(msg,cls=DateTimeEncoder)
-      if (len(jmsg) < 1000) {
-        logging.debug(jmsg)
-      }
+      if (len(jmsg) < 1000):
+          logging.debug(jmsg)
+
       super(WebsocketServer,self).sendMessage(jmsg)
 
       #maxMessageSize = 1000
