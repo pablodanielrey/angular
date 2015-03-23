7c331a6a9a71bcf21cf948ad5dd5dae225983f3b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-03-23 16:24:12 -0300

    prepare para que se puedea minimizar la app. por lo menos el controlador del login. con cada controlador hay que hacer lo mimso.

--- walter/modules/login/login.js
+++ walter/modules/login/login.js
@@ -5,7 +5,8 @@ var app = angular.module('mainApp');
  * @param $scope Scope
  * Supone la existencia de un elemento padre que maneja los eventos dontShowMessage y showMessage para administrar mensajes
  */
-app.controller("LoginCtrl", function($rootScope,$scope, Session, Credentials, Notifications) {
+app.controller("LoginCtrl", ['$rootScope','$scope','Session','Credentials','Notifications',
+	function($rootScope,$scope, Session, Credentials, Notifications) {
 
 		$scope.user = {
 			username: '',
@@ -45,4 +46,4 @@ app.controller("LoginCtrl", function($rootScope,$scope, Session, Credentials, No
 				});
 		};
 
-});
+}]);
