fcd5fd7007471cc3681c5c4443e6d97fe7571963
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-03-24 15:58:38 -0300

    corregí un error cuando se muestran los legajos

--- walter/modules/systems/student/editStudent.js
+++ walter/modules/systems/student/editStudent.js
@@ -1,61 +1,60 @@
 
 var app = angular.module('mainApp');
 
-app.controller('EditStudentCtrl', function($scope, $timeout, Student, Session) {
-  $scope.student = {};
+app.controller('EditStudentCtrl', ['$scope','$timeout','Student','Session','Notifications',
+  function($scope, $timeout, Student, Session, Notifications) {
+    $scope.student = {};
 
-  $scope.save = function() {
-    var s = Session.getCurrentSession();
-    if (s == null) {
-      return;
-    }
-
-    if (s.selectedUser == undefined || s.selectedUser == null) {
-      return;
-    }
-
-    $scope.student.id = s.selectedUser;
-
-    Student.persistStudent($scope.student,
-		function(student) {
-			alert("registro actualizado");
-		},
-		function(error) {
-			alert(error);
-		}
-	);
-  }
-
-
-  /**
-  * Carga los datos del usuario seleccionado dentro de la sesion
-  */
-  $scope.loadStudentData = function() {
-    console.log('load data');
-    var s = Session.getCurrentSession();
-    if (s == null) {
-      return;
-    }
-    if (s.selectedUser == undefined || s.selectedUser == null) {
-      alert("error");
-    }
-
-    var uid = s.selectedUser;
-
-    Student.findStudentData(uid,
-      function(data) {
-        $scope.student = data;
-      },
-      function(error) {
-        alert(error);
+    $scope.save = function() {
+      var s = Session.getCurrentSession();
+      if (s == null) {
+        return;
       }
-    );
-  }
 
-  $timeout(function() {
-    $scope.loadStudentData();
-  },0);
+      if (s.selectedUser == undefined || s.selectedUser == null) {
+        return;
+      }
+
+      $scope.student.id = s.selectedUser;
+
+      Student.persistStudent($scope.student,
+  		function(student) {
+  			Notifications.message("registro actualizado");
+  		},
+  		function(error) {
+  		  Notifications.message(error);
+  		}
+  	);
+    }
 
 
+    /**
+    * Carga los datos del usuario seleccionado dentro de la sesion
+    */
+    $scope.loadStudentData = function() {
+      console.log('load data');
+      var s = Session.getCurrentSession();
+      if (s == null) {
+        return;
+      }
+      if (s.selectedUser == undefined || s.selectedUser == null) {
+        Notifications.message("error");
+      }
 
-})
+      var uid = s.selectedUser;
+
+      Student.findStudentData(uid,
+        function(data) {
+          $scope.student = data.student;
+        },
+        function(error) {
+          Notifications.message(error);
+        }
+      );
+    }
+
+    $timeout(function() {
+      $scope.loadStudentData();
+    },0);
+  }]
+)
