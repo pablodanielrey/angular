6431524540f3948aed8ff63518e7ed5fd3512592
Emanuel Pais <emanuel84@gmail.com>
2015-03-30 16:31:41 -0300

    Elimine el switch que estaba de mas, era para mostrar el nombre de la justificacion, pero ya esta cargado en la bd

--- web/modules/systems/assistance/adminRequestAssistance.js
+++ web/modules/systems/assistance/adminRequestAssistance.js
@@ -61,12 +61,7 @@ app.controller('AdminRequestAssistanceCtrl', function($scope, $timeout, Assistan
                 $scope.justifications = new Map();
                 for (x in response) {
                     var r = response[x];
-                    switch(r.name) {
-                        case "absent": name = "Ausente con Aviso"; break;
-                        case "compensatory": name = "Compensatorio"; break;
-                        default: name = r.name; break;
-                    }
-                    $scope.justifications.set(r.id,name);
+                    $scope.justifications.set(r.id,r.name);
                     $scope.loadRequests();
                 }
             },
