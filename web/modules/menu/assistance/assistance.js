a4a146ce6ff564e403567d1982e83f2aef8dd607
Emanuel Pais <emanuel84@gmail.com>
2015-04-07 09:36:58 -0300

    Saque del menu de usuario de asistencia la parte de administracion

--- web/modules/menu/assistance/assistance.js
+++ web/modules/menu/assistance/assistance.js
@@ -55,9 +55,6 @@ app.controller('AssistanceOptionCtrl', function($scope, $rootScope, Profiles, As
                     $scope.items = [];
                     $scope.items.push({ label:'Inicio', img:'fa-tachometer', function: $scope.summary});
                     $scope.items.push({ label:'Solicitudes', img:'fa-ticket', function: $scope.requestAssistance});
-                    $scope.items.push({ label:'Adm. Solicitudes ', img:'fa-ticket', function: $scope.adminRequestAssistance});
-                    $scope.items.push({ label:'Horas Extras ', img:'fa-plus', function: $scope.requestAuthority});
-                    $scope.items.push({ label:'Admin Horas Extras ', img:'fa-plus', function: $scope.adminRequestOverTime});
 
                     Assistance.getUserOfficeRoles(
                       function(roles) {
@@ -66,7 +63,6 @@ app.controller('AssistanceOptionCtrl', function($scope, $rootScope, Profiles, As
                           $scope.items.push({ label:'Adm. Solicitudes ', img:'fa-ticket', function: $scope.adminRequestAssistance});
                           $scope.items.push({ label:'Horas Extras ', img:'fa-plus', function: $scope.requestAuthority});
                           $scope.items.push({ label:'Admin Horas Extras ', img:'fa-plus', function: $scope.adminRequestOverTime});
-                          $scope.items.push({ label:'Incumplimiento de horarios', img:'fa-exclamation-triangle', function: $scope.assistanceFails});
                         }
                       },
                       function(err) {
