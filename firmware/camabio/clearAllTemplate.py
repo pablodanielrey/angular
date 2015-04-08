e186bd62c1da1832adb89d00e4d6f65494b54c29
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-04-08 15:32:43 -0300

    inicie el codigo para implementar los comandos de los sensores de camabio

---
+++ firmware/camabio/clearAllTemplate.py
@@ -0,0 +1,40 @@
+import array
+import serial
+import camabio
+
+"""
+Command Packet
+PREFIX 0xAA55
+CMD 0x0106
+LEN 0
+DATA Null
+CKS Check Sum
+
+Response Packet
+PREFIX 0x55AA
+RCM 0x0106
+LEN 4
+RET ERR_SUCCESS or ERR_FAIL
+DATA 2byte Success:Total number of deleted template Fail:Error code
+CKS Check Sum
+"""
+
+
+data = [0x55,0x06,0x01,0x00,0x00,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x00,0x00]
+camabio.setChksum(data)
+
+print('abriendo puerto seriel')
+ser = serial.Serial("/dev/ttyS1",115200,timeout=5)
+
+print('escribiendo bytes en el puerto serie')
+camabio.printArray(data)
+bytesToWrite = array.array('B', data).tostring()
+ser.write(bytesToWrite);
+ser.flush()
+
+print('tratando de leer bytes desde el puerto serie: ')
+data2 = ser.read(len(data))
+if data2 == None:
+    print('No se leyo ningun byte')
+else:
+    camabio.printHexString(data2)
