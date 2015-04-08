8a9c3365531ddbcea376505ce694f5ca9730a242
Emanuel Pais <emanuel84@gmail.com>
2015-04-08 09:54:12 -0300

    Agregue las compas que faltaban y modifique el estilo

--- web/modules/systems/assistance/fails.js
+++ web/modules/systems/assistance/fails.js
@@ -14,19 +14,51 @@ app.controller('AssistanceFailsCtrl', function($scope, $timeout, Notifications, 
     $scope.model.assistanceFails = [{}]
   }
 
-  $scope.search = function() {
-    $scope.model.assistanceFails = [{}];
-    $scope.model.end.setHours(23);
-    $scope.model.end.setMinutes(59);
+  $scope.initializeDate = function() {
     $scope.model.begin.setHours(0);
     $scope.model.begin.setMinutes(0);
-    console.log($scope.model.begin);
-    console.log($scope.model.end);
+    $scope.model.begin.setSeconds(0);
+    $scope.model.end.setHours(23);
+    $scope.model.end.setMinutes(59);
+    $scope.model.end.setSeconds(59);
+  }
+
+  $scope.search = function() {
+    $scope.model.assistanceFails = [{}];
+    $scope.initializeDate();
     Assistance.getFailsByDate($scope.model.begin, $scope.model.end,
       function(response) {
         for (var i = 0; i < response.length; i++) {
           var r = response[i];
           r.fail.dateFormat = new Date(r.fail.date).toLocaleDateString();
+
+          if (r.fail.startSchedule || r.fail.endSchedule) {
+            r.fail.dateSchedule = (r.fail.startSchedule) ? r.fail.startSchedule : r.fail.endSchedule;
+          }
+
+          //esto es de prueba
+          // r.fail.start = r.fail.date;
+          
+          if (r.fail.start || r.fail.end) {
+            r.fail.wh = (r.fail.start) ?  new Date(r.fail.start) : new Date(r.fail.end);
+            var hs = r.fail.wh.getHours();
+            var min = r.fail.wh.getMinutes();
+            r.fail.wh = ('0' + hs).substr(-2) + ":" + ('0' + min).substr(-2);
+          } else {
+            r.fail.wh = "-";
+          }
+
+          // esto es de prueba
+          // r.fail.seconds = 3825;
+
+          if (r.fail.seconds) {
+            var hoursDiff = Math.floor((r.fail.seconds / 60) / 60);
+            var minutesDiff = Math.floor((r.fail.seconds / 60) % 60);
+            r.fail.diff = ('0' + hoursDiff).substr(-2) + ":" + ('0' + minutesDiff).substr(-2);
+          } else {
+            r.fail.diff = "-";
+          }
+
           $scope.model.assistanceFails.push(r);
         }
         $scope.predicate = 'user.dni';
