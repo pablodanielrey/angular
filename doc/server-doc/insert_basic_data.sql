5daf9857177a76a9349ae29dc0d94b87abbeeafa
Emanuel Pais <emanuel84@gmail.com>
2015-04-09 14:47:30 -0300

    Pablo modifico para agregar info

--- doc/server-doc/insert_basic_data.sql
+++ doc/server-doc/insert_basic_data.sql
@@ -28,3 +28,6 @@ insert into assistance.offices_roles (user_id,role,office_id) select '1','autori
   para autorizar las horas extras que son pedidas por las personas que tienen rol autoriza en las oficinas
 */
 insert into assistance.offices_roles (user_id,role,office_id) select '1','horas-extras',id from assistance.offices o where o.parent is null;
+
+
+insert into credentials.auth_profile (user_id,profile) select id,'ADMIN-ASSISTANCE' from profile.users where dni in ('27294557','31381082','30001823','29694757');
