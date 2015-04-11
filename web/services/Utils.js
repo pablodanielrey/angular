d029c4697eea0c8b3aff17cd533a3d260439cab7
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-04-11 15:23:51 -0300

    agregue un método para convertir un base64 recibido desde el server. en un blob application/octet-stream que se puede guardar haceindo un windows.saveAs(b,name)

--- web/services/Utils.js
+++ web/services/Utils.js
@@ -2,6 +2,13 @@ var app = angular.module('mainApp');
 
 app.service('Utils', function() {
 
+  this.base64ToBlob = function(data) {
+    var binary = atob(data.cv);
+    var array = new Uint8Array(binary.length);
+    for( var i = 0; i < binary.length; i++ ) { array[i] = binary.charCodeAt(i); };
+    return new Blob([array],{type: "octet/stream;base64"});
+  }
+
   this.getId = function() {
     var id =  (Math.floor((Math.random() * 1000000000) + 1)).toString();
     return id;
