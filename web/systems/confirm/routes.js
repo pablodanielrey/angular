d0a118bc7af6f69960a4b120d3e73a013aeddbbe
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-04-24 13:53:29 -0300

    corregi el problema de confirmar un mail

---
+++ web/systems/confirm/routes.js
@@ -0,0 +1,19 @@
+
+var app = angular.module('mainApp')
+
+app.config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider
+
+  .when('/confirmMail/:hash', {
+    templateUrl: '/modules/users/confirmMail.html',
+    controller: 'ConfirmMailCtrl'
+  })
+
+  .when('/confirmAccountRequest/:hash?', {
+    templateUrl: '/modules/account/confirmAccountRequest.html',
+    controller: 'ConfirmAccountRequestCtrl'
+  })
+
+  }
+])
