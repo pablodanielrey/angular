08d4eca575c252c081ecb84b33d4a742d2e42464
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-05-04 22:49:45 -0300

    acomode para que se muestren las justificaciones usando una funcion

--- web/modules/systems/assistance/showAssistance.js
+++ web/modules/systems/assistance/showAssistance.js
@@ -308,6 +308,48 @@ $scope.order = function(predicate, reverse) {
   }
 
 
+
+
+
+
+  /**
+  * CODIGO PORONGA A ARREGLAR. LO METO AHORA PORQUE ESTOY CANSADO Y PARA SOLUCIONAR EL TEMA DE LAS JUSTIFICAIONES
+  */
+
+  $scope.model.justificationNames = {
+    'e0dfcef6-98bb-4624-ae6c-960657a9a741':'Ausente con aviso',
+    '48773fd7-8502-4079-8ad5-963618abe725':'Compensatorio',
+    'fa64fdbd-31b0-42ab-af83-818b3cbecf46':'Boleta de Salida',
+    '4d7bf1d4-9e17-4b95-94ba-4ca81117a4fb':'Art 102',
+    'b70013e3-389a-46d4-8b98-8e4ab75335d0':'Pre-Exámen',
+    '76bc064a-e8bf-4aa3-9f51-a3c4483a729a':'Licencia Anual Ordinaria',
+    '50998530-10dd-4d68-8b4a-a4b7a87f3972':'Resolución',
+    'f9baed8a-a803-4d7f-943e-35c436d5db46':'Licencia Médica Corta Duración',
+    'a93d3af3-4079-4e93-a891-91d5d3145155':'Licencia Médica Largo Tratamiento',
+    'b80c8c0e-5311-4ad1-94a7-8d294888d770':'Licencia Médica Atención Familiar',
+    '478a2e35-51b8-427a-986e-591a9ee449d8':'Justificado por Médico',
+    '5ec903fb-ddaf-4b6c-a2e8-929c77d8256f':'Feriado',
+    '874099dc-42a2-4941-a2e1-17398ba046fc':'Paro',
+    'b309ea53-217d-4d63-add5-80c47eb76820':'Cumpleaños',
+    '0cd276aa-6d6b-4752-abe5-9258dbfd6f09':'Duelo',
+    'e8019f0e-5a70-4ef3-922c-7c70c2ce0f8b':'Donación de Sangre'
+  };
+
+
+  $scope.getJustificationName = function(id) {
+    return $scope.model.justificationNames[id];
+  }
+
+
+
+
+
+
+
+
+
+
+
   /**
    * En funcion de los datos del formulario se consultaran las justificaciones
    * @returns {undefined}
