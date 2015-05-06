d9cfe7a7d01c481680707bb5cc9633305bfbc5e6
Walter Blanco <walter@creativehands.com.ar>
2015-05-06 09:52:16 -0300

    modifique el estilo y el html de assitanceFails

--- web/services/Utils.js
+++ web/services/Utils.js
@@ -127,72 +127,72 @@ app.service('Utils', function() {
     var j = this.getJustification(justificationId);
     return j.name;
   };
-  
+
   this.getJustificationShortName = function(justificationId){
      var j = this.getJustification(justificationId);
      return j.shortName;
   };
-  
+
   this.getJustificationIcon = function(justificationId){
     var j = this.getJustification(justificationId);
     return j.icon;
   };
- 
-  
+
+
   this.getJustification = function(justificationId){
     switch(justificationId){
       case 'e0dfcef6-98bb-4624-ae6c-960657a9a741':
-        return {id:justificationId, name:'Ausente con aviso', shortName:'AA', icon:null};
+        return {id:justificationId, name:'Ausente con aviso', shortName:'AA', icon:'fa-ticket'};
       break;
       case '48773fd7-8502-4079-8ad5-963618abe725':
-        return {id:justificationId, name:'Compensatorio', shortName:'C', icon:null};
+        return {id:justificationId, name:'Compensatorio', shortName:'C', icon:'fa-ticket'};
       break;
       case 'fa64fdbd-31b0-42ab-af83-818b3cbecf46':
-        return {id:justificationId, name:'Boleta de Salida', shortName:'BS', icon:null};
+        return {id:justificationId, name:'Boleta de Salida', shortName:'BS', icon:'fa-ticket'};
       break;
       case '4d7bf1d4-9e17-4b95-94ba-4ca81117a4fb':
-        return {id:justificationId, name:'Art 102', shortName:'102', icon:null};
+        return {id:justificationId, name:'Art 102', shortName:'102', icon:'fa-ticket'};
       break;
       case 'b70013e3-389a-46d4-8b98-8e4ab75335d0':
-        return {id:justificationId, name:'Pre-Exámen', shortName:'PE', icon:null};
+        return {id:justificationId, name:'Pre-Exámen', shortName:'PE', icon:'fa-graduation-cap'};
       break;
       case '76bc064a-e8bf-4aa3-9f51-a3c4483a729a':
-        return {id:justificationId, name:'Licencia Anual Ordinaria', shortName:'AO', icon:null};
+        return {id:justificationId, name:'Licencia Anual Ordinaria', shortName:'LAO', icon:'fa-plane'};
       break;
       case '50998530-10dd-4d68-8b4a-a4b7a87f3972':
-        return {id:justificationId, name:'Resolución', shortName:'R', icon:null};
+        return {id:justificationId, name:'Resolución', shortName:'R', icon:'fa-file-text-o'};
       break;
       case 'f9baed8a-a803-4d7f-943e-35c436d5db46':
-        return {id:justificationId, name:'Licencia Médica Corta Duración', shortName:'MCD', icon:null};
+        return {id:justificationId, name:'Licencia Médica Corta Duración', shortName:'MCD', icon:'fa-medkit'};
       break;
       case 'a93d3af3-4079-4e93-a891-91d5d3145155':
-        return {id:justificationId, name:'Licencia Médica Largo Tratamiento', shortName:'MLT', icon:null};
+        return {id:justificationId, name:'Licencia Médica Largo Tratamiento', shortName:'MLT', icon:'fa-medkit'};
       break;
       case 'b80c8c0e-5311-4ad1-94a7-8d294888d770':
-        return {id:justificationId, name:'Licencia Médica Atención Familiar', shortName:'MAF', icon:null};
+        return {id:justificationId, name:'Licencia Médica Atención Familiar', shortName:'MAF', icon:'fa-medkit'};
       break;
       case '478a2e35-51b8-427a-986e-591a9ee449d8':
-        return {id:justificationId, name:'Justificado por Médico', shortName:'JM', icon:null};
+        return {id:justificationId, name:'Justificado por Médico', shortName:'JM', icon:'fa-medkit'};
       break;
       case '5ec903fb-ddaf-4b6c-a2e8-929c77d8256f':
-        return {id:justificationId, name:'Feriado', shortName:'F', icon:null};
+        return {id:justificationId, name:'Feriado', shortName:'F', icon:'fa-calendar'};
       break;
       case '874099dc-42a2-4941-a2e1-17398ba046fc':
-        return {id:justificationId, name:'Paro', shortName:'P', icon:null};
+        return {id:justificationId, name:'Paro', shortName:'P', icon:'fa-calendar'};
       break;
       case 'b309ea53-217d-4d63-add5-80c47eb76820':
-        return {id:justificationId, name:'Cumpleaños', shortName:'CU', icon:null};
+        return {id:justificationId, name:'Cumpleaños', shortName:'CU', icon:'fa-birthday-cake'};
       break;
       case '0cd276aa-6d6b-4752-abe5-9258dbfd6f09':
-        return {id:justificationId, name:'Duelo', shortName:'D', icon:null};
+        return {id:justificationId, name:'Duelo', shortName:'D', icon:'fa-circle'};
       break;
       case 'e8019f0e-5a70-4ef3-922c-7c70c2ce0f8b':
-        return {id:justificationId, name:'Donación de Sangre', shortName:'DS', icon:null};
+        return {id:justificationId, name:'Donación de Sangre', shortName:'DS', icon:'fa-tint'};
       break;
       default:
-        return {id:justificationId, name:'No definido', shortName:'-', icon:null};
+        return {id:justificationId, name:'No definido', shortName:'-', icon:'fa-ticket'};
     }
   };
-  
+
 
 });
