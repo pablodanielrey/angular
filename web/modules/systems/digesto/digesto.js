ac19910f03295e0dc897b4ab382c4ee912ed031b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-05-12 21:58:13 -0300

    hicismos alguns pruebas

---
+++ web/modules/systems/digesto/digesto.js
@@ -0,0 +1,25 @@
+
+
+var app = angular.module('digestoApp',[]);
+
+app.controller('MenuCtrl', function($scope) {
+
+  $scope.mv = false;
+
+  $scope.menuVisible = function() {
+    return $scope.mv;
+  };
+
+  $scope.toogleMenu = function() {
+    $scope.mv = !$scope.mv;
+  };
+
+});
+
+app.controller('DigestoCtrl',function($scope) {
+
+  $scope.mostrarMenu = function() {
+    alert('jano jano');
+  }
+
+});
