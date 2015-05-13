b8f0e658591508917ab16f5cdd870ab789aa9bdb
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-05-13 16:36:32 -0300

    agregue un ejemplo del comando

--- firmware/pc/main.py
+++ firmware/pc/main.py
@@ -2,5 +2,13 @@
 
 import serial
 
+#ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+
+def getHex2Bytes(h):
+    return h.to_bytes(2,byteorder='little')
+
+
+cmd = getHex2Bytes(0xaa55) + getHex2Bytes(0x124)
+
+print('{}'.format(cmd))
