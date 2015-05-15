ea7a585f2487e16705bf5fd53cb42f86fbeeb5cb
ivancas84 <ivancas84@gmail.com>
2015-05-15 10:29:12 -0300

    quitar on blur

--- web/modules/systems/assistance/requestAuthority.js
+++ web/modules/systems/assistance/requestAuthority.js
@@ -205,14 +205,7 @@ app.controller('RequestAuthorityCtrl', ["$scope", "$timeout", "$window", "Assist
     $scope.model.displayListUser = false;
   };
   
-  /**
-   * Mostrar lista de usuarios
-   */
-  $scope.restartSearchUser = function(){
-    if($scope.model.user_id === null){
-      $scope.model.searchUser = "";
-    }
-  };
+  
 
 
   /**
