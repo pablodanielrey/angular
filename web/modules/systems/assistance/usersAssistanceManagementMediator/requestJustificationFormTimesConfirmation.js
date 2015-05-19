eb116d84d49f96cdf3563328eec9ea900095d777
Emanuel Pais <emanuel84@gmail.com>
2015-05-19 09:56:56 -0300

    Cambie el metodo que se llamaba desde el cliente, en lugar de llamar al requestJustification llamaba al requestJustificationRange

--- web/modules/systems/assistance/usersAssistanceManagementMediator/requestJustificationFormTimesConfirmation.js
+++ web/modules/systems/assistance/usersAssistanceManagementMediator/requestJustificationFormTimesConfirmation.js
@@ -2,20 +2,20 @@
 app.controller('UsersAssistanceManagementMediatorRequestJustificationFormTimesConfirmationCtrl', ["$scope", "Assistance", "Notifications", "Utils", function($scope, Assistance, Notifications, Utils) {
 
   if(!$scope.model) Notifications.message("No esta definido el modelo");
-  
-  //***** variables del formulario *****  
+
+  //***** variables del formulario *****
   $scope.model.date = null; //definida en controlador hermano
   $scope.model.begin = null; //definida en controlador hermano
   $scope.model.end = null; //definida en controlador hermano
   $scope.model.dateFormated = null; //fecha en formato amigable para el usuario
   $scope.model.beginTimeFormated = null; //fecha en formato amigable para el usuario
   $scope.model.timeFormated = null; //diferencia de tiempo en formato amigable para el usuario
-  $scope.model.endTimeFormated = null; //fecha en formato amigable para el 
-  
+  $scope.model.endTimeFormated = null; //fecha en formato amigable para el
+
   $scope.model.processingRequest = false;
 
-  
-  
+
+
   /******************
    * INICIALIZACION *
    ******************/
@@ -26,7 +26,7 @@ app.controller('UsersAssistanceManagementMediatorRequestJustificationFormTimesCo
     $scope.model.dateFormated = null;
     $scope.model.processingRequest = false;
   };
-  
+
 
   $scope.$on('JustificationsRequestsUpdatedEvent', function(event, data){
     $scope.clearConfirmation();
@@ -36,24 +36,24 @@ app.controller('UsersAssistanceManagementMediatorRequestJustificationFormTimesCo
 	$scope.$on('JustificationStatusChangedEvent', function(event, data) {
     $scope.clearConfirmation();
 	});
-  
-  
+
+
   $scope.$watch('model.selectedUser', function() {
       $scope.clearConfirmation();
-  }); 
-  
+  });
+
   $scope.$watch('model.justificationSelectedId', function() {
     $scope.clearConfirmation();
   });
-  
-  
+
+
   $scope.$watch('model.date', function() {
     $scope.model.dateFormated = null;
     if($scope.model.date !== null){
 			$scope.model.dateFormated = Utils.formatDate($scope.model.date);
     }
-  }); 
-  
+  });
+
   $scope.$watch('model.begin', function() {
     $scope.model.beginTimeFormated = null;
     if($scope.model.begin !== null){
@@ -61,7 +61,7 @@ app.controller('UsersAssistanceManagementMediatorRequestJustificationFormTimesCo
     }
     $scope.defineTimeFormated();
   });
-  
+
   $scope.$watch('model.end', function() {
     $scope.model.endTimeFormated = null;
     if($scope.model.end !== null){
@@ -69,33 +69,33 @@ app.controller('UsersAssistanceManagementMediatorRequestJustificationFormTimesCo
     }
     $scope.defineTimeFormated();
   });
-  
+
   $scope.defineTimeFormated = function(){
     if ($scope.model.begin && $scope.model.end) $scope.model.timeFormated = Utils.getDifferenceTimeFromDates($scope.model.begin, $scope.model.end);
   }
-  
+
   $scope.isDatesDefined = function(){
-    return ($scope.model.beginTimeFormated !== null && $scope.model.endTimeFormated !== null && $scope.model.dateFormated !== null);    
+    return ($scope.model.beginTimeFormated !== null && $scope.model.endTimeFormated !== null && $scope.model.dateFormated !== null);
   };
-  
-  
+
+
  $scope.save = function() {
-   
+
    $scope.model.processingRequest = true;
-   
+
     var request = {
 			id:$scope.justification.id,
 			begin:new Date($scope.model.date),
       end:new Date($scope.model.date),
-		};    
-    
+		};
+
     request.begin.setHours($scope.model.begin.getHours(), $scope.model.begin.getMinutes());
     request.end.setHours($scope.model.end.getHours(), $scope.model.end.getMinutes());
-    
+
     console.log(request);
     console.log($scope.model.selectedUser);
 
-    Assistance.requestJustificationRange($scope.model.selectedUser.id, request, "APPROVED",
+    Assistance.requestJustification($scope.model.selectedUser.id, request, "APPROVED",
 			function(ok) {
 				$scope.clearConfirmation(); //limpiar contenido
         $scope.clearSections();
@@ -110,8 +110,8 @@ app.controller('UsersAssistanceManagementMediatorRequestJustificationFormTimesCo
 			}
 		);
   };
-  
-  
+
+
 
 
 
