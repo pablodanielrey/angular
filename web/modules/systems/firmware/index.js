a82a7e393babdfa784bea5e55c35a5db9d298841
Emanuel Pais <emanuel84@gmail.com>
2015-05-29 09:23:36 -0300

    Termine el ingresar el codigo Agregue el display password

--- web/modules/systems/firmware/index.js
+++ web/modules/systems/firmware/index.js
@@ -5,13 +5,30 @@ app.controller("IndexCtrl", ['$scope','$timeout','$location','Notifications', 'F
   $scope.model = {
     date:new Date(),
     day:'',
-    hours:''
+    hours:'',
+    displayCodeContainer: false,
+    displayInfoContainer: true,
+    enabledCommit: false,
+    code: '',
+    displayInputCode: false,
+    displayPassword: false
   }
 
   $scope.initialize = function() {
     $scope.updateDate();
     $scope.updateDay();
+    $scope.model.displayCodeContainer = false;
+    $scope.model.displayInfoContainer = true;
+    $scope.model.enabledCommit = false;
+    $scope.model.code = '';
+    $scope.model.displayInputCode = false;
+    $scope.model.displayPassword = false;
   }
+
+  /* ----------------------------------------------------------
+   * ------------- ACTUALIZACION DEL RELOJ --------------------
+   * ----------------------------------------------------------
+   */
 
   $scope.updateDay = function() {
     var options = {
@@ -31,14 +48,6 @@ app.controller("IndexCtrl", ['$scope','$timeout','$location','Notifications', 'F
     $scope.model.hours = hs + ":" + min + ":" + sec;
   }
 
-  $scope.redirect = function() {
-    $location.path('/enroll')
-  }
-
-  $scope.enterCode = function() {
-    $scope.redirect();
-  }
-
   $scope.updateDate = function() {
     var day = $scope.model.date.getDay();
 
@@ -54,6 +63,92 @@ app.controller("IndexCtrl", ['$scope','$timeout','$location','Notifications', 'F
       $scope.updateDate();
     }, 1000);
   }
+
+
+  /* ----------------------------------------------------------
+   * ---------------- Ingreso del codigo ----------------------
+   * ----------------------------------------------------------
+   */
+
+  $scope.redirect = function() {
+    $location.path('/enroll')
+  }
+
+
+  $scope.enterCode = function() {
+    $scope.model.displayInputCode = false;
+    $scope.model.displayPassword = true;
+  }
+
+  $scope.$watch('model.code', function(newValue, oldValue) {
+
+    var isZero = newValue == '0' && (oldValue == null || oldValue.trim() == '');
+    var isNotNumber = isNaN(newValue);
+    if (isZero || isNotNumber || newValue.indexOf('.') > -1) {
+      $scope.model.code = oldValue;
+    }
+
+    if ($scope.model.code == null || $scope.model.code.trim() == '') {
+      $scope.model.enabledCommit = false;
+    } else {
+      $scope.model.enabledCommit = true;
+    }
+   });
+
+  $scope.addNumber = function(number) {
+    if ($scope.model.displayInfoContainer) {
+      $scope.model.displayInfoContainer = false;
+      $scope.model.displayCodeContainer = true;
+      $scope.model.enabledCommit = true;
+      $scope.model.displayInputCode = true;
+      $scope.model.displayPassword = false;
+    }
+
+    if ($scope.model.code == null) {
+        $scope.model.code = '';
+    }
+
+    $scope.model.code += number;
+  }
+
+  $scope.cancel = function() {
+    $scope.model.code = '';
+    $scope.model.displayCodeContainer = false;
+    $scope.model.displayInfoContainer = true;
+    $scope.model.enabledCommit = false;
+    $scope.model.displayInputCode = false;
+    $scope.model.displayPassword = false;
+  }
+
+  $scope.deleteNumber = function() {
+    $scope.model.code = ($scope.model.code == null || $scope.model.code.trim() == '') ? null : $scope.model.code.substring(0, $scope.model.code.length-1);
+  }
+
+  /* ----------------------------------------------------------
+   * ------------- Manejo visual de la pantalla ---------------
+   * ----------------------------------------------------------
+   */
+
+  $scope.isDisplayCodeContainer = function() {
+    return $scope.model.displayCodeContainer;
+  }
+
+  $scope.isDisplayInputCode = function() {
+    return $scope.model.displayInputCode;
+  }
+
+  $scope.isDisplayInputPassword = function() {
+    return $scope.model.displayPassword;
+  }
+
+  $scope.isDisplayInfoContainer = function() {
+    return $scope.model.displayInfoContainer;
+  }
+
+  $scope.isEnabledCommit = function() {
+    return $scope.model.enabledCommit;
+  }
+
 
   $scope.initialize();
 
