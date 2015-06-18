11518c27d8e7fdf979d588016793a46492929ac5
Emanuel Pais <emanuel84@gmail.com>
2015-06-18 20:58:43 -0300

    Termine el agregar usuario y eliminar usuario. Me falta asignarcelo a una oficina

--- web/systems/offices/modules/usersOffices.controller.js
+++ web/systems/offices/modules/usersOffices.controller.js
@@ -11,15 +11,21 @@ function UsersOfficesController($scope, $location, Notifications, Office, Users)
   vm.model = {
     officeSelected: null,
     allOffices: [],
-    users: []
+    users: [],
+    usersOffice: [],
+    officeChange: null
   }
 
   vm.initialize = initialize;
   vm.initalizeAllUsers = initializeAllUsers;
+  vm.initializeUsersOffice = initializeUsersOffice;
   vm.loadOffices = loadOffices;
+  vm.addUser = addUser;
+  vm.removeUser = removeUser;
 
   function initialize() {
     vm.initalizeAllUsers();
+    vm.initializeUsersOffice();
   }
 
   $scope.$on('$viewContentLoaded', function(event) {
@@ -56,11 +62,29 @@ function UsersOfficesController($scope, $location, Notifications, Office, Users)
     Users.findUser(uid,
       function(user) {
         vm.model.users.push(user);
+        var u = getUser(vm.model.usersOffice,user);
+        if (u != null) {
+          user.deleted = true;
+          removeItem(vm.model.usersOffice, u);
+          vm.model.usersOffice.push(user);        
+        }
       },
       function(error) {
         Notification.message(error);
       }
     );
+  }
+
+  function getUser(users,user) {
+    if (users == null || users.length == 0) {
+      return null;
+    }
+
+    for (var i = 0; i < users.length; i++) {
+      if (user.id == users[i].id) {
+        return users[i];
+      }
+    }
   }
 
   function getOfficesIds(offices) {
@@ -114,6 +138,32 @@ function UsersOfficesController($scope, $location, Notifications, Office, Users)
   // ///////// LISTADO DE USUARIOS DE LA OFICINA //////////
   // //////////////////////////////////////////////////////
 
+  function initializeUsersOffice() {
+    usersOffice = [];
+    officeChange = null;
+  }
 
+
+  // //////////////////////////////////////////////////////
+  // /////////// AGREGAR-ELIMINAR USUARIO  ////////////////
+  // //////////////////////////////////////////////////////
+
+  function addUser(user) {
+    user.deleted = true;
+    vm.model.usersOffice.push(user);
+  }
+
+  function removeUser(user) {
+    user.deleted = false;
+    removeItem(vm.model.usersOffice, user);
+  }
+
+
+  function removeItem(array,item) {
+    var index = array.indexOf(item);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
 
 }
