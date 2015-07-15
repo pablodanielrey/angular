a380e8118a80cc3039f46e9411132d125d4af021
ivancas84 <ivancas84@gmail.com>
2015-07-15 13:59:20 -0300

    agregar solucion temporal para armar el arbol

---
+++ web/systems/issues/services/IssueClient.js
@@ -0,0 +1,19 @@
+
+
+app.service('IssueClient', ['Utils','Messages','Session', function(Utils,Messages,Session) {
+	
+  /**
+   * Solicitar un nuevo pedido
+   * @param {Object} request Datos del pedido
+   * @param {function} callbackOk
+   * @param {function} callbackError
+   */
+  this.generateTree = function(data) {
+    
+    
+    
+  };
+
+
+
+}]);
