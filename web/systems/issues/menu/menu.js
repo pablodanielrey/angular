f64f10a1f589b3de4f7e771119fd753c37319091
Walter Blanco <walter@creativehands.com.ar>
2015-07-15 13:04:04 -0300

    continuo trabajando en la visual del sistema

--- web/systems/issues/menu/menu.js
+++ web/systems/issues/menu/menu.js
@@ -5,8 +5,8 @@ app.controller('MenuCtrl', ['$scope', function ($scope) {
   $scope.model = {
     class:'',
     items: [
-      { name: 'Crear Norma',img:'fa fa-file-text', url: '#/load' },
-      { name: 'Buscar Norma',img:'fa fa fa-search', url: '#/search' },
+      { name: 'Crear Pedido',img:'fa fa-tag', url: '#/load' },
+      { name: 'Pedidos',img:'fa fa-tags', url: '#/search' },
     ],
     cerrado : false
   };
