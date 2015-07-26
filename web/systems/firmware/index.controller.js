3f0bd6be60a1ff3e7ac9d05245347995c432b4a9
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-07-26 14:55:42 -0300

    movi la definicion del modulo de angular a los archivos correctos

--- web/systems/firmware/index.controller.js
+++ web/systems/firmware/index.controller.js
@@ -1,49 +1,10 @@
 angular
-    .module('mainApp',['ngRoute','vxWamp'])
+    .module('mainApp')
     .controller('IndexController',IndexController)
-    .config(function($wampProvider) {
-
-      var conn = {
-        url: config_firmware.url,
-        realm: config_firmware.realm
-      };
-      console.log(conn);
-      $wampProvider.init(conn);
-    });
-    /*
-    .run(function($wamp) {
-      $wamp.open();
-    });
-    */
 
 IndexController.$inject = ['$rootScope','$scope','$location','$timeout','$wamp'];
 
 function IndexController($rootScope,$scope,$location,$timeout,$wamp) {
-
-  // mensajes que vienen del socket. solo me interesan los eventos, las respuestas son procesadas por otro lado.
-  $rootScope.$on('onSocketMessage', function(event, data) {
-
-    var response = JSON.parse(data);
-
-    // tiene que tener tipo si o si.
-    if (response.type == undefined) {
-      return;
-    }
-    $rootScope.$broadcast(response.type,response.data);
-  });
-
-
-  // cambia la url de la pagina en base al evento.
-  $rootScope.$on('routeEvent', function(event, data) {
-    $location.path(data);
-  });
-
-
-  /*
-  $timeout(function() {
-    WebSocket.registerHandlers();
-  },0);
-*/
 
   $scope.$on('$viewContentLoaded', function(event) {
     $wamp.open();
