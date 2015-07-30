f6f3202eec193285dd4075456f4e8e7c5c71e477
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-07-30 15:33:27 -0300

    confgure el injector dentro del scope global, si no las otras funciones y objetos me tiran que no esta definido

--- firmware/camabio/mainFirmware.py
+++ firmware/camabio/mainFirmware.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+import sys, logging, inject
+sys.path.insert(0,'../../python')
+
+from model.config import Config
+
+logging.basicConfig(level=logging.DEBUG)
+
+def config_injector(binder):
+    binder.bind(Config,Config('firmware-config.cfg'))
+
+inject.configure(config_injector)
+config = inject.instance(Config)
 
 
 '''
@@ -6,20 +18,11 @@
 '''
 if __name__ == '__main__':
 
-    import sys, logging, inject
-    sys.path.insert(0,'../../python')
-
-    logging.basicConfig(level=logging.DEBUG)
-
     from autobahn.asyncio.wamp import ApplicationRunner
     from network.wampFirmware import WampFirmware
-    from model.config import Config
 
-    def config_injector(binder):
-        binder.bind(Config,Config('firmware-config.cfg'))
 
-    inject.configure(config_injector)
-    config = inject.instance(Config)
+
 
     url = config.configs['firmware_url']
     realm = config.configs['firmware_realm']
