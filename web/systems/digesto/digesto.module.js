52dd97acf907d851d55568886a1d715e13d1fd24
Emanuel Pais <emanuel84@gmail.com>
2015-08-10 17:41:25 -0300

    commit parcial

--- web/systems/digesto/digesto.module.js
+++ web/systems/digesto/digesto.module.js
@@ -1,15 +1,23 @@
 angular
     .module('mainApp',['ngRoute','vxWamp'])
     .config(function($wampProvider) {
-      var conn = {
-        url: config_digesto.url,
-        realm: config_digesto.realm
-      };
-      console.log(conn);
-      $wampProvider.init(conn);
-    });
-    /*
+      if (config.url == undefined || config.url == 'autodetect') {
+        console.log(config);
+        var conn = {
+          url: "ws://" + location.host + ":8000/ws",
+          realm: config.realm
+        };
+        console.log(conn);
+        $wampProvider.init(conn);
+      } else {
+        var conn = {
+          url: config.url,
+          realm: config.realm
+        };
+        console.log(conn);
+        $wampProvider.init(conn);
+      }
+    })
     .run(function($wamp) {
       $wamp.open();
     });
-    */
