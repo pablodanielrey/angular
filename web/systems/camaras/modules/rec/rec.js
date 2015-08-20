fc6272f39c7b4861b1d442143021816af9d4fd8e
Walter Blanco <walter@creativehands.com.ar>
2015-08-20 12:33:21 -0300

    se agregaron botones de funcionalidad en los videos grabados

--- web/systems/camaras/modules/rec/rec.js
+++ web/systems/camaras/modules/rec/rec.js
@@ -4,7 +4,8 @@ app.controller('rec', function($scope,$timeout) {
 
     $scope.model = {
       class:'',
-      contentRecordings:false
+      contentRecordings:false,
+      rate: 1
     };
 
 
@@ -12,5 +13,51 @@ app.controller('rec', function($scope,$timeout) {
       $scope.model.contentRecordings = false;
     });
     // console.log('hola');
+
+    $scope.pause = function() {
+      $scope.model.rate = 1;
+      var video = document.getElementById("video");
+      video.playbackRate = $scope.model.rate;
+      //video.play();
+    }
+
+    $scope.faster = function() {
+      $scope.model.rate = $scope.model.rate + 0.5;
+      var video = document.getElementById("video");
+      video.playbackRate = $scope.model.rate;
+
+      if ($scope.model.rate > 0) {
+        video.play();
+      }
+    }
+
+    $scope.slower = function() {
+      var video = document.getElementById("video");
+      $scope.model.rate = $scope.model.rate - 0.5;
+
+      if ($scope.model.rate < 0) {
+        $scope.model.rate = 0;
+        video.pause();
+      }
+
+      video.playbackRate = $scope.model.rate;
+    }
+
+    $scope.seekForward = function() {
+      $scope.model.rate = 1;
+      var video = document.getElementById("video");
+      video.currentTime = video.currentTime + 1;
+      video.playbackRate = $scope.model.rate;
+      video.pause();
+    }
+
+    $scope.seekBackwards = function() {
+      $scope.model.rate = 1;
+      var video = document.getElementById("video");
+      video.currentTime = video.currentTime - 1;
+      video.playbackRate = $scope.model.rate;
+      video.pause();
+    }
+
   }
 );
