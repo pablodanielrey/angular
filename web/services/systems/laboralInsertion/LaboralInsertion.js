f93c4a7d6423702f41cba7db7f4d5543ed58c6bb
ivancas84 <ivancas84@gmail.com>
2015-08-26 13:02:25 -0300

    incorporar metodo de consulta al server para probar la nueva estructura

--- web/services/systems/laboralInsertion/LaboralInsertion.js
+++ web/services/systems/laboralInsertion/LaboralInsertion.js
@@ -7,4 +7,17 @@ LaboralInsertion.inject = ['$rootScope','$wamp','Session']
 
 function LaboralInsertion($rootScope,$wamp,Session) {
 
+  this.find = function(userId, cok, cerr) {
+    var sessionId = Session.getSessionId();
+    
+    $wamp.call('system.laboralInsertion.find',[userId])
+      .then(function(data) {
+        cok(data);
+      },function(err) {
+        cerr(err);
+      }
+    );
+  }
+
+
 }
