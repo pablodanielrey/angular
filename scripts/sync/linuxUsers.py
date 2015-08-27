45e0260dd4efe949a50ba3c0d6d663dcd4492123
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-08-27 17:59:28 -0300

    agregue para que lea los archivos actuales y agregue solo las personas que no existen

--- scripts/sync/linuxUsers.py
+++ scripts/sync/linuxUsers.py
@@ -18,16 +18,68 @@ if __name__ == '__main__':
     con = psycopg2.connect(host=host, port=port, user=user, password=dbpassword, dbname=db)
     try:
         cur = con.cursor()
+        cur.execute('select username from credentials.user_password up, domain.users du where up.user_id = du.id')
+        if cur.rowcount <= 0:
+            sys.exit()
+
+        existent = []
+        for username in cur:
+            existent.append(username[0])
+
+
+        linesp = []
+        f = open('/etc/passwd','r')
+        try:
+            for l in f:
+                u = l.split(':')[0]
+                if u not in existent:
+                    linesp.append(l)
+
+        finally:
+            f.close()
+
+        linesg = []
+        f2 = open('/etc/group', 'r')
+        try:
+            for l in f2:
+                u = l.split(':')[0]
+                if u not in existent:
+                    linesg.append(l)
+
+        finally:
+            f2.close()
+
+        liness = []
+        f3 = open('/etc/shadow', 'r')
+        try:
+            for l in f3:
+                u = l.split(':')[0]
+                if u not in existent:
+                    liness.append(l)
+
+        finally:
+            f3.close()
+
         cur.execute('select username,password from credentials.user_password up, domain.users du where up.user_id = du.id')
         if cur.rowcount <= 0:
             sys.exit()
 
+
         uid = 1000
         gid = 1000
-        f = open('/tmp/passwd', 'w')
-        f2 = open('/tmp/group', 'w')
-        f3 = open('/tmp/shadow', 'w')
+        f = open('/etc/passwd', 'w')
+        f2 = open('/etc/group', 'w')
+        f3 = open('/etc/shadow', 'w')
         try:
+            for l in linesp:
+                f.write(l)
+
+            for l in linesg:
+                f2.write(l)
+
+            for l in liness:
+                f3.write(l)
+
             for (username, password) in cur:
                 f.write('{0}:x:{1}:{2}:{0}:/home/{0}:/bin/bash\n'.format(username, uid, gid))
                 f2.write('{0}:x:{1}:{0}\n'.format(username, gid))
