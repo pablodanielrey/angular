72d887c062a7233ba2b4a41a325d44203cee8dff
ivancas84 <ivancas84@gmail.com>
2015-08-31 12:22:57 -0300

    desarrollo de controlador

---
+++ web/systems/laboralInsertion/modules/registration/registration.controller.js
@@ -0,0 +1,38 @@
+angular
+  .module('mainApp')
+  .service('Registration',Registration);
+
+Registration.inject = ['$rootScope','$wamp','Session']
+
+
+function Registration($rootScope,$wamp,Session) {
+
+  $scope.status = {
+  
+  };
+  
+  
+  $scope.initialize = function(){
+    
+  
+  };
+  
+  $scope.submit = function(){
+  };
+  
+  $scope.process = function(){
+  };
+  
+  
+  $scope.$on('$viewContentLoaded', function(event) {
+		$scope.initialize();
+	});
+
+  
+  
+};
+
+
+
+
+  
