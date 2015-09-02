c87a7d2182ed00b5b543007362b9e310586f3281
ivancas84 <ivancas84@gmail.com>
2015-09-02 11:30:53 -0300

    agregar ultimos cambios al main

--- web/systems/vacation/main.py
+++ web/systems/vacation/main.py
@@ -39,24 +39,19 @@ for userId in usersId:
 begin = datetime.datetime(2015, 8, 31)
 future = datetime.datetime(2020, 8, 31)
     
-    
-justificationRequests = justification.getJustificationRequestsByDate(con, ['APPROVED'], usersId, begin, future)
-
 
 for userId in usersId:
     stock = justification.getJustificationStock(con, userId, '76bc064a-e8bf-4aa3-9f51-a3c4483a729a', begin)
     if stock > 0:
         userData = users.findUser(con,userId)
-        print(userData["name"] + ' ' + userData["lastname"] + ' ' +  str(stock))
+        print(userId + ' ' + userData["name"] + ' ' + userData["lastname"] + ' ' +  str(stock))
 
         
-    
-    
-    
+print("")
     
 
+justificationRequests = justification.getJustificationRequestsByDate(con, ['APPROVED'], usersId, begin, future)
 
-"""
 laoByUsers = {}
 for justificationRequest in justificationRequests:
     if justificationRequest["justification_id"] == '76bc064a-e8bf-4aa3-9f51-a3c4483a729a':
@@ -73,8 +68,26 @@ for userId in laoByUsers:
     print(userData["name"] + ' ' + userData["lastname"] + ' ' + str(laoByUsers[userId]))
 
 
-#print(justificationRequests)
+print("")
+
 """
+firstDayOfYear = datetime.datetime(2015, 1, 1)
+justificationRequestsUser = justification.getJustificationRequestsByDate(con, ['APPROVED'], ['c3f23c7c-4abf-402e-b6f3-ee3fd70f4484'], firstDayOfYear, future)
+count = 0
+for justificationRequestUser in justificationRequestsUser:
+    if justificationRequestUser["justification_id"] == '76bc064a-e8bf-4aa3-9f51-a3c4483a729a':
+        count += 1
+        print(justificationRequestUser)
+        print()
+     
+print(count)
+"""
+
+
+
+
+
+
 
 
     
