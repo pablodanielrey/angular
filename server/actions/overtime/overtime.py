b12ee2176a1bd13b009157c7c07870c3431c9ca5
ivancas84 <ivancas84@gmail.com>
2015-09-15 11:19:17 -0300

    agregar script para consultar requerimientos de horas extras a administrar

--- server/actions/overtime/overtime.py
+++ server/actions/overtime/overtime.py
@@ -35,6 +35,8 @@ class OvertimeWamp(ApplicationSession):
     def onJoin(self, details):
         logging.debug('registering methods')
         yield from self.register(self.getOvertimeRequests_async, 'overtime.getOvertimeRequests')
+        yield from self.register(self.getOvertimeRequestsToManage_async, 'overtime.getOvertimeRequestsToManage')
+
 
 
     def _getDatabase(self):
@@ -46,7 +48,7 @@ class OvertimeWamp(ApplicationSession):
 
 
     '''
-     ' Obtener requerimiento de overtime a partir de una una lista de usuarios
+     ' Obtener requerimientos de horas extra
      ' @param usersIds Lista de usuarios, si es una lista vacia retorna todos los usuarios
      ' @param states Lista de estados, si es una lista vacia retorna todos los estados
      '      [APPROVED, PENDING, REJECTED]'
@@ -64,4 +66,28 @@ class OvertimeWamp(ApplicationSession):
     def getOvertimeRequests_async(self, usersIds, states):
         loop = asyncio.get_event_loop()
         r = yield from loop.run_in_executor(None, self.getOvertimeRequests, usersIds, states)
+        return r
+
+
+    '''
+     ' Obtener requerimientos de horas extra para administrar
+     ' @param usersId Id de usuario
+     ' @param states Lista con el estado estado de las solicitudes a consultar
+     '      [APPROVED, PENDING, REJECTED]'
+     ' @param group Grupo (Oficinas)
+     '      ROOT|TREE --> ROOT = oficinas directas, TREE = oficinas directas y todas las hijas
+     '''
+    def getOvertimeRequestsToManage(self, userId, states, group):
+        con = self._getDatabase()
+        try:            
+            requests = self.overtime.getOvertimeRequestsToManage(con, userId, states, group)
+            return requests
+
+        finally:
+            con.close()
+
+    @coroutine
+    def getOvertimeRequestsToManage_async(self,userId, states=[], group='ROOT'):
+        loop = asyncio.get_event_loop()
+        r = yield from loop.run_in_executor(None, self.getOvertimeRequestsToManage, userId, states, group)
         return r
