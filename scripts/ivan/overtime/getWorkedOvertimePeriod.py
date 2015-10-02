3997a3d74d7a8447298d25ff98fb65120291d02e
ivancas84 <ivancas84@gmail.com>
2015-10-02 12:00:56 -0300

    redefinir getWorkedOvertimePeriod para que recorra los usuarios e imprima los segundos de horas extra realizadas

--- scripts/ivan/overtime/getWorkedOvertimePeriod.py
+++ scripts/ivan/overtime/getWorkedOvertimePeriod.py
@@ -5,15 +5,15 @@ import datetime
 sys.path.insert(0,'../../../python')
 
 from model.config import Config
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 
 from autobahn.asyncio.wamp import ApplicationSession
 from asyncio import coroutine
 
 
 '''
-python3 getWorkedOvertimePeriod.py userId date1 date2
-python3 getWorkedOvertimePeriod.py e43e5ded-e271-4422-8e85-9f1bc0a61235 "01/01/2015" "31/12/2015"
+python3 getWorkedOvertimePeriod.py date1 date2 
+python3 getWorkedOvertimePeriod.py "01/01/2015" "31/12/2015"
 
 '''
 
@@ -33,38 +33,53 @@ class WampMain(ApplicationSession):
 
     @coroutine
     def onJoin(self, details):
-        logging.info("********** MINUTOS TRABAJADOS DE REQUERIMIENTOS DE HORAS EXTRAS APROBADAS EN UN PERIODO **********")
+        print("********** MINUTOS TRABAJADOS DE REQUERIMIENTOS DE HORAS EXTRAS APROBADAS EN UN PERIODO **********")
 
-        if len(sys.argv) < 4:
+        if len(sys.argv) < 3:
             sys.exit("Error de parámetros")
-
-        userId = sys.argv[1]
         
-        dateAux = sys.argv[2]
+        dateAux = sys.argv[1]
         date1 = datetime.datetime.strptime(dateAux, "%d/%m/%Y").date()
 
-        dateAux = sys.argv[3]
+        dateAux = sys.argv[2]
         date2 = datetime.datetime.strptime(dateAux, "%d/%m/%Y").date()
         
-        requests = yield from self.call('overtime.getOvertimeRequests', [userId], ["APPROVED"])
+        requests = yield from self.call('overtime.getOvertimeRequests', [], ["APPROVED"])
         
-        seconds = 0
+        usersId = []
+        
         for request in requests:
-            dateAuxStr = request["begin"]
-            dateAux = dateAuxStr[:10]
-            date = datetime.datetime.strptime(dateAux, "%Y-%m-%d").date()
-
-            if(date >= date1) and (date <= date2):
-                seconds += yield from self.call('overtime.getWorkedOvertime', userId, date)
-
-        print(seconds)
-            
-
-
-        #seconds = yield from self.call('overtime.getWorkedOvertime', userId, date)
+            if request["user_id"] not in usersId:
+                usersId.append(request["user_id"])
         
-        #logging.info(seconds)
+        users = yield from self.call('users.findUsersByIds', usersId)       
 
+
+        toPrint = []
+        for user in users:
+            requests = yield from self.call('overtime.getOvertimeRequests', [user["id"]], ["APPROVED"])
+            
+            seconds = 0
+            if len(requests) == 0:
+                continue;
+                
+            for request in requests:
+                dateAuxStr = request["begin"]
+                dateAux = dateAuxStr[:10]
+                date = datetime.datetime.strptime(dateAux, "%Y-%m-%d").date()
+
+                if(date >= date1) and (date <= date2):
+                    seconds += yield from self.call('overtime.getWorkedOvertime', user["id"], date)
+
+            if seconds > 0:
+                append = user["id"] + ", " + user["name"] + ", " + user["lastname"] + ", " + user["dni"] + ", " + str(seconds)
+                toPrint.append(append)
+        
+
+        for pr in toPrint:
+            print(pr)
+    
+        sys.exit()
       
 if __name__ == '__main__':
 
