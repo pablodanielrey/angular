766f4823ac3e08cbd2cdc5cd7ae196992dd45443
root <root@debian8.econo.unlp.edu.ar>
2015-11-24 17:30:45 -0300

    acomodamos la resolucion de las cam

--- web/systems/camaras/modules/live/live.js
+++ web/systems/camaras/modules/live/live.js
@@ -18,10 +18,10 @@ app.controller('createRegulation', function($scope,$timeout) {
     $scope.$on('$viewContentLoaded', function(event) {
       $scope.model.contentFiltersView = false;
 
-      var player = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c1/v.m3u8", parentId: "#player"});
-      var player2 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c2/v.m3u8", parentId: "#player2"});
-      var player3 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c3/v.m3u8", parentId: "#player3"});
-      var player4 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c4/v.m3u8", parentId: "#player4"});
+      var player = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c1/v.m3u8", parentId: "#player", width: "500", height: "300"});
+      var player2 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c2/v.m3u8", parentId: "#player2", width: "500", height: "300"});
+      var player3 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c3/v.m3u8", parentId: "#player3" , width: "500", height: "300"});
+      var player4 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c4/v.m3u8", parentId: "#player4" , width: "500", height: "300"});
 
     });
 
