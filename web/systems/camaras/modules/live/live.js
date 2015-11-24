5e9c50d800948957c9c8066fe60eeec062f81837
root <root@debian8.econo.unlp.edu.ar>
2015-11-24 16:47:52 -0300

    pablo agrego la nueva forma de reproduccion de las camaras

--- web/systems/camaras/modules/live/live.js
+++ web/systems/camaras/modules/live/live.js
@@ -17,7 +17,15 @@ app.controller('createRegulation', function($scope,$timeout) {
 
     $scope.$on('$viewContentLoaded', function(event) {
       $scope.model.contentFiltersView = false;
+
+      var player = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c1/v.m3u8", parentId: "#player"});
+      var player2 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c2/v.m3u8", parentId: "#player2"});
+      var player3 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c3/v.m3u8", parentId: "#player3"});
+      var player4 = new Clappr.Player({source: "http://camaras.econo.unlp.edu.ar/c4/v.m3u8", parentId: "#player4"});
+
     });
+
+
 
   }
 );
