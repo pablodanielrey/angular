fd1f84537ea0ce35b3b70424e7b11471bce90c04
Emanuel Pais <emanuel84@gmail.com>
2016-02-05 08:15:28 -0300

    agregar impresion de datos y documentacion a los scripts

--- scripts/users/base/addUserPasswordWindows.py
+++ scripts/users/base/addUserPasswordWindows.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 '''
-    Obtiene los usuarios de la base de datos principal que pertenecen a la oficina Cedlas y los sincroniza con el linux actual.
-    Tambien actualiza la clave en el samba
+Agregar usuarios en windows y establecer clave del fce
+Utilizar findAllOffices para consultar los id de oficinas
+
+python3 addUserPasswordWindows.py "officeId"
+
+python3 findAllOffices.py #consultar oficinas para buscar un determinado id
+python3 addUserPasswordWindows.py a645e297-cc8e-43eb-a0a9-ce3484cc80d6
 '''
 
 import connection
@@ -9,9 +14,6 @@ import groups
 import sys
 import users
 import subprocess
-from subprocess import PIPE
-
-
 
 import logging
 logging.getLogger().setLevel(logging.INFO)
@@ -22,18 +24,21 @@ if __name__ == '__main__':
         officeId = sys.argv[1]
 
         office = groups.OfficeDAO.findById(con, officeId)
-      
+
+        if(office.name):
+            print("GRUPO: " + office.name)
+            
         for userId in office.users:
           userInfo = users.UserPasswordDAO.findByUserId(con, userId)
+         
           for userData in userInfo:
               if userData.username and userData.password:
+                  print("USUARIO: " + userData.username)
                   cmd = "NET user " + userData.username + " " + userData.password + " /add /passwordreq:yes" 
                   if(sys.version_info.major == 3) and (sys.version_info.minor > 4):
                     cp = subprocess.run(cmd, shell=True)
                   else:
                     cp = subprocess.call(cmd, shell=True)
-                    
-                  logging.info(cp)
 
     finally:
         connection.closeConnection(con)
