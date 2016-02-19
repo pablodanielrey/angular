0ef46ea850351e896efd9e0b35162f8d11d34b4e
Walter Blanco <walter@creativehands.com.ar>
2016-02-19 18:22:22 -0300

    empece con el sistema de administrar usuarios

---
+++ web/systems/account/modules/editAccounts/index.controller.js
@@ -0,0 +1,8 @@
+var app = angular.module('mainApp');
+
+app.controller('EditAccountsCtrl', ["$rootScope", '$scope',
+
+  function ($rootScope, $scope) {
+    console.log("EditAccountsCtrl");
+  }
+]);
