b5d435397491956e825d1d1307884152415d9d9e
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-02-20 15:06:36 -0300

    corregi el import Users

--- scripts/users/base/importUsers.py
+++ scripts/users/base/importUsers.py
@@ -8,14 +8,25 @@
 '''
 
 import sys
+import inject
+from connection.connection import Connection
 
 if __name__ == '__main__':
 
     print(sys.path)
 
-    import createUser
-    import csv
-    reader = csv.reader(sys.stdin)
-    for name, lastname, dni in reader:
-        print('\n\n{} {} {}'.format(name,lastname,dni))
-        createUser.createUser(dni, name, lastname)
+    inject.configure()
+    conn = inject.instance(Connection)
+    con = conn.get()
+    try:
+        import createUser
+        import csv
+        reader = csv.reader(sys.stdin)
+        for name, lastname, dni in reader:
+            print('\n\n{} {} {}'.format(name,lastname,dni))
+            createUser.createUser(con, dni, name, lastname)
+
+        con.commit()
+
+    finally:
+        conn.put(con)
