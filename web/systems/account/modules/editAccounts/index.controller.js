68c59a138467fb4cc97779a66d7d5cd3769255bd
Walter Blanco <walter@creativehands.com.ar>
2016-02-24 16:54:55 -0300

    modifique el estilo de la pantalla de busqueda de usuarios y cree la pantalla de crear usuarios

--- web/systems/account/modules/editAccounts/index.controller.js
+++ web/systems/account/modules/editAccounts/index.controller.js
@@ -2,7 +2,9 @@ var app = angular.module('mainApp');
 
 app.controller('EditAccountsCtrl', ["$rootScope", '$scope',
 
+
   function ($rootScope, $scope) {
     console.log("EditAccountsCtrl");
   }
+
 ]);
