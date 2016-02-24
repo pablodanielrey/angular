8a4118e7ac5820e2c8f670f9258b0fa009c8ad69
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-02-24 17:36:08 -0300

    cree la entidad y el DAO

--- python/model/laboralinsertion/inscription.py
+++ python/model/laboralinsertion/inscription.py
@@ -4,14 +4,16 @@ class Inscription:
 
     def __init__(self):
         self.id = None
-        self.user_id = None
+        self.userId = None
         self.degree = None
         self.workType = None
         self.reside = False
         self.travel = False
         self.workExperience = False
         self.created = None
-
+        self.average1 = 0
+        self.average2 = 0
+        self.approved = 0
 
 class InscriptionDAO:
 
@@ -19,8 +21,17 @@ class InscriptionDAO:
     def _fromResult(r):
         i = Inscription()
         i.id = r['id']
-        i.user_id = r['user_id']
-        i.
+        i.userId = r['user_id']
+        i.degree = r['degree']
+        i.workType = r['work_type']
+        i.reside = r['reside']
+        i.travel = r['travel']
+        i.workExperience = r['work_experience']
+        i.created = r['created']
+        i.average1 = r['average1']
+        i.average2 = r['average2']
+        i.approved = r['approved']
+        return i
 
     @staticmethod
     def persist(con, inscription):
@@ -30,11 +41,11 @@ class InscriptionDAO:
             if inscription.id is None:
                 inscription.id = str(uuid.uuid4())
                 ins = inscription.__dict__
-                cur.execute('insert into laboral_insertion.inscriptions (id, user_id, degree, work_type, reside, travel, work_experience) values '
-                            '(%(id)s, %(userId)s, %(degree)s, %(workType)s, %(reside)s, %(travel)s, %(workExperience)s)', ins)
+                cur.execute('insert into laboral_insertion.inscriptions (id, user_id, degree, work_type, reside, travel, work_experience, average1, average2, approved) values '
+                            '(%(id)s, %(userId)s, %(degree)s, %(workType)s, %(reside)s, %(travel)s, %(workExperience)s, %(average1)s, %(average2)s, %(approved)s )', ins)
             else:
                 cur.execute('update laboral_insertion.inscriptions (user_id = %(userId)s, degree = %(degree)s, work_type = %(workType)s, '
-                            'reside = %(reside)s, travel = %(travel)s) where id = %(id)s', ins)
+                            'reside = %(reside)s, travel = %(travel)s, average1 = %(average1)s, average2 = %(average2)s, approved = %(approved)s) where id = %(id)s', ins)
 
         finally:
             cur.close()
@@ -63,16 +74,3 @@ class InscriptionDAO:
 
         finally:
             cur.close()
-
-
-class Averages:
-    ''' Promedios definidos para las carreras de las personas '''
-
-    def __init__(self):
-        self.id = None
-        self.userId = None
-        self.degree = None
-        self.average1 = 0
-        self.average2 = 0
-        self.approved = 0
-        self.created = None
