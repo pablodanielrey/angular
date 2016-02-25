b6d4f351cd59eee04777c0c01882634973e8cac6
Emanuel Pais <emanuel84@gmail.com>
2016-02-25 10:35:41 -0300

    Implemente el control del filtro de fecha, el cual deberia ser igual para todos

--- web/systems/laboralInsertion/modules/search/filters.controller.js
+++ web/systems/laboralInsertion/modules/search/filters.controller.js
@@ -7,17 +7,27 @@ FiltersCtrl.$inject = ['$rootScope','$scope'];
 function FiltersCtrl($rootScope, $scope) {
 
   $scope.model = {
-    filters: []
+    filters: [],
+    selectFilterDate: null
   }
 
   $scope.view = {
     degrees:[
-      {name: 'Seleccionar ...', value:'empty'},
-      {name:'Contador Público',value:'contador'},
-      {name: 'Lic. en Economía', value:'economia'},
-      {name: 'Lic. en Administración', value:'administracion'}
+      {type:"filterDegree", name: 'Seleccionar ...', value:'empty'},
+      {type:"filterDegree", name:'Contador Público',value:'contador'},
+      {type:"filterDegree", name: 'Lic. en Economía', value:'economia'},
+      {type:"filterDegree", name: 'Lic. en Administración', value:'administracion'}
+    ],
+    filtersDate: [
+      {type:"filterDate",descriptionType:'Fecha de Inscripción', name:"2015", value:"2015"},
+      {type:"filterDate",descriptionType:'Fecha de Inscripción', name:"2014", value:"2014"},
+      {type:"filterDate",descriptionType:'Fecha de Inscripción', name:"2013", value:"2013"},
+      {type:"filterDate",descriptionType:'Fecha de Inscripción', name:"2012", value:"2012"},
+      {type:"filterDate",descriptionType:'Fecha de Inscripción', name:"2011", value:"2011"}
     ]
   }
+
+
 
   /*
     filter {
@@ -26,6 +36,22 @@ function FiltersCtrl($rootScope, $scope) {
     }
   */
 
-  console.log("hola");
+  $scope.addDateFilter = addDateFilter;
+  $scope.removeFilter = removeFilter;
+
+  function addDateFilter() {
+    $scope.model.filters.push($scope.model.selectFilterDate);
+    var i = $scope.view.filtersDate.indexOf($scope.model.selectFilterDate);
+    $scope.view.filtersDate.splice(i,1);
+  }
+
+  function removeFilter(filter) {
+
+    switch (filter.type) {
+      case "filterDate": $scope.view.filtersDate.push(filter); break;
+    }
+    var i = $scope.model.filters.indexOf(filter);
+    $scope.model.filters.splice(i,1);    
+  }
 
 }
