90d7f504a2c36f21eeaa1d0f84c6703b87203aba
Walter Blanco <walter@creativehands.com.ar>
2016-02-26 12:10:14 -0300

    cree el sistema de tutorias

---
+++ web/systems/tutorias/modules/createTutoring/index.controller.js
@@ -0,0 +1,10 @@
+var app = angular.module('mainApp');
+
+app.controller('CreateTutoring', ["$rootScope", '$scope',
+
+
+  function ($rootScope, $scope) {
+    console.log("CreateTutoringCtrl");
+  }
+
+]);
