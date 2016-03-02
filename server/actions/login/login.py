e4df509ad5f28d8e6527e8c5e3455d3548b705cb
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-03-02 18:27:14 -0300

    otro error de improt

--- server/actions/login/login.py
+++ server/actions/login/login.py
@@ -5,7 +5,7 @@ import logging
 from model.login.login import Login
 from model.login.session import SessionDAO
 from model.registry import Registry
-import model.connection
+from model.connection import connection
 
 import asyncio
 from asyncio import coroutine
