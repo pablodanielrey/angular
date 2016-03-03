97c3d4685222fbdce6970c42c5c441b76a53138a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-03-02 21:00:18 -0300

    saque el main que ya no se usa para nada

--- server/ejecutar-insercionlaboral.sh
+++ server/ejecutar-insercionlaboral.sh
@@ -25,4 +25,4 @@ screen -S a5 -d -m python3 mainFiles.py
 screen -S a4 -d -m python3 mainLaboralInsertion.py
 #screen -S a3 -d -m python3 mainFirmware.py
 screen -S a2 -d -m python3 mainLogin.py
-screen -S a1 -d -m python3 main.py
+#screen -S a1 -d -m python3 main.py
