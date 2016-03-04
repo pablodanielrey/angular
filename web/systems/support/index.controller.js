7f7a422af638793ad68a5aba2b94e96d929ad82f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-03-04 15:22:27 -0300

    agegamos el modulo de support

---
+++ web/systems/support/index.controller.js
@@ -0,0 +1,40 @@
+
+app.controller('IndexCtrl',IndexCtrl);
+
+IndexCtrl.$inject = ['$rootScope','$scope', '$window', 'Notifications', 'Login'];
+
+function IndexCtrl($rootScope, $scope, $window, Notifications, Login) {
+
+    $scope.model = {
+      hideMenu: false
+    };
+
+    $scope.hideMenu = function() {
+      return $scope.model.hideMenu;
+    }
+
+    $scope.initialize = initialize;
+
+    function initialize() {
+
+      if (!Login.isLogged()) {
+        $window.location.href = '/systems/login/index.html';
+      }
+
+
+        /*Login.validateSession(
+          function(v) {
+            if (!v) {
+              $window.location.href = "/systems/login/index.html";
+            }
+          },
+          function(err) {
+            Notifications.message(err);
+        })*/
+      }
+
+    $scope.$on('$viewContentLoaded', function(event) {
+      $scope.initialize();
+    });
+
+};
---
+++ web/systems/support/routes.js
@@ -0,0 +1,22 @@
+
+var app = angular.module('mainApp')
+
+app.config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider
+
+  // .when('/main', {
+  //    templateUrl: '/systems/laboralInsertion/modules/main.html'
+  // })
+
+  .when('/resetPassword', {
+     templateUrl: '/systems/support/modules/resetPassword/index.html',
+     controller: 'ResetPasswordCtrl'
+  })
+
+
+  .otherwise({
+    redirectTo: '/resetPassword'
+  });
+
+}]);
