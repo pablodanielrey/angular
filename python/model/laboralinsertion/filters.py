e873554a0cda52d797ed7223a35583a9fcf55756
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-03-10 09:42:53 -0300

    serualizo una lista usando json pickle

--- python/model/laboralinsertion/filters.py
+++ python/model/laboralinsertion/filters.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import re
 
 class Filter:
-
-    fr = re.compile("^{'filter':'(?P<type>.*)', 'data':(?P<data>.*)}$")
 
     @staticmethod
     def _groupFilters(filters=[]):
@@ -38,42 +35,11 @@ class Filter:
     def filter(self, list):
         return self._filter(list)
 
-    def toJson(self):
-        return "{{'filter':'{}', 'data':{}}}".format(self.__class__.__name__, self._toJson())
-
-    @classmethod
-    def fromJson(cls, s):
-        m = cls.fr.match(s)
-        if not m:
-            return None
-
-        t = m.group('type')
-        d = m.group('data')
-
-        ''' deserializa el objeto desde json '''
-        for sub in cls.__subclasses__():
-            if sub.__name__ != t:
-                continue
-
-            o = sub._fromJson(d)
-            if o is not None:
-                return o
-        return None
-
 
 class FDegree(Filter):
 
     def __init__(self):
         self.degree = 'Lic. En Economía'
-
-    def _toJson(self):
-        return "'{}'".format(self.degree)
-
-    @classmethod
-    def _fromJson(cls, data):
-        d = FDegree()
-        d.degree = data[1:][:-1]
-        return d
 
     def _filter(self, inscriptions):
         return [ i for i in inscriptions if i.degree == self.degree ]
@@ -88,17 +54,6 @@ class FInscriptionDate(Filter):
         else:
             self.date = date
 
-    def _toJson(self):
-        return "'{}'".format(self.date.isoformat())
-
-    @classmethod
-    def _fromJson(cls, data):
-        import dateutil.parser
-        f = FInscriptionDate()
-        date = data[1:][:-1]
-        f.date = dateutil.parser.parse(date)
-        return f
-
     def _filter(self, inscriptions):
         return [ i for i in inscriptions if i.created.date().year == self.date.date().year ]
 
@@ -107,6 +62,12 @@ if __name__ == '__main__':
 
     import datetime
     fs = [ FInscriptionDate(), FDegree(), FInscriptionDate(datetime.datetime.now() - datetime.timedelta(hours = -1 * 24 * 365)) ]
+
+    import jsonpickle
+    import sys
+
+    print(jsonpickle.encode(fs))
+    sys.exit(1)
 
     from inscription import Inscription
     ins = Inscription()
