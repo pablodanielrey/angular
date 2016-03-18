0a2fd7dd273fc04c0f03e393d22dd969e2907a1f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-03-18 15:52:20 -0300

    puse un logging para ver si esta siendo desincronizando

--- server/actions/systems/laboralinsertion/laboralInsertion.py
+++ server/actions/systems/laboralinsertion/laboralInsertion.py
@@ -241,6 +241,7 @@ class LaboralInsertionWamp(ApplicationSession):
     def findAllInscriptions(self, filters):
         con = self.conn.get()
         try:
+            logging.debug(filters)
             filterss = Filter.fromJson(filters)
             data = self.laboralInsertion.findAllInscriptions(con, filterss)
             insc = [ i.__dict__ for i in data ]
