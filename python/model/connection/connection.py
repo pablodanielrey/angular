55e71819cf0f48f6f0dfb3469b48c28433461db2
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-03-30 11:45:34 -0300

    puse el destructor para que limpie las conexiones de la base. tambien limite el numero de conexiones máximas

--- python/model/connection/connection.py
+++ python/model/connection/connection.py
@@ -5,6 +5,7 @@
 import psycopg2
 from psycopg2 import pool
 from psycopg2.extras import DictCursor
+import logging
 import inject
 
 from model.registry import Registry
@@ -18,10 +19,16 @@ class Connection:
         self.database = registry.get('database')
         self.user = registry.get('user')
         self.password = registry.get('password')
-        self.pool = psycopg2.pool.ThreadedConnectionPool(10, 20, host=self.host, database=self.database, user=self.user, password=self.password, cursor_factory=DictCursor)
+        self.pool = psycopg2.pool.ThreadedConnectionPool(1, 10, host=self.host, database=self.database, user=self.user, password=self.password, cursor_factory=DictCursor)
 
     def get(self):
+        logging.debug('obteniendo conexion a la base')
         return self.pool.getconn()
 
     def put(self, conn):
+        logging.debug('retornando la conexion al pool')
         self.pool.putconn(conn)
+
+    def __del__(self):
+        logging.debug('cerrando todas las conexiones a la base')
+        self.pool.closeall()
