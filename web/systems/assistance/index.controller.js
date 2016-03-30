7879be23a181d376e5c2e1ae9d0e9c57dd6a289c
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-03-30 17:10:11 -0300

    Agregue el Menu para celular y correji la visual de la pantalla de inicio

--- web/systems/assistance/index.controller.js
+++ web/systems/assistance/index.controller.js
@@ -10,13 +10,22 @@ function IndexCtrl($rootScope, $scope, $wamp, $window, Notifications, Login) {
     $scope.model = {
       hideMenu: false
     };
+    $scope.styleMenu = 'full';
 
     $scope.hideMenu = function() {
       return $scope.model.hideMenu;
     }
 
+    $scope.$watch(function() { return $window.innerWidth; }, function(o,n) {
+        if ($window.innerWidth <= 720) {
+          $scope.styleMenu = 'celular';
+        } else {
+          $scope.styleMenu = 'full';
+        }
+    });
 
     $scope.initialize = function() {
+      $scope.styleMenu = 'full';
       if (!Login.isLogged()) {
         $window.location.href = "/systems/login/index.html";
       }
