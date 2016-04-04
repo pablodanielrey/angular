9c1bdf70f1f530d0154e4ec964d63ee2c074fc20
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-04-04 14:08:58 -0300

    iniciamos el cambio de nombres apra que eset dentro de la estructura.

--- web/systems/barcode/angular/initializeApp.js
+++ web/systems/barcode/angular/initializeApp.js
@@ -8,59 +8,6 @@
  * ngFileUpload: Directiva para subir archivos (incluir si se necesita subir archivos, var app = angular.module("MainApp", [..., 'ngFileUpload'], ... //adicionalmente se debe incluir la librería html
  */
 
-var app = angular.module("MainApp", ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'ngFileUpload'], function($httpProvider) {
+var app = angular.module("MainApp", ['ngRoute'], function() {
  
-  // Use x-www-form-urlencoded Content-Type
-  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
- 
-  /**
-   * The workhorse; converts an object to x-www-form-urlencoded serialization.
-   * @param {Object} obj
-   * @return {String}
-   */
-  var param = function(obj) {
-    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
-      
-    for(name in obj) {
-      value = obj[name];
-        
-      if(value instanceof Array) {
-        for(i=0; i<value.length; ++i) {
-          subValue = value[i];
-          fullSubName = name + '[' + i + ']';
-          innerObj = {};
-          innerObj[fullSubName] = subValue;
-          query += param(innerObj) + '&';
-        }
-      }
-      else if(value instanceof Object) {
-        for(subName in value) {
-          subValue = value[subName];
-          fullSubName = name + '[' + subName + ']';
-          innerObj = {};
-          innerObj[fullSubName] = subValue;
-          query += param(innerObj) + '&';
-        }
-      }
-      else if(value !== undefined && value !== null)
-        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-    }
-      
-    return query.length ? query.substr(0, query.length - 1) : query;
-  };
- 
-  // Override $http service's default transformRequest
-  $httpProvider.defaults.transformRequest = [function(data) {
-    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-  }];
-
 })
-
-.config(['uibDatepickerConfig', function(uibDatepickerConfig) {
-  uibDatepickerConfig.showWeeks = false;
-  uibDatepickerConfig.startingDay = 1;
-}])
-
-.config(['uibDatepickerPopupConfig', function(uibDatepickerConfig) {
-  uibDatepickerConfig.showButtonBar = false;
-}]);
