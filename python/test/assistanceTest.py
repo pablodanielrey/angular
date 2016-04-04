1033bc47765cf3539ec0254b8a813210f1addbad
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-04-04 20:14:57 -0300

    agregue para correr el libreoffice y corregimos el tema del daily del schedule

--- python/test/assistanceTest.py
+++ python/test/assistanceTest.py
@@ -29,21 +29,38 @@ if __name__ == '__main__':
 
         logging.info('buscando los usuarios')
 
-        # uid, v = UserDAO.findByDni(con, sys.argv[1])
-        # uids = [uid]
-        uids = [ u for u,v in UserDAO.findAll(con) ]
+        uids = []
+        #uid, v = UserDAO.findByDni(con, "30001823")    # walter
+        #uid, v = UserDAO.findByDni(con, "27821597")     # maxi
+        #uid, v = UserDAO.findByDni(con, "31073351")     # ivan
+        #uid, v = UserDAO.findByDni(con, "33212183")     # santiago
+        #uid, v = UserDAO.findByDni(con, "27294557")     # pablo
+        #uid, v = UserDAO.findByDni(con, "31381082")     # ema
+        #uid, v = UserDAO.findByDni(con, "29694757")      # oporto
+        uids.append(uid)
+
+        #uid, v = UserDAO.findByDni(con, "31381082")
+        #uids.append(uid)
+
+        #uids = [ u for u,v in UserDAO.findAll(con) ]
 
         logging.info('cargando los periodos')
 
         a = inject.instance(AssistanceModel)
-        wps = a.getWorkPeriods(con, uids, datetime.datetime.now() - datetime.timedelta(10000), datetime.datetime.now())
+        wps = a.getWorkPeriods(con, uids, datetime.datetime.now() - datetime.timedelta(days=63), datetime.datetime.now())
 
+        totalHoras = 0
         logs = []
         for w in wps:
             wp = wps[w]
             for w1 in wp:
                 sec = w1.getWorkedSeconds()
-                logging.info('{} --> e:{}, s:{} --> {}:{} '.format(w1.date, w1.getStartDate(), w1.getEndDate(), int(sec / 60 / 60), int(sec / 60 % 60)))
+                hi = None if w1.getStartLog() is None else w1.getStartLog().log.time()
+                hs = None if w1.getEndLog() is None else w1.getEndLog().log.time()
+                logging.info('{} --> e:{}, s:{} --> {}:{} -- he {} - hs {}'.format(w1.date, w1.getStartDate(), w1.getEndDate(), int(sec / 60 / 60), int(sec / 60 % 60), hi, hs))
+                totalHoras = totalHoras + sec
+
+        logging.info('total de horas trabajadas : {}:{}'.format(int(totalHoras / 60 / 60), int(totalHoras / 60 % 60)))
 
         #logging.info(json.dumps(logs, cls=Serializer))
             #logging.info(json.dumps(wps[w], cls=Serializer))
