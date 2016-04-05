667a5dc7a6a9594bcaf2a29b21d773793a8b5742
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-04-05 03:17:43 -0300

    cambie para que se grafique la cantidad de horas

--- python/test/assistanceTest.py
+++ python/test/assistanceTest.py
@@ -84,10 +84,13 @@ if __name__ == '__main__':
         doc = calc.open_spreadsheet('template.ods')
         sheet = doc.sheets[0]
 
+        sh = 0
+        chartStart = 0
         index = 2
         totalHoras = 0
         logs = []
         for w in wps:
+            chartStart = index
             wp = wps[w]
             for w1 in wp:
                 sec = w1.getWorkedSeconds()
@@ -100,14 +103,25 @@ if __name__ == '__main__':
                 logging.info('{} --> e:{}, s:{} --> {}:{} -- he {} - hs {}'.format(w1.date, sd, ed, th, tm, hi, hs))
                 totalHoras = totalHoras + sec
 
-                sheet[index,0].value = findUser(users, w1.userId).dni if findUser(users, w1.userId) is not None else 'nada'
-                sheet[index,1].value = w1.date
-                sheet[index,2].value = sd
-                sheet[index,3].value = ed
-                sheet[index,4].value = hi
-                sheet[index,5].value = hs
-                sheet[index,6].value = sec
+                us = findUser(users, w1.userId) if findUser(users, w1.userId) is not None else 'nada'
+                sheet[index,0].value = us.dni if us != 'nada' else ''
+                sheet[index,1].value = us.name if us != 'nada' else ''
+                sheet[index,2].value = us.lastname if us != 'nada' else ''
+                sheet[index,3].value = w1.date
+                sheet[index,4].value = sd
+                sheet[index,5].value = ed
+                sheet[index,6].value = hi
+                sheet[index,7].value = hs
+                #sheet[index,8].value = datetime.timedelta(seconds=sec)
+                sheet[index,8].value = sec
+                sheet[index,9].formula = '={}/60/60'.format(sheet[index,8].address)
                 index = index + 1
+
+            logging.info(chartStart)
+            logging.info(index)
+            c = sheet.charts.create('Horas Trabajadas {}'.format(len(sheet.charts)), sheet[chartStart:index, 12:12 + len(wp)], sheet[chartStart:index, 9:10])
+            #c.change_type(pyoo.LineDiagram)
+            index = index + 1
 
         logging.info('total de horas trabajadas : {}:{}'.format(int(totalHoras / 60 / 60), int(totalHoras / 60 % 60)))
 
