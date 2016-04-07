bd78bfaa6587728e148715e2e3282fe7b6513981
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-04-07 19:37:20 -0300

    actualice el remporte con algunos graficos

--- python/test/assistanceReport.py
+++ python/test/assistanceReport.py
@@ -29,6 +29,34 @@ def findUser(users, uid):
         if uid == u.id:
             return u
 
+class WpStatistics:
+
+    def __init__(self, userId):
+        self.userId = userId
+        self.secondsToWork = 0              # total que deberia trabajar
+        self.secondsWorked = 0              # total trabajado
+        self.secondsLate = 0                # total de llegadas tarde
+        self.countLate = 0                  # cantidad de llegadas tarde
+        self.secondsEarly = 0               # total de salidas tempranas
+        self.countEarly = 0                 # cantidad de salidas tempranas
+
+    def updateStatistic(self, start, end, hourStart, hourEnd):
+
+        if end is not None and start is not None:
+            self.secondsToWork = self.secondsToWork + (end - start).total_seconds()
+
+        if hourStart is not None and hourEnd is not None:
+            self.secondsWorked = self.secondsWorked + (hourEnd - hourStart).total_seconds()
+
+        if hourStart is not None and start is not None and hourStart > start:
+            self.secondsLate = self.secondsLate + (hourStart - start).total_seconds()
+            self.countLate = self.countLate + 1
+
+        if hourEnd is not None and end is not None and end > hourEnd:
+            self.secondsEarly = self.secondsEarly + (end - hourEnd).total_seconds()
+            self.countEarly = self.countEarly + 1
+
+
 def workedPeriodsToPyoo(wps, users):
 
     import uuid
@@ -44,6 +72,7 @@ def workedPeriodsToPyoo(wps, users):
         for user in users:
 
             if user:
+                stats = WpStatistics(user.id)
 
                 """ creo una copia del template para poder llenar los datos """
                 sheet = doc.sheets.copy('Template', '{} {} {}'.format(user.dni, user.name, user.lastname), sheetIndex)
@@ -86,11 +115,74 @@ def workedPeriodsToPyoo(wps, users):
                     if len(w1.justifications) > 0:
                         sheet[index,12].value = w1.justifications[0].getIdentifier()
 
+                    stats.updateStatistic(sd, ed, hi, hs)
                     index = index + 1
 
 
+                """ calculo los totales """
+                indexLastData = index
+                index = index + 4
+                #sheet[index-1,6].value = 'Cantidad Total a Trabajar'
+                sheet[index,6].value = stats.secondsToWork
+                sheet[index+1,6].value = '{} d {} h {} m {} s'.format(int(stats.secondsToWork / 60 / 60 / 24), int(stats.secondsToWork / 60 / 60), int(stats.secondsToWork / 60 % 60), int(stats.secondsToWork % 60))
 
-            #c = sheet.charts.create('Horas Trabajadas {}'.format(len(sheet.charts)), sheet[chartStart:index, 12:12 + len(wp)], sheet[chartStart:index, 9:10])
+                #index = index + 4
+                #sheet[index-1,6].value = 'Cantidad Total Trabajada'
+                sheet[index,9].value = stats.secondsWorked
+                sheet[index+1,9].value = '{} d {} h {} m {} s'.format(int(stats.secondsWorked / 60 / 60 / 24), int(stats.secondsWorked / 60 / 60), int(stats.secondsWorked / 60 % 60), int(stats.secondsWorked % 60))
+
+                #index = index + 4
+                #sheet[index-1,6].value = 'Cantidad Total de Lllegadas Tarde'
+                sheet[index,50].value = int(stats.secondsLate)
+                #sheet[index+1,10].value = '{} d {} h {} m {} s'.format(int(stats.secondsLate / 60 / 60 / 24), int(stats.secondsLate / 60 / 60), int(stats.secondsLate / 60 % 60), int(stats.secondsLate % 60))
+                sheet[index,10].value = '{} llegadas tarde'.format(int(stats.countLate))
+
+                #index = index + 4
+                #sheet[index-1,6].value = 'Cantidad Total de Salidas Tempranas'
+                sheet[index,51].value = int(stats.secondsEarly)
+                #sheet[index+1,11].value = '{} d {} h {} m {} s'.format(int(stats.secondsEarly / 60 / 60 / 24), int(stats.secondsEarly / 60 / 60), int(stats.secondsEarly / 60 % 60), int(stats.secondsEarly % 60))
+                sheet[index,11].value = '{} Salidas Tempranas'.format(int(stats.countEarly))
+
+
+                """ armo los graficos comparativos """
+
+                chartSize = 10
+                chartLength = int(len(wp) / 4)
+                columnStart = 3
+
+                # horas de trabajo / horas trabajadas
+                chartStart = indexLastData + 10;
+                chartEnd = chartStart + chartSize
+                sheet[chartStart,0].value = 'Trabajo/Trabajadas'
+                c = sheet.charts.create('{}'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[2:indexLastData, 6:7], sheet[2:indexLastData, 9:10]])
+
+                # horas trabajadas / llegadas tarde
+                chartStart = chartEnd + 1
+                chartEnd = chartStart + chartSize
+                sheet[chartStart,0].value = 'Trabajadas/Llegadas Tarde'
+                c2 = sheet.charts.create('{} 2'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[2:indexLastData, 9:10], sheet[2:indexLastData, 10:11]])
+
+                # horas trabajadas / salidas temprano
+                chartStart = chartEnd + 1
+                chartEnd = chartStart + chartSize
+                sheet[chartStart,0].value = 'Trabajadas/Salidas Temprano'
+                c3 = sheet.charts.create('{} 3'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[2:indexLastData, 9:10], sheet[2:indexLastData, 11:12]])
+
+                chartLength = 2
+                indexData = indexLastData + 4
+                # total horas trabajadas / total horas llegadas tarde
+                chartStart = chartEnd + 1
+                chartEnd = chartStart + chartSize
+                sheet[chartStart,0].value = 'Total: Trabajadas/Llegadas Tarde'
+                c3 = sheet.charts.create('{} 4'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[indexData, 9], sheet[indexData, 50]])
+
+                # total horas trabajadas / total horas de salidas temprano
+                chartStart = chartEnd + 1
+                chartEnd = chartStart + chartSize
+                sheet[chartStart,0].value = 'Total: Trabajadas/Salidas Temprano'
+                c3 = sheet.charts.create('{} 5'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[indexData, 9], sheet[indexData, 51]])
+
+
             #index = index + 1
         doc.save(fn)
 
@@ -113,8 +205,8 @@ def _getUsers(con):
     #uid, v = UserDAO.findByDni(con, "24040623")     # miguel rey
     #uids.append(uid)
 
-    #uid, v = UserDAO.findByDni(con, "27821597")     # maxi
-    #uids.append(uid)
+    uid, v = UserDAO.findByDni(con, "27821597")     # maxi
+    uids.append(uid)
 
     uid, v = UserDAO.findByDni(con, "31073351")     # ivan
     uids.append(uid)
