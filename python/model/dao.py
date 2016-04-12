65436922a330cb969f077e90443902afb0c124e7
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-04-12 09:21:52 -0300

    agregar metodo create dependencies encargado de recorrer las dependencias y llamar al createSchema

--- python/model/dao.py
+++ python/model/dao.py
@@ -1,7 +1,13 @@
 
 class DAO:
-
+    dependencies = []
+    
     @classmethod
     def _createSchema(cls, con):
         for c in cls.__subclasses__():
             c._createSchema()
+            
+    @classmethod
+    def _createDependencies(cls, con):
+        for c in cls.dependencies:
+            c._createSchema(con)
