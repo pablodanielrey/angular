8be3dcd38516297518fd423c7874f8620fa9a774
Emanuel Pais <emanuel84@gmail.com>
2016-04-18 13:58:53 -0300

    Cree la migracion de Boleta de salida

--- python/test/migrateJustifications.py
+++ python/test/migrateJustifications.py
@@ -13,6 +13,7 @@ from model.connection.connection import Connection
 from model.assistance.justifications.status import Status
 from model.assistance.justifications.informedAbsenceJustification import InformedAbsenceJustification, InformedAbsenceJustificationDAO
 from model.assistance.justifications.compensatoryJustification import CompensatoryJustification, CompensatoryJustificationDAO
+from model.assistance.justifications.outTicketJustification import OutTicketWithoutReturnJustification, OutTicketWithReturnJustification, OutTicketJustificationDAO
 
 """
 UNDEFINED = 0
@@ -51,7 +52,7 @@ def createAA(con):
     cur = con.cursor()
     try:
         logging.info('Migrando los ausentes con aviso')
-        # creo el la tabla
+        # creo la tabla
         InformedAbsenceJustificationDAO._createSchema(con)
         # id de la justificación Ausente con Aviso
         id = 'e0dfcef6-98bb-4624-ae6c-960657a9a741'
@@ -81,7 +82,7 @@ def createCompensatory(con):
     cur = con.cursor()
     try:
         logging.info('Migrando los Compensatorios')
-        # creo el la tabla
+        # creo la tabla
         CompensatoryJustificationDAO._createSchema(con)
         # id de la justificación Compensatorio
         id = '48773fd7-8502-4079-8ad5-963618abe725'
@@ -102,6 +103,39 @@ def createCompensatory(con):
     finally:
         cur.close()
 
+def createBS(con):
+    """
+        migra las justificaciones Boleta de Salida
+    """
+    cur = con.cursor()
+    try:
+        logging.info("Migrando las Boleta de Salida")
+        # creo la tabla
+        OutTicketJustificationDAO._createSchema(con)
+        # id de la justificación Boleta de Salida
+        id = 'fa64fdbd-31b0-42ab-af83-818b3cbecf46'
+
+        cur.execute('select id, user_id, requestor_id, jbegin, jend from assistance.justifications_requests where justification_id = %s',(id,))
+        for jr in cur:
+            logging.info('obteniendo justificacion : {}:{}'.format(jr['id'], jr['requestor_id']))
+
+            userId = jr['user_id']
+            ownerId = jr['requestor_id']
+            date = jr['jbegin']
+            end = jr['jend']
+
+            if end is None:
+                just = OutTicketWithoutReturnJustification(userId, ownerId, date)
+            else:
+                just = OutTicketWithReturnJustification(userId, ownerId, date, end)
+
+            just.id = jr['id']
+            j = just.findById(con, [just.id])
+            if (j is None or len(j) <= 0):
+                setStatus(con, just)
+    finally:
+        cur.close()
+
 if __name__ == '__main__':
 
     logging.getLogger().setLevel(logging.INFO)
@@ -111,8 +145,9 @@ if __name__ == '__main__':
     try:
         con = conn.get()
 
-        createAA(con)
-        createCompensatory(con)
+        # createAA(con)
+        # createCompensatory(con)
+        createBS(con)
 
         con.commit()
     finally:
