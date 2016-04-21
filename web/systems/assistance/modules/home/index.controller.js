197d93b7f5ef4b2a220158e28e1cb8e61df48005
Emanuel Pais <emanuel84@gmail.com>
2016-04-21 14:37:27 -0300

    Agregue algunos detalles del home

--- web/systems/assistance/modules/home/index.controller.js
+++ web/systems/assistance/modules/home/index.controller.js
@@ -1,10 +1,41 @@
-var app = angular.module('mainApp');
+angular
+  .module('mainApp')
+  .controller('HomeCtrl', HomeCtrl);
 
-app.controller('CreateTutoringCtrl', ["$rootScope", '$scope',
+HomeCtrl.inject = ['$rootScope', '$scope', 'Users', 'Login']
 
+function HomeCtrl($rootScope, $scope, Users, Login) {
 
-  function ($rootScope, $scope) {
-    console.log("CreateTutoringCtrl");
+  $scope.initialize = initialize;
+  $scope.loadUser = loadUser;
+
+  $scope.model = {
+    user: null,
+    userId: ''
   }
 
-]);
+  $scope.$on('$viewContentLoaded', function(event) {
+    $scope.userId = '';
+    Login.getSessionData()
+      .then(function(s) {
+          $scope.userId = s.user_id;
+          $scope.initialize();
+      }, function(err) {
+        console.log(err);
+      });
+  });
+
+  function initialize() {
+    $scope.loadUser();
+  }
+
+  function loadUser() {
+    Users.findById([$scope.userId]).then(function(users) {
+      $scope.model.user = (users.length > 0) ? users[0] : null;
+    }, function(error) {
+      console.log('Error al buscar el usuario')
+    });
+  }
+
+
+}
