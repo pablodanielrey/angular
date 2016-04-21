324d113bf16cb34193df72b7ae1312a4d7d76cb8
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-04-21 14:03:25 -0300

    agregar TestSuite y clase TestUserFindById

--- python/testecono/TestUser.py
+++ python/testecono/TestUser.py
@@ -8,23 +8,22 @@ from model.users.users import User
 from model.users.users import Telephone
 from model.users.users import UserDAO
 
-class TestUserPersist(TestEcono):
+
+class TestUser(TestEcono):
     
     def setUp(self):
-        super(TestUserPersist, self).setUp()
+        super(TestUser, self).setUp()
         try:
             con = self.connection.get()
             try:
                 UserDAO._createSchema(con)
                 con.commit()
-                self.user = self.defineUserToPersist()
        
             finally:
                 self.connection.put(con)
             
         except Exception as e: 
             logging.error(str(e))
-      
             
     def defineUser(self):
         telephone = Telephone()
@@ -37,8 +36,30 @@ class TestUserPersist(TestEcono):
         user.dni = "31012345"
         user.telephones = [ telephone ]
         
-        return user
+        return user           
+
+    def assertEqualTelephones(self, telephone, telephone2):
+        self.assertEqual(telephone.type, telephone2.type)
+
+
+    def assertEqualUsers(self, user, user2):
+        self.assertEqual(user.name, user2.name)
+        self.assertEqual(user.lastname, user2.lastname)
+        self.assertEqual(user.dni, user2.dni)
+        self.assertEqual(len(user.telephones), len(user2.telephones))
+        for t1, t2 in zip(user.telephones, user2.telephones):
+           self.assertEqualTelephones(t1,t2)
+
+
+
+
+
+class TestUserPersist(TestUser):
     
+    def setUp(self):
+        super(TestUserPersist, self).setUp()
+        self.user = self.defineUserToPersist()
+
     def defineUserToPersist(self):
         user = self.defineUser()
       
@@ -55,50 +76,88 @@ class TestUserPersist(TestEcono):
             self.connection.put(con)
 
         return user    
-         
-         
-         
-    def assertEqualTelephones(self, telephone, telephone2):
-        self.assertEqual(telephone.type, telephone2.type)
-
-
-    
-    def assertEqualUsers(self, user, user2):
-        self.assertEqual(user.name, user2.name)
-        self.assertEqual(user.lastname, user2.lastname)
-        self.assertEqual(user.dni, user2.dni)
-        self.assertEqual(len(user.telephones), len(user2.telephones))
-        for t1, t2 in zip(user.telephones, user2.telephones):
-           self.assertEqualTelephones(t1,t2)
 
             
     def test_persist_user(self):
         try:
             con = self.connection.get()
             try:
+                ##### insertar #####
                 UserDAO.persist(con, self.user)
                 con.commit()
-
                 uid = UserDAO.findByDni(con, self.user.dni)
                 u = UserDAO.findById(con, [uid[0]])
                 self.assertEqualUsers(self.user, u[0])
-
+                
+                ##### actualizar #####
+                self.user.name = "Test Nomb"
+                UserDAO.persist(con, self.user)
+                con.commit()
+                uid = UserDAO.findByDni(con, self.user.dni)
+                u = UserDAO.findById(con, [uid[0]])
+                self.assertEqualUsers(self.user, u[0])
+                
+                ##### error #####
                 user = self.defineUser()
                 with self.assertRaises(Exception):
                     UserDAO.persist(con, user)
 
+                               
+            finally:
+                self.connection.put(con)
                 
+        except Exception as e: 
+            logging.error(str(e))
+            
+            
+            
+            
+            
+            
+            
+            
+            
+class TestUserFindById(TestEcono):
+    
+    def setUp(self):
+        super(TestUserFindById, self).setUp()
+        self.user = self.userToFind()
 
+    def userToFind(self):
+        user = self.defineUser()
+      
+        con = self.connection.get()
+
+        try:
+            u = UserDAO.findByDni(con, user.dni)
+    
+            if(u is None):
+                UserDAO.persist(con, u)
+           
+            return u;
+            
+        finally:
+            self.connection.put(con)
+
+        return user
+        
+        
+    def test_find_by_id(self):
+        try:
+            con = self.connection.get()
+            try:
+                u = UserDAO.findById(con,[self.user.id])
+                self.assertEqualUsers(self.user, u[0])
                 
+                u = UserDAO.findById(con, "not_exists")
+                #self.assertNull(u)
                 
+                               
             finally:
                 self.connection.put(con)
                 
         except Exception as e: 
             logging.error(str(e))
 
-    
 
 
-if __name__ == '__main__':
-    unittest.main()
