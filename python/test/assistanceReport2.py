e9523240191005d28f7463437789dd19099a37e3
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-05-10 19:45:30 -0300

    inicio del reporte 2

--- python/test/assistanceReport2.py
+++ python/test/assistanceReport2.py
@@ -32,142 +32,6 @@ def findUser(users, uid):
         if uid == u.id:
             return u
 
-def workedPeriodsToPyoo(wps, users):
-
-    import uuid
-    f = str(uuid.uuid4())
-    fn = '/tmp/{}.ods'.format(f)
-
-    import pyoo
-    calc = pyoo.Desktop('localhost', 2002)
-    doc = calc.open_spreadsheet('template.ods')
-    try:
-        sheetIndex = 0
-
-        for user in users:
-
-            if user:
-                stats = WpStatistics(user.id)
-
-                """ creo una copia del template para poder llenar los datos """
-                sheet = doc.sheets.copy('Template', '{} {} {}'.format(user.dni, user.name, user.lastname), sheetIndex)
-                sheetIndex = sheetIndex + 1
-
-                index = 2
-                wp = wps[user.id]
-                for w1 in wp:
-                    sec = w1.getWorkedSeconds()
-                    sd = w1.getStartDate()
-                    ed = w1.getEndDate()
-                    hi = None if w1.getStartLog() is None else w1.getStartLog().log.astimezone(tzlocal()).replace(tzinfo=None)
-                    hs = None if w1.getEndLog() is None else w1.getEndLog().log.astimezone(tzlocal()).replace(tzinfo=None)
-
-                    sheet[index,0].value = user.dni
-                    sheet[index,1].value = user.name
-                    sheet[index,2].value = user.lastname
-
-                    sheet[index,3].value = w1.date
-                    if sd is not None: sheet[index,4].value = sd
-                    if ed is not None: sheet[index,5].value = ed
-                    if sd is not None and ed is not None: sheet[index,6].formula = '={}-{}'.format(sheet[index,5].address, sheet[index,4].address)
-
-                    if hi is not None: sheet[index,7].value = hi
-                    if hs is not None: sheet[index,8].value = hs
-
-                    if hi is not None and hs is not None:
-                        sheet[index,9].formula = '={}-{}'.format(sheet[index,8].address, sheet[index,7].address)
-
-                    if hi is not None and hi > sd:
-                        sheet[index,10].formula = '={}-{}'.format(sheet[index,7].address, sheet[index,4].address)
-                    #else:
-                        #sheet[index,9].value = 0
-
-                    if hs is not None and ed > hs:
-                        sheet[index,11].formula = '={}-{}'.format(sheet[index,5].address, sheet[index,8].address)
-                    #else:
-                        #sheet[index,10].value = 0
-
-                    if len(w1.justifications) > 0:
-                        sheet[index,12].value = w1.justifications[0].getIdentifier()
-
-                    stats.updateStatistics(w1)
-                    index = index + 1
-
-                indexLastData = index
-
-                """ calculo los totales """
-                index = index + 4
-                #sheet[index-1,6].value = 'Cantidad Total a Trabajar'
-                #sheet[index,6].value = stats.secondsToWork
-                #sheet[index+1,6].value = '{} d {} h {} m {} s'.format(int(stats.secondsToWork / 60 / 60 / 24), int(stats.secondsToWork / 60 / 60), int(stats.secondsToWork / 60 % 60), int(stats.secondsToWork % 60))
-
-                #index = index + 4
-                #sheet[index-1,6].value = 'Cantidad Total Trabajada'
-                #sheet[index,9].value = stats.secondsWorked
-                #sheet[index+1,9].value = '{} d {} h {} m {} s'.format(int(stats.secondsWorked / 60 / 60 / 24), int(stats.secondsWorked / 60 / 60), int(stats.secondsWorked / 60 % 60), int(stats.secondsWorked % 60))
-
-                #index = index + 4
-                #sheet[index-1,6].value = 'Cantidad Total de Lllegadas Tarde'
-                sheet[index,50].value = int(stats.secondsLate)
-                #sheet[index+1,10].value = '{} d {} h {} m {} s'.format(int(stats.secondsLate / 60 / 60 / 24), int(stats.secondsLate / 60 / 60), int(stats.secondsLate / 60 % 60), int(stats.secondsLate % 60))
-                sheet[index,10].value = '{} llegadas tarde'.format(int(stats.countLate))
-
-                #index = index + 4
-                #sheet[index-1,6].value = 'Cantidad Total de Salidas Tempranas'
-                sheet[index,51].value = int(stats.secondsEarly)
-                #sheet[index+1,11].value = '{} d {} h {} m {} s'.format(int(stats.secondsEarly / 60 / 60 / 24), int(stats.secondsEarly / 60 / 60), int(stats.secondsEarly / 60 % 60), int(stats.secondsEarly % 60))
-                sheet[index,11].value = '{} Salidas Tempranas'.format(int(stats.countEarly))
-
-                """ detallo las justificaciones """
-
-
-
-                """ armo los graficos comparativos """
-
-                chartSize = 10
-                chartLength = int(len(wp) / 4)
-                columnStart = 3
-
-                # horas de trabajo / horas trabajadas
-                chartStart = indexLastData + 10;
-                chartEnd = chartStart + chartSize
-                sheet[chartStart,0].value = 'Trabajo/Trabajadas'
-                c = sheet.charts.create('{}'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[2:indexLastData, 6:7], sheet[2:indexLastData, 9:10]])
-
-                # horas trabajadas / llegadas tarde
-                chartStart = chartEnd + 1
-                chartEnd = chartStart + chartSize
-                sheet[chartStart,0].value = 'Trabajadas/Llegadas Tarde'
-                c2 = sheet.charts.create('{} 2'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[2:indexLastData, 9:10], sheet[2:indexLastData, 10:11]])
-
-                # horas trabajadas / salidas temprano
-                chartStart = chartEnd + 1
-                chartEnd = chartStart + chartSize
-                sheet[chartStart,0].value = 'Trabajadas/Salidas Temprano'
-                c3 = sheet.charts.create('{} 3'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[2:indexLastData, 9:10], sheet[2:indexLastData, 11:12]])
-
-                chartLength = 2
-                indexData = indexLastData + 4
-                # total horas trabajadas / total horas llegadas tarde
-                chartStart = chartEnd + 1
-                chartEnd = chartStart + chartSize
-                sheet[chartStart,0].value = 'Total: Trabajadas/Llegadas Tarde'
-                c3 = sheet.charts.create('{} 4'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[indexData, 9], sheet[indexData, 50]])
-
-                # total horas trabajadas / total horas de salidas temprano
-                chartStart = chartEnd + 1
-                chartEnd = chartStart + chartSize
-                sheet[chartStart,0].value = 'Total: Trabajadas/Salidas Temprano'
-                c3 = sheet.charts.create('{} 5'.format(user.id), sheet[chartStart:chartEnd, columnStart:columnStart + chartLength], [sheet[indexData, 9], sheet[indexData, 51]])
-
-
-            #index = index + 1
-        doc.save(fn)
-
-    finally:
-        doc.close()
-
-
 def _secondsToHours(seconds):
     if seconds == 0:
         return ""
@@ -202,9 +66,9 @@ def sortOfficeUsers(users, stats):
     return r
 
 
-def createReportDir():
+def createReportDir(office):
     import os
-    newpath = '/tmp/reporte-asistencia-{}'.format(datetime.datetime.now())
+    newpath = '/tmp/reporte-asistencia-{}-{}'.format(office, datetime.datetime.now())
     if not os.path.exists(newpath):
         os.makedirs(newpath)
     return newpath
@@ -448,7 +312,6 @@ if __name__ == '__main__':
     logging.info('Generando reporte de asistencia desde : {} hasta {}'.format(rstart, rend))
 
     reg = inject.instance(Registry)
-    rp = createReportDir()
     conn = Connection(reg.getRegistry('dcsys'))
     con = conn.get()
     try:
@@ -465,8 +328,9 @@ if __name__ == '__main__':
         stats = a.getStatistics(con, userIds, rstart, rend)
 
         for office in offices:
+            rp = createReportDir()
             statsToPyoo(rp, stats, users, [office])
+            createZipFile(office, rp)
 
     finally:
         conn.put(con)
-    createZipFile(rp)
