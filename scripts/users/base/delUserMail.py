e2904f051ad013a90221e2c7a0e33abefd8f8128
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-05-19 12:10:13 -0300

    definimos delete a los metodos que eliminan entidades

---
+++ scripts/users/base/delUserMail.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+'''
+    Elimina un email a un usuario.
+    Lo agrega como CONFIRMADO!!! ojo
+    Forma de invocación:
+
+        PYTHONPATH="../../../python" python3 delUserMail.py id-del-mail
+
+'''
+from model.connection import connection
+from model.users import users
+from model.registry import Registry
+import createUser
+import systems
+import logging
+
+def delMail(con, eid):
+    emails = users.Mail.findByUserId(con, uid)
+    assert len(emails) == 1
+    emails[0].delete(con)
+    logging.info('email eliminado')
+
+if __name__ == '__main__':
+
+    import sys
+
+    dni = sys.argv[1]
+    eid = sys.argv[2]
+
+    assert dni is not None
+    assert email is not None
+
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    import inject
+    #inject.configure()
+
+    r = inject.instance(Registry)
+    conn = connection.Connection(r.getRegistry('dcsys'))
+    con = conn.get()
+    try:
+        delMail(con, eid)
+        con.commit()
+        
+    except Exception as e:
+        logging.warn('Error eliminando email')
+        logging.exception(e)
+
+    finally:
+        conn.put(con)
