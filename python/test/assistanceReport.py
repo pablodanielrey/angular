5771d53957a680d68a42fea2bca1d30d4051b82d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-05-19 18:15:32 -0300

    agregue para que haga de meses

--- python/test/assistanceReport.py
+++ python/test/assistanceReport.py
@@ -444,7 +444,7 @@ if __name__ == '__main__':
 
     logging.getLogger().setLevel(logging.INFO)
 
-    rstart = datetime.datetime.now() - datetime.timedelta(days=7)
+    rstart = datetime.datetime.now() - datetime.timedelta(days=30)
     rend = datetime.datetime.now()
     if len(sys.argv) >= 2:
         import dateutil
