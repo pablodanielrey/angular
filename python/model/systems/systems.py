811f9a1a3c0241356de9d4f5827e15ed814c9e9d
Emanuel Pais <emanuel84@gmail.com>
2016-05-31 08:48:06 -0300

    Deje funcionando el perfil de fce. Se puede cambiar la foto y muestra los datos basicos

---
+++ python/model/systems/systems.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Systems:
+
+    @classmethod
+    def listSystems(cls, con, userId):
+        pass
+
+    @classmethod
+    def _isStudent(cls, con):
+        pass
+
+    @classmethod
+    def _isMail(cls, con):
+        pass
+
+    @classmethod
+    def _isOwncloud(cls, con):
+        pass
+
+    @classmethod
+    def _isAssistance(cls, con):
+        pass
+
+    @classmethod
+    def _isTutors(cls, con):
+        pass
