de9b4b2fecf282b349f0f187f5bf80ceab18041d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-06-03 16:23:44 -0300

    puse un alert asi notan que se modifico el tipo

--- web/systems/accountProvisional/modules/home/index.controller.js
+++ web/systems/accountProvisional/modules/home/index.controller.js
@@ -155,6 +155,7 @@ function HomeCtrl($rootScope, $scope, Account, Login, Users, Student, $timeout) 
   function updateType() {
     Account.updateType($scope.model.user, $scope.model.selectedType.value).then(function(data) {
       console.log("Se modifico correctamente");
+      alter("Se modificó correctamente");
     }, function(error) {
       console.log(error);
 
