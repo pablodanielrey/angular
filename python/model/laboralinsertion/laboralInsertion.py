0526a366763232c132a8b41897c118f48d4ddeed
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-06-06 20:49:44 -0300

    tenia error de sintaxis

--- python/model/laboralinsertion/laboralInsertion.py
+++ python/model/laboralinsertion/laboralInsertion.py
@@ -48,9 +48,9 @@ class LaboralInsertion:
 
         ''' aplico los filtros '''
         if len(filters) > 0:
-            inscriptions3 = Filter.apply(con, inscriptions2, filters)
+            inscriptions2 = Filter.apply(con, inscriptions2, filters)
 
-        return inscriptions3
+        return inscriptions2
 
     def getFilters(self):
         return Filters.getFilters()
