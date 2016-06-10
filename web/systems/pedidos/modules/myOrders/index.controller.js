b5c526fb23857b2bf4fd4f11cea1aa5d29163c0f
Emanuel Pais <emanuel84@gmail.com>
2016-06-10 14:00:13 -0300

    Trae mis pedidos, falta completar los datos

--- web/systems/pedidos/modules/myOrders/index.controller.js
+++ web/systems/pedidos/modules/myOrders/index.controller.js
@@ -8,9 +8,16 @@ function MyOrdersCtrl($rootScope, $scope, Issue, Login, $timeout) {
 
   $scope.initialize = initialize;
   $scope.getMyIssues = getMyIssues;
+  $scope.viewDetail = viewDetail;
 
   $scope.model = {
-    userId: null
+    userId: null,
+    issues: [],
+    issueSelected: null
+  }
+
+  $scope.view = {
+    style: ''
   }
 
 
@@ -26,19 +33,28 @@ function MyOrdersCtrl($rootScope, $scope, Issue, Login, $timeout) {
   });
 
   function initialize() {
+    $scope.model.issues = [];
     $scope.getMyIssues();
   }
 
   function getMyIssues() {
     Issue.getMyIssues().then(
-        function(issues) {
+      function(issues) {
+        $scope.$apply(function() {
+          $scope.model.issues = issues;
           console.log(issues);
-        },
-        function(err) {
-          console.log('error')
-        }
+        });
+
+      },
+      function(err) {
+        console.log('error')
+      }
     );
-    
+  }
+
+  function viewDetail(issue) {
+    $scope.model.issueSelected = issue;
+    $scope.view.style = 'pantallaDetallePedido';
   }
 
 }
