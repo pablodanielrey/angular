ab1dbc8417a3d3ec3a8539bcefaf150aa3abeb12
Emanuel Pais <emanuel84@gmail.com>
2016-06-21 11:12:11 -0300

    Cambie el controlador de horarios respetando las buenas practicas de angular

--- web/systems/assistance/modules/schedule/index.controller.js
+++ web/systems/assistance/modules/schedule/index.controller.js
@@ -1,8 +1,49 @@
-angular
-  .module('mainApp')
-  .controller('ScheduleCtrl', ScheduleCtrl);
+(function() {
+    'use strict';
 
-ScheduleCtrl.inject = ['$scope', 'Login', 'Assistance', 'Users', 'Office', '$location', '$timeout']
+    angular
+        .module('mainApp')
+        .controller('ScheduleController', ScheduleController);
 
-function ScheduleCtrl($scope, Login, Assistance, Users, Office, $location, $timeout) {
-}
+    ScheduleController.$inject = ['$scope', 'Login', 'Assistance'];
+
+    /* @ngInject */
+    function ScheduleController($scope, Login, Assistance) {
+        var vm = this;
+
+        // Variables
+        vm.model = {
+
+        }
+
+        vm.view = {
+          styles: ['pantallaUsuario', 'pantallaJefe'],
+          style: null,
+          styles2: ['', 'pantallaUsuarios', 'nuevoHorarioSemanal', 'nuevoHorarioEspecial'],
+          style2: null,
+          styles3: ['', 'pantallaMensaje'],
+          style3: null,
+          styles4: ['', 'procesando', 'procesado', 'errorDeSistema'],
+          style4: null
+        }
+        // Funciones
+        vm.activate = activate;
+        vm.initView = initView;
+
+
+        /////////////////////////////////////////
+        activate();
+
+        function activate() {
+          initView();
+        }
+
+        function initView() {
+          vm.view.style = vm.view.styles[1];
+          vm.view.style2 = vm.view.styles2[0];
+          vm.view.style3 = vm.view.styles3[0];
+          vm.view.style4 = vm.view.styles4[0];          
+        }
+
+    }
+})();
