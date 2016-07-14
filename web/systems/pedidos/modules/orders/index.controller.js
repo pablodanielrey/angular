cb7cfe03b4108cbe4c2508a76b08b6bc164bfc6f
Emanuel Pais <emanuel84@gmail.com>
2016-07-14 14:23:23 -0300

    Dividi la pantalla de pedidos

--- web/systems/pedidos/modules/orders/index.controller.js
+++ web/systems/pedidos/modules/orders/index.controller.js
@@ -1,35 +1,125 @@
-angular
-  .module('mainApp')
-  .controller('OrdersCtrl', OrdersCtrl);
+(function() {
+    'use strict';
 
-OrdersCtrl.inject = ['$rootScope', '$scope', 'Issue', 'Login', '$timeout', 'Users', 'Office']
+    angular
+        .module('mainApp')
+        .controller('OrdersCtrl', OrdersCtrl);
 
-function OrdersCtrl($rootScope, $scope, Issue, Login, $timeout, Users, Office) {
+    OrdersCtrl.$inject = ['$scope', '$timeout', 'Login', 'Issue'];
 
-  $scope.initialize = initialize;
+    /* @ngInject */
+    function OrdersCtrl($scope, $timeout, Login, Issue) {
+        var vm = this;
 
-  $scope.model = {
-    userId: null
-  }
+        // variables del modelo
+        vm.model = {
+          userId: '',
+          issues: []
+        }
 
-  $scope.view = {
-    style: '',
-    style2: ''
-  }
+        // variables de la vista
+        vm.view = {
+          style: '',
+          styles: ['', 'pantallaDetallePedido', 'pantallaMensaje', 'pantallaNuevoPedido', 'buscarPedidos'],
+          style2: '',
+          styles2: ['', 'buscarOficina', 'buscarArea', 'buscarConsulta', 'buscarUsuario', 'buscarOficinaDeUsuario', 'seleccionarEstado', 'seleccionarPrioridad'],
+          style3: '',
+          styles3: ['','pantallaMensajeAlUsuario'],
+          style4: '',
+          styles4: ['', 'mensajeCargando', 'mensajeError', 'mensajeEnviado', 'mensajePedidoCreado']
+        }
 
-  $scope.$on('$viewContentLoaded', function(event) {
-    $scope.model.userId = '';
-    Login.getSessionData()
-      .then(function(s) {
-          $scope.model.userId = s.user_id;
-          $scope.initialize();
-      }, function(err) {
-        console.log(err);
-      });
-  });
+        // métodos
+        vm.initializeModel = initializeModel;
+        vm.initializeView = initializeView;
 
-  function initialize() {
+        vm.closeMessage = closeMessage;
+        vm.messageLoading = messageLoading;
+        vm.messageError = messageError;
+        vm.messageSending = messageSending;
+        vm.messageCreated = messageCreated;
 
-  }
+        vm.loadIssues = loadIssues;
 
-}
+        activate();
+
+        function activate() {
+          vm.model.userId = '';
+          Login.getSessionData()
+            .then(function(s) {
+                vm.model.userId = s.user_id;
+                vm.initializeModel();
+                vm.initializeView();
+            }, function(err) {
+              $scope.$apply(function() {
+                vm.messageError(err);
+              })
+            });
+        }
+
+/* ************************************************************************ */
+/* ********************** INICIALIZACION ********************************** */
+/* ************************************************************************ */
+
+        function initializeView() {
+          vm.view.style = vm.view.styles[0];
+          vm.view.style2 = vm.view.styles2[0];
+          vm.view.style3 = vm.view.styles3[0];
+          vm.view.style4 = vm.view.styles4[0];
+        }
+
+        function initializeModel() {
+          vm.loadIssues();
+        }
+
+/* ************************************************************************ */
+/* **********************  ********************************** */
+/* ************************************************************************ */
+
+      function  loadIssues() {
+        vm.model.issues = [];
+        Issue.getAssignedIssues().then(
+          function(issues) {
+            
+          },
+          function(error) {
+            $scope.$apply(function() {
+              vm.messageError(err);
+            })
+          }
+        );
+      }
+
+/* ************************************************************************ */
+/* ************************** MENSAJES ************************************ */
+/* ************************************************************************ */
+      function messageError(error) {
+        vm.view.style3 = vm.view.styles3[1];
+        vm.view.style4 = vm.view.styles4[2];
+        $timeout(function() {
+          vm.closeMessage();
+        }, 2000);
+      }
+
+      function closeMessage() {
+        vm.view.style3 = vm.view.styles3[0];
+        vm.view.style4 = vm.view.styles4[0];
+      }
+
+      function messageLoading() {
+        vm.view.style3 = vm.view.styles3[1];
+        vm.view.style4 = vm.view.styles4[1];
+      }
+
+      function messageSending() {
+        vm.view.style3 = vm.view.styles3[1];
+        vm.view.style4 = vm.view.styles4[3];
+      }
+
+      function messageCreated() {
+        vm.view.style3 = vm.view.styles3[1];
+        vm.view.style4 = vm.view.styles4[4];
+      }
+
+    }
+})();
