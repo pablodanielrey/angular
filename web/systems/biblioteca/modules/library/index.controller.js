0b44adf1dca183245800bd5a8d58564a0133f39c
Walter Blanco <walter@creativehands.com.ar>
2016-07-14 18:03:09 -0300

    Continuo trabajando en el sistema de biblioteca

--- web/systems/biblioteca/modules/library/index.controller.js
+++ web/systems/biblioteca/modules/library/index.controller.js
@@ -1,10 +1,126 @@
 var app = angular.module('mainApp');
 
 app.controller('LibraryCtrl', ["$rootScope", '$scope',
-
-
   function ($rootScope, $scope) {
     console.log("LibraryCtrl");
-  }
 
+    var nombres = ['Walter Roberto', 'Pablo Daniel', 'Alejandro Agustin', 'Maximiliano Antonio', 'Ivan Roberto'];
+    var dnis = ['2738457298', '27294557', '30001823', '12345678', '876554432'];
+    var cclase = '';
+
+    $scope.logs = [];
+    for (var i = 0; i < 100; i++) {
+      if (i % 2 == 0) {
+        cclase = 'entrada';
+      } else {
+        cclase = 'salida';
+      };
+
+      $scope.logs.push(
+        {
+          clase: cclase,
+          tipo: 'NoDocente',
+          name: nombres[i % nombres.length],
+          lastname: 'Blanco',
+          dni: dnis[i % dnis.length],
+          hora: '10:30 am'
+        }
+      );
+    };
+
+    /*
+      repetir solooooo
+
+      $scope.variable = [];
+      for (var i = 0; i < cantidad; i++) {
+        $scope.variable.push('valor a agregar');
+      }
+
+      y en el html usar:
+
+      ng-repeat='i in variable'
+
+    */
+
+
+    /*
+
+      la lista inicial va entre []
+      los valores internos a la lista van entre {}
+      se separan usando ,
+      acordarse al final de la lista poner ;
+      ej:
+
+      $scope.variable = [
+
+        {
+            hora:'sdfdsf',
+            nombre:'sdfdsf',
+            dni:'sdfdsf',
+            lastname:'sdfdsf',
+            tipo:'sdf',
+            clase:'sdf'
+        },
+
+        {
+            hora:'sdfdsf',
+            nombre:'sdfdsf',
+            dni:'sdfdsf',
+            lastname:'sdfdsf',
+            tipo:'sdf',
+            clase:'sdf'
+        },
+
+        {
+            hora:'sdfdsf',
+            nombre:'sdfdsf',
+            dni:'sdfdsf',
+            lastname:'sdfdsf',
+            tipo:'sdf',
+            clase:'sdf'
+        },
+
+        {
+            hora:'sdfdsf',
+            nombre:'sdfdsf',
+            dni:'sdfdsf',
+            lastname:'sdfdsf',
+            tipo:'sdf',
+            clase:'sdf'
+        },
+
+        {
+            hora:'sdfdsf',
+            nombre:'sdfdsf',
+            dni:'sdfdsf',
+            lastname:'sdfdsf',
+            tipo:'sdf',
+            clase:'sdf'
+        },
+
+        {
+            hora:'sdfdsf',
+            nombre:'sdfdsf',
+            dni:'sdfdsf',
+            lastname:'sdfdsf',
+            tipo:'sdf',
+            clase:'sdf'
+        },
+
+        {
+            hora:'sdfdsf',
+            nombre:'sdfdsf',
+            dni:'sdfdsf',
+            lastname:'sdfdsf',
+            tipo:'sdf',
+            clase:'sdf'
+        }
+
+    ];
+
+    */
+
+
+
+  }
 ]);
