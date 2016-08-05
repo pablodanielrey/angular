294ee2e5ebb81f73d5264b334e9406a066542557
Emanuel Pais <emanuel84@gmail.com>
2016-08-05 10:35:22 -0300

    Corregi un error

--- web/systems/issues/index.controller.js
+++ web/systems/issues/index.controller.js
@@ -9,16 +9,7 @@
     function IndexCtrl($scope) {
         var vm = this;
         $scope.time = new Date().getTime();
-        $scope.getMenu = getMenu;
-        $scope.getMenuCel = getMenuCel;
 
-        function getMenu() {
-          return 'modules/menu/index.html?t=' + $scope.time;
-        }
-
-        function getMenuCel() {
-          return 'modules/menuCel/index.html?t=' + $scope.time;
-        }
     };
 
 })();
