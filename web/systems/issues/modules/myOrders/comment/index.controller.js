ad8b0e333620e4b6f14f2ffd29c8b521cb0cd13b
Emanuel Pais <emanuel84@gmail.com>
2016-08-16 11:22:25 -0300

    Quedo funcionando el crear comentario

--- web/systems/issues/modules/myOrders/comment/index.controller.js
+++ web/systems/issues/modules/myOrders/comment/index.controller.js
@@ -5,16 +5,82 @@
         .module('issues')
         .controller('MyOrdersCommentCtrl', MyOrdersCommentCtrl);
 
-    MyOrdersCommentCtrl.$inject = ['$scope', '$routeParams'];
+    MyOrdersCommentCtrl.$inject = ['$scope', '$routeParams', '$location', 'Issues', 'Files'];
 
     /* @ngInject */
-    function MyOrdersCommentCtrl($scope, $routeParams) {
+    function MyOrdersCommentCtrl($scope, $routeParams, $location, Issues, Files) {
         var vm = this;
+
+        vm.model = {
+          issue: null,
+          replyDescription: '',
+          files: []
+        }
+
+        vm.addFile = addFile;
+        vm.removeFile = removeFile;
+        vm.createComment = createComment;
+        vm.cancelComment = cancelComment;
+
 
         activate();
 
         function activate() {
-          console.log($routeParams);
+          var params = $routeParams;
+          if (params.issueId == undefined) {
+            $location.path('/myOrdersList');
+          }
+          vm.model.files = [];
+          loadIssue(params.issueId);
         }
+
+        function loadIssue(id) {
+          Issues.findById(id).then(
+            function(issue) {
+              vm.model.issue = issue;
+            }, function(error) {
+              vm.messageError(error);
+            }
+          );
+        }
+
+        function addFile(fileName,fileContent, fileType, fileSize) {
+          var file = {};
+          file.name = fileName;
+          file.content = window.btoa(fileContent);
+          file.type = fileType;
+          file.size = fileSize;
+          file.codec = Files.BASE64;
+          vm.model.files.push(file);
+        }
+
+        function removeFile(file) {
+          var index = vm.model.files.indexOf(file);
+          if (index >= 0) {
+            vm.model.files.splice(index, 1);
+          }
+        }
+
+        function cancelComment() {
+          $location.path('myOrdersDetail/' + vm.model.issue.id);
+        }
+
+        function createComment() {
+          var subject = vm.model.issue.subject;
+          var parentId = vm.model.issue.id;
+          var officeId = vm.model.issue.projectId;
+
+          // vm.messageLoading();
+          Issues.createComment(subject, vm.model.replyDescription, parentId, officeId, vm.model.files).then(
+            function(data) {
+
+            }, function(error) {
+              vm.messageError(error);
+            }
+          );
+        }
+
+
+
     }
 })();
