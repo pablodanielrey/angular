928899f3f6a0c0176d8f4fa3943fc2989b3fda31
Emanuel Pais <emanuel84@gmail.com>
2016-08-18 13:27:20 -0300

    Deje funcionando el buscador de usuario en la pantalla de creacion

--- web/systems/issues/modules/orders/create/index.controller.js
+++ web/systems/issues/modules/orders/create/index.controller.js
@@ -5,15 +5,16 @@
         .module('issues')
         .controller('OrdersCreateCtrl', OrdersCreateCtrl);
 
-    OrdersCreateCtrl.$inject = ['$scope', '$location', 'Login', 'Issues'];
+    OrdersCreateCtrl.$inject = ['$scope', '$location', 'Login', 'Issues', 'Files'];
 
     /* @ngInject */
-    function OrdersCreateCtrl($scope, $location,  Login, Issues) {
+    function OrdersCreateCtrl($scope, $location,  Login, Issues, Files) {
         var vm = this;
 
         // variables del modelo
         vm.model = {
-          user: null,
+          user: {name: '-'},
+          search: '',
           users: []
         }
 
@@ -21,37 +22,56 @@
 
         // variables de la vista
         vm.view = {
-
+          searching: false
         }
 
         // métodos
-
-
         activate();
         vm.cancel = cancel;
         vm.searchUsers = searchUsers;
-
-        function searchUsers(regex) {
-
-          Issues.searchUsers(regex).then(
-            function(users) {
-              vm.model.users = users;              
-            }, function(error) {
-              console.log(error);
-            }
-          );
-        }
+        vm.getUserPhoto = getUserPhoto;
+        vm.selectUser = selectUser;
 
 
         function activate() {
           vm.model.userId = Login.getCredentials()['userId'];
-          searchUsers('walt');
         }
 
         function cancel() {
           $location.path('/orders');
         }
 
+        function getUserPhoto(user) {
+          if (user == null || user.photo == null) {
+            return '/systems/login/modules/img/imgUser.jpg';
+          } else {
+            return Files.toDataUri(user.photo);
+          }
+        }
+
+        function searchUsers() {
+          if (vm.view.searching) {
+            return
+          }
+          if (vm.model.search.length < 5) {
+            vm.view.searching = false;
+            return;
+          }
+          vm.view.searching = true;
+          Issues.searchUsers(vm.model.search).then(
+            function(users) {
+              vm.view.searching = false;
+              vm.model.users = users;
+            }, function(error) {
+              console.log(error);
+            }
+          );
+        }
+
+        function selectUser(user) {
+          vm.view.style2 = '';
+          vm.model.user = user;
+        }
 
     }
 })();
