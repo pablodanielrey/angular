4b25981183e516468ffbf6463f65848cc5eb6016
Emanuel Pais <emanuel84@gmail.com>
2016-09-23 13:49:18 -0300

    Cree el modelo de oficina, falta crear los DAO

--- python/model/offices/office.py
+++ python/model/offices/office.py
@@ -1,31 +1,87 @@
 # -*- coding: utf-8 -*-
 from model.serializer import JSONSerializable
 
-class AbstractOffice(JSONSerializable):
+
+class Office(JSONSerializable):
+
+    officeType = ['unit', 'office', 'physical-office', 'area']
 
     def __init__(self):
-        self.name = ''
-
-class Office(AbstractOffice):
-
-    def __init__(self):
-        super().__init__()
+        self.id = None
         self.parent = None
+        self.name = None
+        self.telephone = None
+        self.number = None
+        self.type = officeType[1]
+        self.email = None
+        self.designations = []
+        self.childs = []
 
-class Area(AbstractOffice):
+    def persist(self, con):
+        return OfficeDAO.persist(con)
+
+    @classmethod
+    def findAll(cls, con, type=None):
+        return OfficeDAO.findAll(con, type)
+
+    @classmethod
+    def findByIds(cls, con, ids):
+        return OfficeDAO.findByIds(con, ids)
+
+
+class Designation(JSONSerializable):
 
     def __init__(self):
-        super().__init__()
-        self.parent()
-
-class Dependency(AbstractOffice):
-
-    def __init__(self):
-        super().__init__()
-
-class Funcion(JSONSerializable):
-
-    def __init__(self):
-        self.office = None
+        self.officeId = None
         self.position = 'Cumple función'
-        self.user = None
+        self.userId = None
+        self.start = None
+        self.end = None
+
+
+    @classmethod
+    def findByIds(cls, con, ids):
+        return DesignationDAO.findByIds(con, ids)
+
+
+    @classmethod
+    def getDesignationByUser(cls, con, userId, history=False):
+        return DesignationDAO.getDesignationByUser(con, userId)
+
+
+    @classmethod
+    def getDesignationByOffice(cls, con, officeId, history=False):
+        return DesignationDAO.getDesignationByOffice(con, officeId)
+
+
+    @classmethod
+    def getDesignationByPosition(cls, con, position, history=False):
+        return DesignationDAO.getDesignationByPosition(con, position)
+
+
+    def persist(self, con):
+        return DesignationDAO.persist(con, self)
+
+
+class OfficeModel():
+
+    @classmethod
+    def getOfficesByUser(cls, con, userId, tree=False, types=None):
+        idsD = Designation.getDesignationByUser(con, userId)
+        desig = Designation.findByIds(con, idsD)
+        oIds = [d.officeId for d in desig]
+        if types is None:
+            return oIds
+
+        offices = Office.findByIds(con, oIds)
+        return [office.id for office in offices if office.type in types]
+
+    @classmethod
+    def getUsers(cls, con, oId):
+        idsD = Designation.getDesignationByOffice(con, oId)
+        desig = Designation.findByIds(con, idsD)
+        uIds = []
+        for d in desig:
+            if d.userId not in uids:
+                uIds.append(d.userId)
+        return uIds
