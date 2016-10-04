7bf66da7b3625d9abc68cdb9a4b67931f172a023
Emanuel Pais <emanuel84@gmail.com>
2016-10-04 10:30:10 -0300

    Deje el buscar con los usuarios incluidos

--- web/systems/issues/directives/listIssues.directive.js
+++ web/systems/issues/directives/listIssues.directive.js
@@ -91,7 +91,13 @@
             return;
           }
           var user = vm.users[issue.userId];
-          return (user == null ) ? 'No tiene nombre' : getName(user) + ' ' + getLastname(user);
+          if (issue == null) {
+            return;
+          }
+
+          issue.fullName = (user == null ) ? 'No tiene nombre' : getName(user) + ' ' + getLastname(user);
+
+          return issue.fullName;
         }
 
         function getName(user) {
@@ -115,7 +121,8 @@
           }
 
           var user = vm.users[issue.creatorId];
-          return (user == null ) ? 'No tiene nombre' : getName(user) + ' ' + getLastname(user);
+          issue.creatorName = (user == null ) ? 'No tiene nombre' : getName(user) + ' ' + getLastname(user);
+          return issue.creatorName;
         }
 
         function getPriority(issue) {
