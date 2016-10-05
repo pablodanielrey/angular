b5e949c5a1230182f243827ede68a3917d146709
Emanuel Pais <emanuel84@gmail.com>
2016-10-05 11:03:11 -0300

    Solucione el error en pedidos asignados, antes de buscar la oficina verifico que exista en el redmine

--- python/model/issue/issue.py
+++ python/model/issue/issue.py
@@ -342,9 +342,14 @@ class RedmineAPI:
         if redmine is None:
             return []
         issues = []
+        projects = [p.identifier for p in redmine.project.all()]
         for pidentifier in pidentifiers:
             # issues.extend(redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pidentifier, subproject_id='!*', status_id='open'))
-            issues.extend(redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pidentifier, status_id='open'))
+
+            if pidentifier in projects:
+                issues.extend(redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pidentifier, status_id='open'))
+            else:
+                print(pidentifier)
         return issues
 
     @classmethod
