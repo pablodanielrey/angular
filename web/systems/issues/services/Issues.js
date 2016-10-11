0ae7d7fa29c1d4d126d012a6ead4a7a5545b1299
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-10-11 20:25:52 -0300

    implemente cache del lado del cliente

--- web/systems/issues/services/Issues.js
+++ web/systems/issues/services/Issues.js
@@ -12,6 +12,7 @@
 		  this.getOfficesIssues = getOfficesIssues;
 		  this.getAssignedIssues = getAssignedIssues;
 		  this.findById = findById;
+			this.findByIds = findByIds;
 			this.findAll = findAll;
 		  this.create = create;
 		  this.createComment = createComment;
@@ -32,21 +33,6 @@
 				return Login.getPrivateTransport().call('issues.search_users', [regex]);
 			}
 
-		  function getMyIssues() {
-				var myIssues = $window.sessionStorage.getItem('myIssues');
-				if (myIssues != null) {
-					var d = $q.defer();
-					d.resolve(JSON.parse(myIssues));
-					return d.promise;
-				}
-
-				return Login.getPrivateTransport().call('issues.get_my_issues').then(
-					function(issues) {
-						$window.sessionStorage.setItem('myIssues', JSON.stringify(issues));
-						return issues;
-					}
-				);
-			}
 
 		  function getOfficesIssues() {
 				return Login.getPrivateTransport().call('issues.get_offices_issues');
@@ -63,7 +49,11 @@
 			}
 
 		  function getAssignedIssues(statuses, froms, tos) {
-				return Login.getPrivateTransport().call('issues.get_assigned_issues', [statuses, froms, tos]).then(findByIds);
+				return Login.getPrivateTransport().call('issues.get_assigned_issues', [statuses, froms, tos]).then(_findFromCacheOrLoad).then(_cacheIssues);
+			}
+
+			function getMyIssues(statuses, froms, tos) {
+				return Login.getPrivateTransport().call('issues.get_my_issues', [statuses, froms, tos]).then(_findFromCacheOrLoad).then(_cacheIssues);
 			}
 
 			function updateIssue(id, status, priority) {
@@ -71,14 +61,6 @@
 				item.statusId = status;
 				item.priority = priority;
 				$window.sessionStorage.setItem(id, JSON.stringify(item));
-			}
-
-			function _loadIssues(ids) {
-				var issues = [];
-				for (var i = 0; i < ids.length; i++) {
-					issues.push(JSON.parse($window.sessionStorage.getItem(ids[i])));
-				}
-				return issues;
 			}
 
 		  function findById(id) {
@@ -96,27 +78,6 @@
 			}
 
 
-			/*
-				Solo sirve para invalidar la cache de los issues que existan.
-			*/
-			function _asyncInvalidateCache(issueId) {
-				var d = $q.defer()
-				$window.sessionStorage.removeItem('assignedIssues');
-				$window.sessionStorage.removeItem('myIssues');
-				d.resolve(issueId);
-				return d.promise;
-			}
-
-			/*
-				Invalida un issue sin invalidar la lista de todos los demas.
-				se usa cuando se modifica un solo issue.
-			*/
-			function _asyncInvalidateSingleIssueCache(issue) {
-				var d = $q.defer()
-				$window.sessionStorage.removeItem(issue.id);
-				d.resolve(issueId);
-				return d.promise;
-			}
 
 		  function create(subject, description, parentId, officeId, fromOfficeId, authorId, files) {
 				return Login.getPrivateTransport().call('issues.create', [subject, description, parentId, officeId, fromOfficeId, authorId, files]).then(_asyncInvalidateCache)
@@ -145,6 +106,103 @@
 			function getAreas(officeId) {
 				return Login.getPrivateTransport().call('issues.get_areas',[officeId]);
 			}
+
+
+			//////////////////////////// CAPA CACHE DEL SISTEMA EN EL CLIENTE ///////////////////////
+			/// la cache se compone de un indice que cachea el findAll y un cache de cada entrada del indice.
+			///
+			/// assignedIssues --> cachea la lista de ids de issues asignados a la persona logueada que existen en el sistema
+			///
+			///  issue.id ---> cache de cada issue en particular dentro del cliente
+			///
+			///////
+
+			/*
+				Obtiene el issue desde la cache o lo carga usando findByIds
+				param: [ id1, id2, id3, .... ]
+				return: [ Issue, Issue, Issue, ..... ]
+			*/
+			function _findFromCacheOrLoad(ids) {
+				var remainingIds = [];
+
+				// cargo desde la cache los issues existentes
+				var issues = [];
+				for (var i = 0; i < ids.length; i++) {
+					var iss = $window.sessionStorage.getItem(ids[i]);
+					if (iss != null) {
+						issues.push(JSON.parse(iss));
+					} else {
+						remainingIds.push(ids[i]);
+					}
+				}
+
+				// si estaban todos los ids pedidos en la cache
+				if (remainingIds.length <= 0) {
+					return $q.when(issues);
+				}
+
+				// si no, cargo lso que faltan desde el servidor usando findByIds, los cacheo y retorno tda la lista.
+				var d = $q.defer();
+				findByIds(remainingIds).then(_cacheIssues).then(function(iss) {
+					var totalIssues = issues.concat(iss);
+					return d.resolve(totalIssues);
+				});
+				return d.promise;
+			}
+
+
+
+			/*
+				Cachea la lista de ids de issues que estan asignados a la persona
+			*/
+			function _asyncCacheAssignedIssuesIndex(issueIds) {
+				$window.sessionStorage.setItem('assignedIssues', JSON.stringify(issueIds));
+				$q.when(issueIds)
+			}
+
+			/*
+				Cachea la lista de ids de issues que realizó la persona
+			*/
+			function _asyncCacheMyIssuesIndex(issueIds) {
+				$window.sessionStorage.setItem('myIssues', JSON.stringify(issueIds));
+				$q.when(issueIds)
+			}
+
+
+			/*
+				Cachea los issues dentro de la cache, solo si ya no existe.
+				param: [ Issue, Issue, ... ]
+				return: [ Issue, Issue, Issue, ... ]
+			*/
+			function _cacheIssues(issues) {
+				for (var i = 0; i < issues.length; i++) {
+					var issue = issues[i];
+					if ($window.sessionStorage.getItem(issue.id) == null) {
+						$window.sessionStorage.setItem(issue.id, JSON.stringify(issue));
+					}
+				}
+				return $q.when(issues);
+			}
+
+			/*
+				Invalida la cache de los issues asignados a la persona y los que realizó.
+			*/
+			function _asyncInvalidateCache(issueId) {
+				$window.sessionStorage.removeItem('assignedIssues');
+				$window.sessionStorage.removeItem('myIssues');
+				return $q.when(issueId);
+			}
+
+			/*
+				Invalida un issue en particular.
+			*/
+			function _asyncInvalidateSingleIssueCache(issue) {
+				$window.sessionStorage.removeItem(issue.id);
+				return $q.when(issueId);
+			}
+
+			//////////////////////////////////
+
 
 		}
 
