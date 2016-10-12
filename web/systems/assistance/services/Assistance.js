f9020566010da959cc28d5304c0738637084b298
Emanuel Pais <emanuel84@gmail.com>
2016-10-11 21:16:59 -0300

    Agregue el metodo findUserById en el services de assistance

--- web/systems/assistance/services/Assistance.js
+++ web/systems/assistance/services/Assistance.js
@@ -34,5 +34,35 @@
 				return Login.getPrivateTransport().call('assistance.get_statistics', [di,de,userIds])
 			}
 
+			/*
+				Setea los usuarios a los logs
+				Ej: [{userId:id1},{userId:id2}]
+				retorna: [{userId:id1, user: user1}, {userId: id2, user: user2}]
+			*/
+			function findUsersById(logs) {
+				var uids = [];
+	      for (var i = 0; i < logs.length; i++) {
+	        uids.push(logs[i].userId);
+	      }
+				return Users.findById(uids).then(
+					function(users) {
+						for (var i = 0; i < logs.length; i++) {
+							for(var j = 0; j < users.length; j++) {
+								if (logs[i].userId == users[j].id) {
+									logs[i].user = users[i];
+									break;
+								}
+							}
+						}
+						return logs;
+					}
+				);
+			}
+
+			/*
+			Setea el atributo photo al user del log
+			Ej: [{user: user1, userId: id1}, {user: user2, userId: id2}]
+			*/
+
     }
 })();
