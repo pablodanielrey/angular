fe6dfa6e2609f8b186e4d0af11f408edb615f9a9
Emanuel Pais <emanuel84@gmail.com>
2016-10-12 12:43:10 -0300

    Refactorice el código

--- web/systems/assistance/modules/logs/index.controller.js
+++ web/systems/assistance/modules/logs/index.controller.js
@@ -4,7 +4,7 @@
       .module('assistance')
       .controller('LogsCtrl', LogsCtrl);
 
-    LogsCtrl.$inject = ['$scope', 'Assistance', 'Users', 'Login', 'Files'];
+    LogsCtrl.$inject = ['$scope', 'Assistance', 'Users', 'Login', 'Files', '$timeout'];
 
     function _getDateInTime(hours, minutes, seconds) {
       var d = new Date()
@@ -15,7 +15,7 @@
       return d;
     }
 
-    function LogsCtrl($scope, Assistance, Users, Login, Files) {
+    function LogsCtrl($scope, Assistance, Users, Login, Files, $timeout) {
         var vm = this;
 
         vm.view = {
@@ -36,13 +36,13 @@
           logs: [],
           todayLogs: [],
           status: [],
+          statusToday: [],
           inside: 0,
           outside: 0
         };
 
         vm.resetSearchAndGetLogs = resetSearchAndGetLogs;
         vm.searchLogs = searchLogs;
-        vm.getUserPhoto = getUserPhoto;
         vm.sortDate = sortDate;
         vm.sortAccess = sortAccess;
         vm.sortName = sortName;
@@ -63,7 +63,9 @@
         }
 
 
-
+        /* ********************************************************************************
+                              MÉTODOS DE ORDENACIÓN
+         * ******************************************************************************** */
         function sortDate() {
           vm.view.reverse = (vm.view.sort[0] == 'date') ? !vm.view.reverse : true;
           vm.view.sort = ["date"];
@@ -83,6 +85,9 @@
           vm.view.reverse = (vm.view.sort[0] == 'dni') ? !vm.view.reverse : true;
           vm.view.sort = ["dni", "date", "name", "lastname"];
         }
+
+
+        /* ---------------------------------------------------------------------------- */
 
 
         function _resetSearch() {
@@ -105,20 +110,9 @@
           _getLogs();
         }
 
-
-        function getUserPhoto(log) {
-          return log != null && "genre" in log && log.genre != null && (log.genre.toLowerCase() == 'femenino' || log.genre.toLowerCase() == 'mujer') ? "/systems/offices/img/avatarWoman.jpg" : "/systems/offices/img/avatarMan.jpg";
-          /* Comentado pq falta obtener la foto del lado del servidor
-          if (log == null || log.photo == null) {
-            var img = log != null && "genre" in log && log.genre != null && (log.genre.toLowerCase() == 'femenino' || log.genre.toLowerCase() == 'mujer') ? "/systems/offices/img/avatarWoman.jpg" : "/systems/offices/img/avatarMan.jpg";
-            return img;
-          } else {
-            return Files.toDataUri(log.photo);
-          }*/
-        }
-
-
-        /////// Estado de los ususarios (si se encuentra afuera o adentro) /////
+        /* ********************************************************************************
+                        Estado de los ususarios (si se encuentra afuera o adentro)
+         * ******************************************************************************** */
 
         function _getStatus(userId) {
           for (var i = 0; i < vm.model.status.length; i++) {
@@ -135,10 +129,9 @@
               vm.model.status[i].status = !(vm.model.status[i].status);
               if (vm.model.status[i].status) {
                 vm.model.inside = vm.model.inside + 1;
-                vm.model.outside = vm.model.outside - 1;
                 return true;
               } else {
-                vm.model.inside = vm.model.inside - 1;
+                vm.model.inside = (vm.model.inside == 0) ? 0 : vm.model.inside - 1;
                 vm.model.outside = vm.model.outside + 1;
                 return false;
               }
@@ -149,176 +142,102 @@
           return true;
         }
 
+        function _calcStatusToday(userId) {
+          for (var i = 0; i < vm.model.statusToday.length; i++) {
+            if (vm.model.statusToday[i].id == userId) {
+              vm.model.statusToday[i].status = !(vm.model.statusToday[i].status);
+              if (vm.model.statusToday[i].status) {
+                return true;
+              } else {
+                return false;
+              }
+            }
+          }
+          vm.model.statusToday.push({id:userId, status:true});
+          return true;
+        }
+
         function _resetStatus() {
           vm.model.status = [];
           vm.model.inside = 0;
           vm.model.outside = 0;
         }
 
-        /////////////////////////
+        // ------------------------------------------------------------------------------------
 
-        function _parseLog(log, users) {
-          var user = _findUser(log.userId, users);
-          var cca = _calcStatus(user.id) ? 'entrada' : 'salida';
+        /* ********************************************************************************
+                                Obtenención de los logs
+         * ******************************************************************************** */
+
+
+        // parsea los logs a mostrar
+        function _parseLog(log, cca) {
           var d = new Date(log.log);
-          var photo = (user.photoSrc != '') ? user.photoSrc : "/systems/issues/img/avatarMan.jpg";
+          var photo = (log.user != null && log.user.photoSrc != '') ? log.user.photoSrc : "/systems/issues/img/avatarMan.jpg";
           return {
               clase: cca,
               tipo: 'NoDocente',
-              name: user.name.trim(),
-              lastname: user.lastname.trim(),
-              photoSrc: photo ,
-              genre: user.genre,
-              dni: user.dni,
+              name: log.user.name.trim(),
+              lastname: log.user.lastname.trim(),
+              photoSrc: photo,
+              dni: log.user.dni,
               date: d
           }
         }
 
+
+        function _parseToday(log) {
+          var cca = _calcStatusToday(log.user.id) ? 'entrada' : 'salida';
+          return _parseLog(log, cca);
+        }
+
+        function _parseAll(log) {
+          var cca = _calcStatus(log.user.id) ? 'entrada' : 'salida';
+          return _parseLog(log, cca);
+        }
+
+
+        // obtiene un resumen de los últimos logs
         function _getTodayLogs() {
           vm.model.todayLogs = [];
           var dayMillis = 24 * 60 * 60 * 1000;
           var start = new Date();
           start.setHours(0); start.setMinutes(0); start.setSeconds(0);
           var end = new Date(start.getTime() + (dayMillis) - 1);
-          Assistance.getLogs(start, end).then(function(logs) {
-
-            if (logs.length <= 0) {
-              return;
-            }
-
-            // busco a los usuarios de los logs.
-            var uids = _getUsers(logs);
-            Users.findById(uids).then(Users.findPhotos).then(Users.photoToDataUri).then(
-              function(users) {
-                $scope.$apply(function() {
-                  var status = [];
-                  for (var i = 0; i < logs.length; i++) {
-                    vm.model.todayLogs.push(_parseLog(logs[i], users, status));
-                  }
-                });
-              }, function(error) {
-                console.error(error);
-              });
-
-
-            /*Users.findById(uids).then(function(users) {
-              // seteo los logs y los estados
-              $scope.$apply(function() {
-                var status = [];
+          Assistance.getLogs(start, end).then(Assistance.findUsersByLogs).then(Assistance.findUserPhotos).then(Assistance.photoToDataUri).then(
+            function(logs) {
+              $timeout(function () {
                 for (var i = 0; i < logs.length; i++) {
-                  vm.model.todayLogs.push(_parseLog(logs[i], users, status));
+                  vm.model.todayLogs.push(_parseToday(logs[i]));
                 }
               });
-
-            }, function(err) {
-              console.log(err);
-            });*/
-
-          }, function(err) {
-            console.log(err);
-          });
+            }, function(error) {
+              console.log(error);
+            }
+          );
         }
 
-
+        // obtiene los logs
         function _getLogs() {
           vm.model.logs = [];
           _resetStatus();
 
           var s = vm.model.search;
-          Assistance.getLogs(s.initDate, s.endDate).then(function(logs) {
-            console.log(logs);
-
-            if (logs.length <= 0) {
-              return;
-            }
-
-            // busco a los usuarios de los logs.
-            var uids = _getUsers(logs);
-            Users.findById(uids).then(function(users) {
-
-              // seteo los logs y los estados
-              $scope.$apply(function() {
-                var status = [];
-                for (var i = 0; i < logs.length; i++) {
-                  vm.model.logs.push(_parseLog(logs[i], users, status));
-                }
-              });
-
-            }, function(err) {
-              console.log(err);
+          vm.view.style = 'cargando';
+          Assistance.getLogs(s.initDate, s.endDate).then(Assistance.findUsersByLogs).then(function(logs){
+            $timeout(function () {
+              for (var i = 0; i < logs.length; i++) {
+                vm.model.logs.push(_parseAll(logs[i]));
+              }
+              vm.view.style = '';
             });
-
           }, function(err) {
             console.log(err);
           })
-
         }
 
     };
 
-
-    function _findUser(uid, users) {
-      for (var i = 0; i < users.length; i++) {
-        if (users[i].id == uid) {
-          return users[i];
-        }
-      }
-      return null;
-    }
-
-    function _getUsers(logs) {
-      var uids = [];
-      for (var i = 0; i < logs.length; i++) {
-        uids.push(logs[i].userId);
-      }
-      return uids;
-    }
 
 
 })();
-
-
-/*
-var app = angular.module('assistance');
-
-app.controller('LogsCtrl', ["$rootScope", '$scope', 'Login',
-  function ($rootScope, $scope) {
-    var mv = this;
-
-    var nombres = ['Walter Roberto', 'Pablo Daniel', 'Alejandro Agustin', 'Maximiliano Antonio', 'Ivan Roberto'];
-    var dnis = ['2738457298', '27294557', '30001823', '12345678', '876554432'];
-    var cclase = '';
-
-    $scope.logs = [];
-
-
-    function getLogs() {
-      console.log('getLogs');
-    }
-
-
-
-    for (var i = 0; i < 100; i++) {
-      if (i % 2 == 0) {
-        cclase = 'entrada';
-      } else {
-        cclase = 'salida';
-      };
-
-      $scope.logs.push(
-        {
-          clase: cclase,
-          tipo: 'NoDocente',
-          name: nombres[i % nombres.length],
-          lastname: 'Blanco',
-          dni: dnis[i % dnis.length],
-          hora: '10:30 am',
-          dia: '05/08/2026'
-        }
-      );
-    };
-
-
-  }
-]);
-*/
