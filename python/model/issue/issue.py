806fd3b50e2f09b017a8e48f15467a867ff7f5b1
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-10-12 10:45:57 -0300

    esta funcionando el filtro de estados para los pedidos que se realizan a la oficina

--- python/model/issue/issue.py
+++ python/model/issue/issue.py
@@ -356,7 +356,11 @@ class RedmineAPI:
     def _getIssuesByUser(cls, con, userId, statuses, froms, tos, redmine):
         if redmine is None:
             return []
-        issues = redmine.issue.filter(author_id=userId, status_id='open')
+
+        issues = []
+        for s in statuses:
+            issues.extend(redmine.issue.filter(author_id=userId, status_id=s))
+
         """ elimino las que tienen padre """
         rissues = [issue.id for issue in issues if 'parent' not in dir(issue)]
         #return [issue.id for issue in issues]
@@ -395,14 +399,14 @@ class RedmineAPI:
 
             if pId in projects:
                 if cIds is None:
-                    issues.extend(redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pId, status_id='open'))
+                    issues.extend(redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pId, status_id='*'))
 
                 elif len(cIds) <= 0:
                     """
                         no tienen oficina origen
                     """
                     logging.info('filtrando por cIds = []')
-                    auxIssues = redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pId, status_id='open')
+                    auxIssues = redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pId, status_id='*')
                     issues.extend([iss for iss in auxIssues if not cls._hasCustomField(iss, RedmineAPI.FROM_FIELD)])
 
                 else:
@@ -415,13 +419,15 @@ class RedmineAPI:
                         issues.extend(redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pId, custom_fields=cFields, status_id='open'))
                     """
 
-                    auxIssues = redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pId, status_id='open')
+                    auxIssues = redmine.issue.filter(tracker_id=cls.TRACKER_ERROR, project_id=pId, status_id='*')
                     filteredIssues = set()
                     for cId in cIds:
                         filteredIssues.update([iss for iss in auxIssues if cls._hasCustomFieldValue(iss, RedmineAPI.FROM_FIELD, cId)])
                     issues.extend(filteredIssues)
 
-        logging.info(issues)
+        if statuses is not None and len(statuses) > 0:
+            issues = [i for i in issues if i.status.id in statuses]
+
         return issues
 
     @classmethod
