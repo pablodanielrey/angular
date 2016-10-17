4952a1bcc879d9205f42a572c7d6f25f2d4f1601
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-10-17 10:23:55 -0300

    converti el traceback a anotación

--- python/model/connection/connection.py
+++ python/model/connection/connection.py
@@ -9,9 +9,35 @@ import inject
 
 from model.registry import Registry
 
-import traceback
+
 import logging
 logging.basicConfig(format='%(asctime)s, %(stack_info)s, %(thread)s, %(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
+
+import cProfile
+def do_cprofile(func):
+    def profiled_func(*args, **kwargs):
+        profile = cProfile.Profile()
+        try:
+            profile.enable()
+            result = func(*args, **kwargs)
+            profile.disable()
+            return result
+        finally:
+            profile.print_stats()
+    return profiled_func
+
+import traceback
+def do_traceback(func):
+    logging.basicConfig(format='%(asctime)s, %(stack_info)s, %(thread)s, %(message)s')
+    logging.getLogger().setLevel(logging.DEBUG)
+    def tracebacked_func(*args, **kwargs):
+        for line in traceback.format_stack():
+            logging.info(line.strip())
+        r = func(*args, **kwargs)
+        return r
+    return tracebacked_func
+
 
 class Connection:
 
@@ -22,22 +48,23 @@ class Connection:
         conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
     def __init__(self, registry=Registry()):
+
+        self.logging.setLevel(logging.DEBUG)
+
         self.host = registry.get('host')
         self.database = registry.get('database')
         self.user = registry.get('user')
         self.password = registry.get('password')
         self.pool = psycopg2.pool.ThreadedConnectionPool(1, 50, host=self.host, database=self.database, user=self.user, password=self.password, cursor_factory=DictCursor)
 
+    @do_traceback
     def get(self):
         self.logging.debug('obteniendo conexion a la base')
-        #for line in traceback.format_stack():
-        #    self.logging.debug(line.strip())
         return self.pool.getconn()
 
+    @do_traceback
     def put(self, conn):
         self.logging.debug('retornando la conexion al pool')
-        #for line in traceback.format_stack():
-        #    self.logging.debug(line.strip())
         self.pool.putconn(conn)
 
     def __del__(self):
