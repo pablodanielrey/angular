ad1d9de6151c155598fec0bbbcdaf83efd32c187
Emanuel Pais <emanuel84@gmail.com>
2016-11-17 13:54:49 -0300

    Lo cambie para que lo escriba a un archivo a las regals de iptables

--- scripts/wifi/generateFirewall.py
+++ scripts/wifi/generateFirewall.py
@@ -22,9 +22,9 @@ if __name__ == '__main__':
     reg = inject.instance(Registry)
     conn = Connection(reg.getRegistry('dcsys'))
     con = conn.get()
+    rules = ""
     try:
         publics = PublicIp.findById(con, PublicIp.findAll(con))
-        rules = ""
         for public in publics:
             # obtengo las ip privada relacionada a esa ip publica
             hosts = DhcpHost.findById(con, DhcpHost.findByMac(con, public.mac))
@@ -34,19 +34,10 @@ if __name__ == '__main__':
             rules = rules + "iptables -t nat -A PREROUTING -d {} -m state --state NEW -j DNAT --to-destination {} \n".format(str(public.ip.ip), str(hosts[0].ip.ip))
             for h in hosts:
                 rules = rules + "iptables -t nat -A POSTROUTING -s {} -j SNAT --to-source {} \n".format(str(h.ip.ip), str(public.ip.ip))
-        print(rules)
+            rules = rules + "\n"
 
     finally:
         conn.put(con)
 
-
-"""
-ips publicas de videoconferencia: 163.10.17.131, 163.10.17.132, 163.10.17.133
-"""
-#  hardware ethernet 38:60:77:05:51:3B;
-#  fixed-address 10.11.1.1;
-
-#host videoconferencia-prueba {
-#  hardware ethernet 50:32:75:a1:1e:f9;
-#  fixed-address 10.100.1.253;
-#}
+    with open('/tmp/firewall','w') as f:
+        f.write(rules)
