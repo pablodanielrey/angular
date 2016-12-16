fa8423d79e3f12262f6819e5251b22431f404d8e
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-12-16 09:19:55 -0300

    agregue inicio de la conexión de base de datos

--- scripts/google/syncUsers.py
+++ scripts/google/syncUsers.py
@@ -1,3 +1,10 @@
+import psycopg2
+from psycopg2 import pool
+from psycopg2.extras import DictCursor
+import sys
+
+
+
 from gauth import GAuth
 
 #https://www.googleapis.com/auth/admin.directory.user, https://www.googleapis.com/auth/admin.directory.user.alias, https://mail.google.com/, https://www.googleapis.com/auth/drive, https://www.googleapis.com/auth/spreadsheets
@@ -11,7 +18,34 @@ SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets'
     ]
 
+
+
+
 if __name__ == '__main__':
+
+    host = sys.argv[1]
+    db = sys.argv[2]
+    duser = sys.argv[3]
+    dpass = sys.argv[4]
+
+
+    pool = psycopg2.pool.ThreadedConnectionPool(1, 50, host=host, database=db, user=duser, password=dpass, cursor_factory=DictCursor)
+    try:
+        conn = pool.getconn()
+        try:
+            conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+            cur = conn.cursor()
+            try:
+                cur.execute('select ')
+
+
+            finally:
+                cur.close()
+        finally:
+            pool.putconn(conn)
+    finally:
+        pool.closeall()
+
     service = GAuth.getService('admin', 'directory_v1', SCOPES, 'econo@econo.unlp.edu.ar')
 
     results = service.users().list(domain='econo.unlp.edu.ar').execute()
