69bdd372d77ef54eac2983daafed55b61108f33b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2016-12-19 19:09:25 -0300

    agregue codigo por el tema del oauth para generar los tokens. pero no esta funcionando.

--- php/wordpress-mautic/mautic-post-send/mautic-post-send.php
+++ php/wordpress-mautic/mautic-post-send/mautic-post-send.php
@@ -12,6 +12,11 @@ Version: 0.1
 Author URI:
 */
 
+include __DIR__ . '/vendor/autoload.php';
+
+use Mautic\Auth\ApiAuth;
+use Mautic\MauticApi;
+
 function mautic_post_send_email( $post_id ) {
 
 	// If this is just a revision, don't send the email.
@@ -25,8 +30,62 @@ function mautic_post_send_email( $post_id ) {
 	$message = "Se ha actualizado un post en el wordpress:\n\n";
 	$message .= $post_title . ": " . $post_url;
 
-	// Send email to admin.
-	wp_mail( 'ditesi@econo.unlp.edu.ar', $subject, $message );
+	error_log('ejecute codigo de wp');
+
+	try {
+
+		$baseUrl = 'http://www.mautic.econo.unlp.edu.ar';
+		$publicKey = '';
+		$secretKey = '';
+
+		$accessToken = '';
+		$accessTokenSecret = '';
+
+		// ApiAuth::initiate will accept an array of OAuth settings
+		$settings = array(
+		    'baseUrl'          => $baseUrl,       // Base URL of the Mautic instance
+		    'version'          => 'OAuth1a', // Version of the OAuth can be OAuth2 or OAuth1a. OAuth2 is the default value.
+		    'clientKey'        => $publicKey,       // Client/Consumer key from Mautic
+  	    'clientSecret'     => $secretKey,       // Client/Consumer secret key from Mautic
+		    'callback'         => '',        // Redirect URI/Callback URI for this script
+				'accessToken'			 => $accessToken,
+				'accessTokenSecret' => $accessTokenSecret
+		);
+
+		error_log('inicializo auth');
+		// Initiate the auth object
+		$initAuth = new ApiAuth();
+		$auth = $initAuth->newAuth($settings);
+
+		error_log('auth inicializada');
+		$api = new MauticApi();
+		$emailApi = $api->newApi('emails', $auth, $baseUrl);
+
+		/*
+		if ($auth->validateAccessToken()) {
+		    if ($auth->accessTokenUpdated()) {
+		        $accessTokenData = $auth->getAccessTokenData();
+
+		        //error_log(json_encode($accessTokenData));
+		    }
+		}
+		*/
+
+		error_log('api inicializada');
+		error_log(json_encode($emailApi->get(9)));
+
+
+		error_log(json_encode($emailApi->send(9)));
+
+		error_log('enviado');
+
+	} catch (Exception $e) {
+		error_log('error');
+		error_log($e);
+	}
+
+	error_log('finalizo');
+
 }
 
 add_action( 'save_post', 'mautic_post_send_email' );
