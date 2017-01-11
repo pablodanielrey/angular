18310801ade613acc3c1e79f70e507155c8a5cb5
ivancas84 <ivancas84@gmail.com>
2017-01-11 13:39:01 -0300

    refactorizar userpassword para adaptarlo al nuevo esquema

--- python/model/users/entities/userPassword.py
+++ python/model/users/entities/userPassword.py
@@ -1,33 +1,9 @@
-from model import Ids
-from model.serializer import JSONSerializable
+from model import Entity
 
-class UserPassword(JSONSerializable):
+class UserPassword(Entity):
 
     def __init__(self):
         self.id = None
         self.userId = None
         self.username = None
         self.password = None
-
-    def setPassword(self, passw):
-        self.password = passw
-
-    def persist(self, ctx):
-        ctx.dao(self).persist(ctx, self)
-        return self
-
-    @classmethod
-    def findByIds(cls, ctx, ids):
-        return ctx.dao(cls).findByIds(ctx, ids)
-
-    @classmethod
-    def findByUserId(cls, ctx, userId):
-        return Ids(cls, ctx.dao(cls).findByUserId(ctx, userId))
-
-    @classmethod
-    def findByUsername(cls, ctx, username):
-        return Ids(cls, ctx.dao(cls).findByUsername(ctx, username))
-
-    @classmethod
-    def findByUsernameAndPassword(cls, ctx, username, password):
-        return Ids(cls, ctx.dao(cls).findByUserPassword(ctx, username, password))
--- python/test/testContext.py
+++ python/test/testContext.py
@@ -1,57 +1,39 @@
-
-
 import sys
 sys.path.append('/home/ivan/www/angular/python') #definir ruta de acceso al modelo
 
+import psycopg2
+import psycopg2.pool
+
 
 from model import SqlContext
-from model.users.entities.user import User
-
-con = None
-ctx = SqlContext(con)
-user = User()
+from psycopg2.extras import DictCursor
 
 
-User.find(ctx)
-
-
-
-User.find(ctx, userId=["123"])
-
-
-
-User.find(ctx, userId=["123"], officeId=["123", "345"])
-
-
-
-User.find(ctx, userId=True, valorBooleano=False, officeId=["123", "345"], orderBy={"officeId":True})
-
-
-
-User.findByIds(ctx, [123])
-
-User.findByIds(ctx, [123], userId=True, officeId=["123", "445"])
-
-
-
-
-
-
-
-
-"""
+h = sys.argv[1]
+d = sys.argv[2]
+u = sys.argv[3]
+p = sys.argv[4]
+ 
+pool = psycopg2.pool.ThreadedConnectionPool(1, 1, host=h, database=d, user=u, password=p, cursor_factory=DictCursor)
+con = pool.getconn()
 ctx = SqlContext(con)
 
-Office.findBy(ctx, userId=["123"])
 
 
 
-Office.findBy(ctx, userId=["123"], officeId=["123", "345"])
 
 
 
-Office.findBy(ctx, userId=True, valorBooleano=False, officeId=["123", "345"], orderBy={"officeId":222})
 
 
 
-"""
+
+
+
+
+
+
+
+
+
+
