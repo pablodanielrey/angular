b859b01e777ef90a97ad87490bf07e6725ef532a
Iván <ivancas84@gmail.com>
2017-04-19 09:43:37 -0300

    reestablecer

--- web/systems/tutorias/coordinadores/index.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'user strict'
-  angular
-      .module('tutorias.coordinadores',['ngRoute', 'ui.bootstrap', 'login','files',])
-      .run(function(Login, $window) {
-        Login.check();
-      });
-})();
